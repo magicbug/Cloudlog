36b89a546371bad05cbf3c3bc697cd7a3caa3a74
Peter Goodhall <peter@magicbug.co.uk>
2011-11-20 14:00:21 +0000

    Added more clublog code

--- application/libraries/Clublog.php
+++ application/libraries/Clublog.php
@@ -4,10 +4,17 @@ class Clublog {
 
 	/*
 		Communicates with the Clublog.org API functions
+
+		Send() 
+
+		TODO: Finish ADIF output
+		TODO: Curl Function
+		TODO: Batch importing
+		TODO: Delete
 	*/
 
 	/* Send QSO in real time */
-	public function send() {
+	public function send($qso) {
 
 		// Load Librarys
 		$CI =& get_instance();
@@ -19,7 +26,8 @@ class Clublog {
 		$username = "";
 		$password = "";
 
-		$qso = "QSO_DATE TIME_ON QSLRDATE QSLSDATE CALL OPERATOR MODE BAND FREQ QSL_RCVD LOTW_QSL_RCVD QSL_SENT DXCC PROP_MODE";
+		$adif = "<qso_date:".strlen($qso_date).">".$qso_date."<time_on:".strlen($time_on).">".$time_on."<call:".strlen($qso['call']).">".$qso['call']."<band:".strlen($qso['band']).">".$qso['band']."<mode:".strlen($qso['mode']).">".$qso['mode']."<freq:".strlen($qso['freq']).">".$qso['freq'];
+
 
 		echo $CI->curl->simple_post('curl_test/message', array('message'=>'Sup buddy'));
 
