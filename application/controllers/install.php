42c0219ab325eaafcdddd54e6ccdce50731a193e
Peter Goodhall <peter@magicbug.co.uk>
2011-12-08 17:19:25 +0000

    Removed the Edit QSO option when searching if you havent got auth privs

---
+++ application/controllers/install.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Install extends CI_Controller {
+
+	/* 
+		Contains the basic functions to setup Cloudlog in its simplist form.
+	*/
+
+	// Collect Database Information
+	public function index() {
+
+	}
+	
+	// Collect config information
+	public function config() {
+		
+	}
+	
+	// Setup Username & Password
+	public function user() {
+		
+	}
+	
+}
