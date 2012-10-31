ee2eb20fefd426100f5bcb5ee8aa250dc0d6119e
Peter Goodhall <peter@magicbug.co.uk>
2012-10-31 22:10:33 +0000

    Order WAB Awards Results ASC

--- application/models/wab.php
+++ application/models/wab.php
@@ -8,6 +8,7 @@ class Wab extends CI_Model {
 	}
 	
 	function get_all() {
+		$this->db->order_by("COL_COMMENT", "ASC"); 
 		$this->db->like('COL_COMMENT', 'WAB:');
 		
 		return $this->db->get($this->config->item('table_name'));
