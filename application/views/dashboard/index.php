d86b9d634c8272aa2f2e35903aa7c6ba5ce415d9
Peter Goodhall <peter@magicbug.co.uk>
2012-11-13 20:20:26 +0000

    Serials TX/RX displayed inside labels for improved UI

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -95,8 +95,8 @@
 					<td><?php $timestamp = strtotime($row->COL_TIME_ON); echo date('H:i', $timestamp); ?></td>
 					<td><a class="qsobox" href="<?php echo site_url('logbook/view')."/".$row->COL_PRIMARY_KEY; ?>"><?php echo strtoupper($row->COL_CALL); ?></a></td>
 					<td><?php echo $row->COL_MODE; ?></td>
-					<td><?php echo $row->COL_RST_SENT; ?> <?php if ($row->COL_STX_STRING) { ?>(<?php echo $row->COL_STX_STRING;?>)<?php } ?></td>
-					<td><?php echo $row->COL_RST_RCVD; ?> <?php if ($row->COL_SRX_STRING) { ?>(<?php echo $row->COL_SRX_STRING;?>)<?php } ?></td>
+					<td><?php echo $row->COL_RST_SENT; ?> <?php if ($row->COL_STX_STRING) { ?><span class="label"><?php echo $row->COL_STX_STRING;?></span><?php } ?></td>
+					<td><?php echo $row->COL_RST_RCVD; ?> <?php if ($row->COL_SRX_STRING) { ?><span class="label"><?php echo $row->COL_SRX_STRING;?></span><?php } ?></td>
 					<?php if($row->COL_SAT_NAME != null) { ?>
 					<td><?php echo $row->COL_SAT_NAME; ?></td>
 					<?php } else { ?>
