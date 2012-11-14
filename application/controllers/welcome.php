054eb0598e47126b2564446f8348e799a20106ba
Peter Goodhall <peter@magicbug.co.uk>
2012-11-14 00:56:54 +0000

    qrz fix

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -5,7 +5,25 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+		
+		
+		// URL to the XML Source
+		$xml_feed_url = 'http://xmldata.qrz.com/xml/current/?username='.$username.';password='.$password.';agent=cloudlog';
+		
+		// CURL Functions
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $xml_feed_url);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$xml = curl_exec($ch);
+		curl_close($ch);
+		
+		// Create XML object
+		$xml = simplexml_load_string($xml);
+		
+		print_r($xml);
+
 	}
 }
 
