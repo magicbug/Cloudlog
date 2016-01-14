d7811676a337a29341d1e25f95e05fb2f023a289
Peter Goodhall <peter@magicbug.co.uk>
2016-01-14 12:21:38 +0000

    removed Powered by Cloudlog at the bottom of every page was mucking up the ui

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,10 +1,6 @@
 </div> <!-- end wrapper -->
 
-	<div id="footer">
-		<div class="container">
-				<a href="http://www.cloudlog.co.uk">Powered by Cloudlog</a>
-		</div>
-	</div>
+<!--Powered by Cloudlog - http://www.cloudlog.co.uk -->
 </body>
 
 </html>
