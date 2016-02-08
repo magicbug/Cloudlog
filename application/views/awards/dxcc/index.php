352fe2ec49d9f98707be4fc528b1898e857470c8
Andy <andy@dev.local>
2016-02-08 16:07:39 +0000

    - When adding a new QSO, there is an ajax call to clublog.org to get country. This now uses dxcc info etc - Allow upload .adif files (not just .adi) (fldigi uses .adif) - Search QSO, and click callsign for details - not opening in modal window as does on main page - Show eQSL upload and download status in main log screen

---
+++ application/views/awards/dxcc/index.php
@@ -0,0 +1,44 @@
+
+<div id="container">
+	<h1><?php echo $page_title; ?></h1>
+
+	<!-- Sub Nav for Awards -->
+	
+	<ul class="tabs">
+	  <li class="active"><a href="<?php echo site_url('awards/dxcc'); ?>">DXCC</a></li>
+	  <li><a href="<?php echo site_url('awards/wab'); ?>">WAB</a></li>
+	  <li><a href="<?php echo site_url('awards/sota'); ?>">SOTA</a></li>
+	  <li><a href="<?php echo site_url('awards/wacral'); ?>">WACRAL</a></li>
+	</ul>
+
+	<table width="100%" class="zebra-striped">
+	<tr>
+	<td>Country</td>
+	<td>160m</td>
+	<td>80m</td>
+	<td>40m</td>
+	<td>30m</td>
+	<td>20m</td>
+	<td>17m</td>
+	<td>15m</td>
+	<td>12m</td>
+	<td>10m</td>
+	<td>6m</td>
+	<td>2m</td>
+	</tr>
+    <?php
+
+        foreach($dxcc as $country=>$val){
+            print("<tr><td>$country</td>");
+            foreach($val as $band=>$count){
+                $count = ($count == 0)?"&nbsp;":$count;
+                print("<td>$count</td>");
+            }
+            print("</tr>");
+        }
+
+    ?>
+
+
+	</table>
+</div>
