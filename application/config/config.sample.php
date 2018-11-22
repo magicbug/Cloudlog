c138441d381c0f8dfcbd5b21d51b0ad9a1ddc440
Peter Goodhall <magicbug@users.noreply.github.com>
2018-11-22 14:11:33 +0000

    Set the timezone in the config sample to UTC

--- application/config/config.sample.php
+++ application/config/config.sample.php
@@ -576,7 +576,7 @@ $config['compress_output'] = FALSE;
 | helper' page of the user guide for information regarding date handling.
 |
 */
-$config['time_reference'] = 'local';
+$config['time_reference'] = 'UTC';
 
 /*
 |--------------------------------------------------------------------------
