e9893d8907ae23420818fb9ea6bf3ee74982cc1f
Peter Goodhall <peter@magicbug.co.uk>
2018-11-27 13:59:27 +0000

    Updated header to remove "Tools" and only have Awards
    I think there isn't much need to have dx cluster and awards on the drop down, it would be far more suitable just to have Awards on its own

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -70,15 +70,10 @@
 			<?php } ?>
 			<li><a href="<?php echo site_url('statistics');?>" title="Statistics">Statistics</a></li>
 
+
 			<?php if(($this->config->item('use_auth') && $this->session->userdata('user_type') >= 99) || $this->config->item('use_auth') === FALSE) { ?>
 
-			<li class="dropdown" data-dropdown="dropdown" >
-				<a href="#" class="dropdown-toggle">Tools</a>
-				<ul class="dropdown-menu">
-				 <li><a href="<?php echo site_url('awards');?>" title="">Awards</a></li>
-				 <li><a href="<?php echo site_url('dxcluster');?>" title="DX Cluster">Cluster</a></li>
-				</ul>
-			</li>
+			<li><a href="<?php echo site_url('awards');?>" title="Awards">Awards</a></li>
 
 			<li class="dropdown" data-dropdown="dropdown" >
 				<a href="#" class="dropdown-toggle">Admin</a>
