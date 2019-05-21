15b8c54e01a476e1dbc60ec67139fe6b5c913847
Peter Goodhall <peter@magicbug.co.uk>
2019-05-21 14:00:39 +0100

    Set favicon

--- application/views/interface_assets/header.php
+++ application/views/interface_assets/header.php
@@ -16,6 +16,8 @@
     <!-- Maps -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/js/leaflet/leaflet.css" />
 
+    <link rel="icon" href="<?php echo base_url(); ?>/favicon.ico">
+
     <title><?php if(isset($page_title)) { echo $page_title; } ?> - Cloudlog</title>
   </head>
   <body>
