0eb4d892fd9a7e44a351f18c67e3b4f72f7b5b52
Kim Huebel <dg9vh@darc.de>
2019-06-26 10:17:06 +0200

    Added new Awards - DOK This gives us a list of worked DOKs (counted only, if DXCC is 230 = Germany)

---
+++ application/views/awards/dok/index.php
@@ -0,0 +1,66 @@
+
+<div class="container">
+	<h1><?php echo $page_title; ?></h1>
+
+	<!-- Sub Nav for Awards -->
+	
+    <?php $this->load->view("awards/nav_bar")?>
+    
+	<table class="table table-striped table-hover">
+	<thead>
+        <tr>
+        <td style="width:225px">DOKs (<?php echo count($doks)?>)</td>
+    <?php
+	foreach ($worked_bands as $slot) {
+		echo "          <td>$slot</td>\n";
+	}
+    ?>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+        foreach($doks as $dok=>$val){
+            print("<tr><td>$dok</td>");
+            foreach($val as $band=>$count){
+		if (in_array($band, $worked_bands)) {
+	                if ($count == 0){
+	      	             print("<td>&nbsp;</td>");
+	                }else{
+	                    printf("<td><a href='dok_details?Dok=\"%s\"&Band=\"%s\"'>%d</a></td>", str_replace("&", "%26", $dok), $band, $count);
+	                }
+		} 
+            }
+            print("</tr>");
+        }
+
+    ?>
+    </tbody>
+
+	</table>
+	<style>
+        #table-fixed{
+            position: fixed;
+            top: 40px;
+            display: none;
+            background-color: white;
+            border: 1px solid black;
+        }
+	</style>
+	<script>
+	var tableOffset = $(".zebra-striped").offset().top-40;
+	$('#table-fixed').width($(".zebra-striped").width());
+	var $header = $(".zebra-striped > thead").clone();
+	var $fixedHeader = $("#table-fixed").append($header);
+
+	$(window).bind("scroll", function() {
+	    var offset = $(this).scrollTop();
+
+        if (offset >= tableOffset && $fixedHeader.is(":hidden")) {
+	        $fixedHeader.show();
+	    }
+	    else if (offset < tableOffset) {
+	        $fixedHeader.hide();
+	    }
+	});
+	</script>
+</div>
