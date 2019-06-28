14a331b942a3c25db82c16859b32207dea8d673b
Peter Goodhall <peter@magicbug.co.uk>
2019-06-28 17:23:37 +0100

    Update index.php

--- application/views/gridsquares/index.php
+++ application/views/gridsquares/index.php
@@ -10,4 +10,19 @@
 		<?php } ?>
 
 	<div id="map" style="width: 100%; height: 800px"></div>
+
+	<?php if ($this->uri->segment(2) == "satellites") { ?>
+		<div class="alert alert-success" role="alert">
+			Confirmed is Green | Worked but not confirmed is Red
+			[This grid square map is publically viewable for sharing]
+		</div>
+	<?php } ?>
+
+	<?php if ($this->uri->segment(2) == "band") { ?>
+		<div class="alert alert-success" role="alert">
+			Confirmed is Green | Worked but not confirmed is Red <br>
+			[This map does not include satellite, internet or repeater QSOs] <br>
+			[This grid square map is publically viewable for sharing]
+		</div>
+	<?php } ?>
 </div>
