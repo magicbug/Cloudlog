09a46b2b66ffde8f2b4aeda0fa66e9cbd8870dc5
Peter Goodhall <peter@magicbug.co.uk>
2019-08-30 21:14:50 +0100

    Fixes #338 Respects the Ignore flag when exporting to eQSL

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -923,10 +923,10 @@ class Logbook_model extends CI_Model {
 
     // Show all QSOs we need to send to eQSL
     function eqsl_not_yet_sent() {
-      //$this->db->select("COL_PRIMARY_KEY, DATE_FORMAT(COL_TIME_ON,\'%Y%m%d\') AS COL_QSO_DATE, DATE_FORMAT(COL_TIME_ON,\'%H%i\') AS TIME_ON, COL_CALL, COL_MODE, COL_BAND");
       $this->db->select("COL_PRIMARY_KEY, COL_TIME_ON, COL_CALL, COL_MODE, COL_BAND, COL_COMMENT, COL_RST_SENT, COL_PROP_MODE");
-      $this->db->where('COL_EQSL_QSL_SENT  !=', 'Y');
+      $this->db->where('COL_EQSL_QSL_SENT !=', 'Y');
       $this->db->or_where('COL_EQSL_QSL_SENT IS NULL');
+      $this->db->or_where('COL_EQSL_QSL_SENT !=', 'I');
 
       return $this->db->get($this->config->item('table_name'));
     }
