1394b255d50daa2cb20234f762621b68cf836129
Peter Goodhall <peter@magicbug.co.uk>
2019-09-04 23:00:09 +0100

    html clean up

--- application/views/update/index.php
+++ application/views/update/index.php
@@ -1,32 +1,29 @@
 <div class="container">
 	<h2><?php echo $page_title; ?></h2>
 
+	<?php if(!extension_loaded('xml')) { ?>
 
-<?php if(!extension_loaded('xml')) { ?>
+	<div class="alert alert-danger" role="alert">
+	  You must install php-xml for this to work.
+	</div>
 
-<div class="alert alert-danger" role="alert">
-  You must install php-xml for this to work.
-</div>
+	<?php } else { ?>
 
-<?php } else { ?>
+		<input type="submit" id="btn_update_dxcc" value="Update Dxcc" />
 
-    <input type="submit" id="btn_update_dxcc" value="Update Dxcc" />
+		<div id="dxcc_update_status">Status:</br></div>
+			
+		<br/>
+		    
+		<a href="<?php echo site_url('update/check_missing_dxcc');?>">Check missing DXCC/Countries values</a>
+		<a href="<?php echo site_url('update/check_missing_dxcc/all');?>">[Re-Check ALL]</a>
 
-    <div id="dxcc_update_status">
-    Status:</br>
-    </div>
-<br/>
-    <a href="<?php echo site_url('update/check_missing_dxcc');?>">Check missing DXCC/Countries values</a>
-    <a href="<?php echo site_url('update/check_missing_dxcc/all');?>">[Re-Check ALL]</a>
-</div>
-
-<style>
-#dxcc_update_status{
-   display: None;
-}
-
-</style>
-<?php } ?>
+		<style>
+			#dxcc_update_status{
+			   display: None;
+			}
+		</style>
+	<?php } ?>
 </div>
 
 
