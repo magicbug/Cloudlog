5412fcb3ac773b636f7f5cbe043b9858b55a2b9a
Peter Goodhall <peter@magicbug.co.uk>
2019-09-11 17:38:09 +0100

    Added contesting controller the start of #347

---
+++ application/controllers/Contesting.php
@@ -0,0 +1,16 @@
+<?php
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+/*
+	This controller will contain features for contesting
+*/
+
+class Contesting extends CI_Controller {
+
+	public function index()
+    {
+    	echo 'Functions to come';
+	}
+}
