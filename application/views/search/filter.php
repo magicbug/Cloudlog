890c4f713f17ccb14657b98ee7932485954bfbdb
Kim - DG9VH <dg9vh@users.noreply.github.com>
2019-09-22 19:55:30 +0200

    Typo: functons => functions

--- application/views/search/filter.php
+++ application/views/search/filter.php
@@ -34,7 +34,7 @@
 		<button class="btn btn-warning" id="btn-reset">Reset</button>
 		</p>
 
-	    	<span class="badge badge-info">Info</span> You can find out how to use the <a href="https://github.com/magicbug/Cloudlog/wiki/Search----Filter" target="_blank">search filter functons</a> in the wiki.</a>
+	    	<span class="badge badge-info">Info</span> You can find out how to use the <a href="https://github.com/magicbug/Cloudlog/wiki/Search----Filter" target="_blank">search filter functions</a> in the wiki.</a>
 	    </p>
 
 	  </div>
