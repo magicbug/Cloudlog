3449ac627db4a0b45ccdeb91ad41743486fed427
Peter Goodhall <peter@magicbug.co.uk>
2019-09-22 20:57:07 +0100

    Added migration file to create a logbook_id

---
+++ application/migrations/024_add_column_logbookid.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_column_logbookid extends CI_Migration {
+
+        public function up()
+        {
+                 $fields = array(
+                        'logbook_id int(11) DEFAULT NULL',
+                );
+
+                $this->dbforge->add_column($this->config->item('table_name'), $fields);
+        }
+
+        public function down()
+        {
+                echo "Not possible, sorry.";
+        }
+}
