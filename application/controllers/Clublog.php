7f18ffcabf1e62934b5e92eea3aebc639921c4ed
Peter Goodhall <peter@magicbug.co.uk>
2019-09-26 13:19:37 +0100

    Update Clublog.php

--- application/controllers/Clublog.php
+++ application/controllers/Clublog.php
@@ -43,71 +43,73 @@ class Clublog extends CI_Controller {
 				if($station_row->qso_total > 0) {
 					$data['qsos'] = $this->logbook_model->get_clublog_qsos($station_row->station_id);
 
-					$string = $this->load->view('adif/data/clublog', $data, TRUE);
+					if($data['qsos']->num_rows()){
+						$string = $this->load->view('adif/data/clublog', $data, TRUE);
 
-					$ranid = uniqid();
+						$ranid = uniqid();
 
-					if ( ! write_file('uploads/clublog'.$ranid.$station_row->station_id.'.adi', $string)) {
-					     echo 'Unable to write the file - Make the folder Upload folder has write permissions.';
-					}
-					else {
+						if ( ! write_file('uploads/clublog'.$ranid.$station_row->station_id.'.adi', $string)) {
+						     echo 'Unable to write the file - Make the folder Upload folder has write permissions.';
+						}
+						else {
 
-						$file_info = get_file_info('uploads/clublog'.$ranid.$station_row->station_id.'.adi');
+							$file_info = get_file_info('uploads/clublog'.$ranid.$station_row->station_id.'.adi');
 
-						// initialise the curl request
-						$request = curl_init('https://clublog.org/putlogs.php');
+							// initialise the curl request
+							$request = curl_init('https://clublog.org/putlogs.php');
 
-						if($this->config->item('directory') != "") {
-							 $filepath = $_SERVER['DOCUMENT_ROOT']."/".$this->config->item('directory')."/".$file_info['server_path'];
-						} else {
-							 $filepath = $_SERVER['DOCUMENT_ROOT']."/".$file_info['server_path'];
+							if($this->config->item('directory') != "") {
+								 $filepath = $_SERVER['DOCUMENT_ROOT']."/".$this->config->item('directory')."/".$file_info['server_path'];
+							} else {
+								 $filepath = $_SERVER['DOCUMENT_ROOT']."/".$file_info['server_path'];
+							}
+
+							if (function_exists('curl_file_create')) { // php 5.5+
+							  $cFile = curl_file_create($filepath);
+							} else { // 
+							  $cFile = '@' . realpath($filepath);
+							}
+
+							// send a file
+							curl_setopt($request, CURLOPT_POST, true);
+							curl_setopt(
+							    $request,
+							    CURLOPT_POSTFIELDS,
+							    array(
+							      'email' => $clublog_info['user_clublog_name'],
+							      'password' => $clublog_info['user_clublog_password'],
+							      'callsign' => $station_row->station_callsign,
+							      'api' => "a11c3235cd74b88212ce726857056939d52372bd",
+							      'file' => $cFile
+							    ));
+
+							// output the response
+							curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+							$response = curl_exec($request);
+							$info = curl_getinfo($request);
+
+							if(curl_errno($request)) {
+							    echo curl_error($request);
+							}
+							curl_close ($request); 
+
+
+							// If Clublog Accepts mark the QSOs
+							if (preg_match('/\baccepted\b/', $response)) {
+								echo "QSOs uploaded and Logbook QSOs marked as sent to Clublog";
+
+								$this->load->model('clublog_model');
+								$this->clublog_model->mark_qsos_sent($station_row->station_id);
+							} else {
+								echo "Error ".$response;
+							}
 						}
 
-						if (function_exists('curl_file_create')) { // php 5.5+
-						  $cFile = curl_file_create($filepath);
-						} else { // 
-						  $cFile = '@' . realpath($filepath);
-						}
-
-						// send a file
-						curl_setopt($request, CURLOPT_POST, true);
-						curl_setopt(
-						    $request,
-						    CURLOPT_POSTFIELDS,
-						    array(
-						      'email' => $clublog_info['user_clublog_name'],
-						      'password' => $clublog_info['user_clublog_password'],
-						      'callsign' => $station_row->station_callsign,
-						      'api' => "a11c3235cd74b88212ce726857056939d52372bd",
-						      'file' => $cFile
-						    ));
-
-						// output the response
-						curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-						$response = curl_exec($request);
-						$info = curl_getinfo($request);
-
-						if(curl_errno($request)) {
-						    echo curl_error($request);
-						}
-						curl_close ($request); 
-
-
-						// If Clublog Accepts mark the QSOs
-						if (preg_match('/\baccepted\b/', $response)) {
-							echo "QSOs uploaded and Logbook QSOs marked as sent to Clublog";
-
-							$this->load->model('clublog_model');
-							$this->clublog_model->mark_qsos_sent($station_row->station_id);
-						} else {
-							echo "Error ".$response;
-						}
-
+					} else {
+							echo "Nothing awaiting upload to clublog";
 					}
 				}
 			}
-		} else {
-				echo "Nothing awaiting upload to clublog";
 		}
 	}
 
