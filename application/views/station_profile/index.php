01aa885bccadda23af444599cf8e58c3a7738fd2
Peter Goodhall <peter@magicbug.co.uk>
2019-10-03 15:12:19 +0100

    Removed items from Station Profile display table shifting to a details area

--- application/views/station_profile/index.php
+++ application/views/station_profile/index.php
@@ -40,12 +40,9 @@
 					<th scope="col">Country</th>
 					<th scope="col">Gridsquare</th>
 					<th scope="col">City</th>
-					<th scope="col">IOTA</th>
-					<th scope="col">SOTA</th>
-					<th scope="col">CQ</th>
-					<th scope="col">ITU</th>
 					<th scope="col">QSO Count</th>
 					<th></th>
+					<th scope="col"></th>
 					<th scope="col"></th>
 				</tr>
 			</thead>
@@ -57,23 +54,23 @@
 					<td><?php echo $row->station_country;?></td>
 					<td><?php echo $row->station_gridsquare;?></td>
 					<td><?php echo $row->station_city;?></td>	
-					<td><?php echo $row->station_iota;?></td>	
-					<td><?php echo $row->station_sota;?></td>
-					<td><?php echo $row->station_cq;?></td>
-					<td><?php echo $row->station_itu;?></td>
 					<td><?php echo $row->qso_total;?></td>
 					<td>
 						<?php if($row->station_active != 1) { ?>			
-							<a href="<?php echo site_url('station/set_active/').$current_active."/".$row->station_id; ?>" class="btn btn-outline-secondary btn-sm btn-sm" onclick="return confirm('Are you sure you want to make logbook <?php echo $row->station_profile_name; ?> the active logbook?');">Set Active</a>
+							<a href="<?php echo site_url('station/set_active/').$current_active."/".$row->station_id; ?>" class="btn btn-outline-secondary btn-sm" onclick="return confirm('Are you sure you want to make logbook <?php echo $row->station_profile_name; ?> the active logbook?');">Set Active</a>
 						<?php } else { ?>
 							<span class="badge badge-success">Active Logbook</span>
 						<?php } ?>
 
-						<?php if($is_there_qsos_with_no_station_id == 0) { ?>
-							<a href="<?php echo site_url('station/reassign_profile/').$row->station_id; ?>" class="btn btn-outline-secondary btn-sm btn-sm" onclick="return confirm('Are you sure you want to reassign QSOs to the  <?php echo $row->station_profile_name; ?> profile?');">Reassign</a>
+						<?php if($is_there_qsos_with_no_station_id >= 1) { ?>
+							<a href="<?php echo site_url('station/reassign_profile/').$row->station_id; ?>" class="btn btn-outline-secondary btn-sm" onclick="return confirm('Are you sure you want to reassign QSOs to the  <?php echo $row->station_profile_name; ?> profile?');">Reassign</a>
 						<?php } ?>
 					</td>
-					<td><a href="<?php echo site_url('station/delete')."/".$row->station_id; ?>" class="btn btn-danger" onclick="return confirm('Are you sure you want delete QSO <?php echo $row->station_profile_name; ?>?');"><i class="fas fa-trash-alt"></i> Delete</a></td>
+					<td>
+						<a href="<?php echo site_url('station/edit')."/".$row->station_id; ?>" class="btn btn-info btn-sm"><i class="fas fa-edit-alt"></i> Edit</a>
+					</td>
+					<td>
+						<a href="<?php echo site_url('station/delete')."/".$row->station_id; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want delete QSO <?php echo $row->station_profile_name; ?>?');"><i class="fas fa-trash-alt"></i> Delete</a></td>
 				</tr>
 
 				<?php } ?>
