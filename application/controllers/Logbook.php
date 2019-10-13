192a63ad4bb631a44728ca5fe6fe26601191bb00
Kim Huebel <dg9vh@darc.de>
2019-10-13 22:42:41 +0200

    Added indicators, if callsign/dxcc is worked on band and mode
    like the locator-input the callsign-input is colored in red or green
    the country-label is enabled with a label that tells us about worked/not wkd

--- application/controllers/Logbook.php
+++ application/controllers/Logbook.php
@@ -221,6 +221,74 @@ class Logbook extends CI_Controller {
 		return;
 	}
 
+	function jsonlookupdxcc($country, $type, $band, $mode) {
+		
+		$return = [
+			"workedBefore" => false,
+		];
+
+		$CI =& get_instance();
+    	$CI->load->model('Stations');
+    	$station_id = $CI->Stations->find_active();
+
+		if($type == "SAT") {
+			$this->db->where('COL_PROP_MODE', 'SAT'); 
+		} else {
+			$this->db->where('COL_MODE', $mode); 
+			$this->db->where('COL_BAND', $band); 
+			$this->db->where('COL_PROP_MODE !=','SAT');
+
+		}
+
+    	$this->db->where('station_id', $station_id); 
+    	$this->db->where('COL_COUNTRY', urldecode($country)); 
+ 
+		$query = $this->db->get($this->config->item('table_name'), 1, 0);
+		foreach ($query->result() as $workedBeforeRow)
+		{
+			$return['workedBefore'] = true;
+		}
+
+		header('Content-Type: application/json');
+		echo json_encode($return, JSON_PRETTY_PRINT);
+
+		return;
+	}
+
+	function jsonlookupcallsign($callsign, $type, $band, $mode) {
+		
+		$return = [
+			"workedBefore" => false,
+		];
+
+		$CI =& get_instance();
+    	$CI->load->model('Stations');
+    	$station_id = $CI->Stations->find_active();
+
+		if($type == "SAT") {
+			$this->db->where('COL_PROP_MODE', 'SAT'); 
+		} else {
+			$this->db->where('COL_MODE', $mode); 
+			$this->db->where('COL_BAND', $band); 
+			$this->db->where('COL_PROP_MODE !=','SAT');
+
+		}
+
+    	$this->db->where('station_id', $station_id); 
+    	$this->db->where('COL_CALL', strtoupper($callsign)); 
+ 
+		$query = $this->db->get($this->config->item('table_name'), 1, 0);
+		foreach ($query->result() as $workedBeforeRow)
+		{
+			$return['workedBefore'] = true;
+		}
+
+		header('Content-Type: application/json');
+		echo json_encode($return, JSON_PRETTY_PRINT);
+
+		return;
+	}
+
 
 	/* Used to generate maps for displaying on /logbook/ */
 	function qso_map() {
