390a9e5e06676545048a0b09a6c770f16781e3f1
phl0 <florian@florian-wolters.de>
2020-03-05 14:58:41 +0100

    Retain selected checkboxes so that you know what you filtered

--- application/views/awards/dxcc/index.php
+++ application/views/awards/dxcc/index.php
@@ -10,10 +10,10 @@
 
             <!-- Multiple Checkboxes (inline) -->
             <div class="form-group row">
-                <div class="col-md-2 control-label" for="checkboxes">Deleted dxcc</div>
+                <div class="col-md-2 control-label" for="checkboxes">Deleted DXCC</div>
                 <div class="col-md-10">
                     <div class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="includedeleted" id="includedeleted" value="1" checked="1">
+                        <input class="form-check-input" type="checkbox" name="includedeleted" id="includedeleted" value="1" <?php if ($this->input->post('includedeleted') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="includedeleted">Include Deleted</label>
                     </div>
                 </div>
@@ -24,15 +24,15 @@
                 <div class="col-md-2" for="checkboxes">Worked / confirmed</div>
                 <div class="col-md-10">
                     <div class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="worked" id="worked" value="1" checked="1">
+                        <input class="form-check-input" type="checkbox" name="worked" id="worked" value="1" <?php if ($this->input->post('worked') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="worked">Show worked</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="confirmed" id="confirmed" value="1" checked="1">
+                        <input class="form-check-input" type="checkbox" name="confirmed" id="confirmed" value="1" <?php if ($this->input->post('confirmed') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="confirmed">Show confirmed</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="notworked" id="notworked" value="1" checked="1">
+                        <input class="form-check-input" type="checkbox" name="notworked" id="notworked" value="1" <?php if ($this->input->post('notworked') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="notworked">Show not worked</label>
                     </div>
                 </div>
@@ -42,11 +42,11 @@
                 <div class="col-md-2">QSL / LoTW</div>
                 <div class="col-md-10">
                     <div class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="qsl" value="1" checked="1" id="qsl">
+                        <input class="form-check-input" type="checkbox" name="qsl" value="1" id="qsl" <?php if ($this->input->post('qsl') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="qsl">QSL</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="lotw" value="1" checked="1" id="lotw">
+                        <input class="form-check-input" type="checkbox" name="lotw" value="1" id="lotw" <?php if ($this->input->post('lotw') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="lotw">LoTW</label>
                     </div>
                 </div>
@@ -56,31 +56,31 @@
                 <div class="col-md-2">Continents</div>
                 <div class="col-md-10">
                     <div class="form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="Antarctica" id="Antarctica" value="1" checked="1">
+                        <input class="form-check-input" type="checkbox" name="Antarctica" id="Antarctica" value="1" <?php if ($this->input->post('Antarctica') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="Antarctica">Antarctica</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input"  type="checkbox" name="Africa" id="Africa" value="1" checked="1">
+                        <input class="form-check-input"  type="checkbox" name="Africa" id="Africa" value="1" <?php if ($this->input->post('Africa') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="Africa">Africa</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input"  type="checkbox" name="Asia" id="Asia" value="1" checked="1">
+                        <input class="form-check-input"  type="checkbox" name="Asia" id="Asia" value="1" <?php if ($this->input->post('Asia') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="Asia">Asia</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input"  type="checkbox" name="Europe" id="Europe" value="1" checked="1">
+                        <input class="form-check-input"  type="checkbox" name="Europe" id="Europe" value="1" <?php if ($this->input->post('Europe') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="Europe">Europe</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input"  type="checkbox" name="NorthAmerica" id="NorthAmerica" value="1" checked="1">
+                        <input class="form-check-input"  type="checkbox" name="NorthAmerica" id="NorthAmerica" value="1" <?php if ($this->input->post('NorthAmerica') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="NorthAmerica">North America</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input"  type="checkbox" name="SouthAmerica" id="SouthAmerica" value="1" checked="1">
+                        <input class="form-check-input"  type="checkbox" name="SouthAmerica" id="SouthAmerica" value="1" <?php if ($this->input->post('SouthAmerica') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="SouthAmerica">South America</label>
                     </div>
                     <div class="form-check-inline">
-                        <input class="form-check-input"  type="checkbox" name="Oceania" id="Oceania" value="1" checked="1">
+                        <input class="form-check-input"  type="checkbox" name="Oceania" id="Oceania" value="1" <?php if ($this->input->post('Oceania') || $this->input->method() !== 'post') echo ' checked="checked"'; ?> >
                         <label class="form-check-label" for="Oceania">Oceania</label>
                     </div>
                 </div>
@@ -91,9 +91,11 @@
                 <label class="col-md-2 control-label" for="band">Band</label>
                 <div class="col-md-2">
                     <select id="band2" name="band" class="form-control">
-                        <option value="All" selected>Every band</option>
+                        <option value="All" <?php if ($this->input->post('band') == "All" || $this->input->method() !== 'post') echo ' selected'; ?> >Every band</option>
                         <?php foreach($worked_bands as $band) {
-                            echo '<option value="' . $band . '">' . $band . '</option>';
+                        echo '<option value="' . $band . '"';
+                        if ($this->input->post('band') == $band) echo ' selected';
+                        echo '>' . $band . '</option>'."\n";
                         } ?>
                     </select>
                 </div>
