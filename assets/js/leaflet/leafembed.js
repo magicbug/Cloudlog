2addfc985ddeb9bd14a81637d8ba735a83d4dd40
mbridak <michael.bridak@gmail.com>
2020-03-12 19:53:48 -0700

    Add mode markers

--- assets/js/leaflet/leafembed.js
+++ assets/js/leaflet/leafembed.js
@@ -1,9 +1,17 @@
-
-
 var map;
 var ajaxRequest;
 var plotlist;
 var plotlayers=[];
+var amicon;
+var cwicon;
+var fmicon;
+var ft4icon;
+var ft8icon;
+var jt9icon;
+var jt65icon;
+var psk31icon;
+var ssbicon;
+var digicon;
 
 function initmap(ShowGrid = 'No') {
     // set up AJAX request
@@ -15,6 +23,78 @@ function initmap(ShowGrid = 'No') {
     
     // set up the map
     map = new L.Map('map');
+    amicon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-am.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '..assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    cwicon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-cw.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    fmicon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-fm.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    ft4icon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-ft4.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    ft8icon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-ft8.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    jt9icon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-jt9.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    jt65icon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-jt65.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    psk31icon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-psk31.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
+    ssbicon = L.icon({
+        iconUrl: '../assets/js/leaflet/images/icon-ssb.png',
+        iconSize: [25,41],
+        iconAnchor: [12,40],
+        popupAnchor: [0,-41],
+        shadowUrl: '../assets/js/leaflet/images/marker-shadow.png',
+        shadowSize: [41,41]
+    });
 
     // create the tile layer with correct attribution
     var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -60,8 +140,29 @@ function stateChanged() {
             plotlist = plotlist['markers'];
 			removeMarkers();
 			for (i=0;i<plotlist.length;i++) {
+				var icn = plotlist[i]["html"].split(" ")[8];
 				var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lng, true);
-				var plotmark = new L.Marker(plotll);
+				if (icn == 'CW'){
+					var plotmark = new L.Marker(plotll, {icon: cwicon});
+				} else if (icn == 'SSB'){
+					var plotmark = new L.Marker(plotll, {icon: ssbicon});
+				} else if (icn == 'FT4'){
+ 					var plotmark = new L.Marker(plotll, {icon: ft4icon});
+				} else if (icn == 'FT8'){
+ 					var plotmark = new L.Marker(plotll, {icon: ft8icon});
+				} else if (icn == 'AM'){
+ 					var plotmark = new L.Marker(plotll, {icon: amicon});
+				} else if (icn == 'FM'){
+ 					var plotmark = new L.Marker(plotll, {icon: fmicon});
+				} else if (icn == 'JT9'){
+ 					var plotmark = new L.Marker(plotll, {icon: jt9icon});
+				} else if (icn == 'JT65'){
+ 					var plotmark = new L.Marker(plotll, {icon: jt65icon});
+				} else if (icn == 'PSK31'){
+ 					var plotmark = new L.Marker(plotll, {icon: psk31icon});
+				} else {
+					var plotmark = new L.Marker(plotll);
+				}
 				plotmark.data=plotlist[i];
 				map.addLayer(plotmark);
 				plotmark.bindPopup("<h3>"+plotlist[i].label+"</h3>"+plotlist[i].html);
