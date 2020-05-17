dbb722cd5d824512fdf9b683254a3ace511c5e99
Peter Goodhall <peter@magicbug.co.uk>
2020-05-17 12:43:30 +0100

    Added Forum to the menu under profile along with help

--- application/views/interface_assets/header.php
+++ application/views/interface_assets/header.php
@@ -160,6 +160,10 @@
 				<a class="dropdown-item" target="_blank" href="https://github.com/magicbug/Cloudlog/wiki" title="Help"><i class="fas fa-question"></i> Help</a>
 				
 				<div class="dropdown-divider"></div>
+
+				<a class="dropdown-item" target="_blank" href="https://forum.cloudlog.co.uk" title="Forum"><i class="fas fa-question"></i> Forum</a>
+				
+				<div class="dropdown-divider"></div>
 				
 				<a class="dropdown-item" href="<?php echo site_url('user/logout');?>" title="Logout"><i class="fas fa-sign-out-alt"></i> Logout</a>
 			</div>
