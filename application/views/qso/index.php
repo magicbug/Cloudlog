0e15b79f4ee4796e8fd0005d81b1058c8aa054a5
Peter Goodhall <peter@magicbug.co.uk>
2020-06-05 21:50:09 +0100

    Fixes #533 where POST date/time has spaces

--- application/views/qso/index.php
+++ application/views/qso/index.php
@@ -42,28 +42,12 @@
               <div class="form-row">
                 <div class="form-group col-md-6">
                   <label for="start_date">Date</label>
-                  <input type="text" class="form-control form-control-sm input_date" name="start_date" id="start_date" value="
-                  <?php
-                    if (($this->session->userdata('start_date') != NULL && ((time() - $this->session->userdata('time_stamp')) < 24 * 60 * 60))) {
-                        echo $this->session->userdata('start_date');
-                    } else {
-                        echo date('d-m-Y');
-                    }
-                    ?>
-                       " <?php echo ($_GET['manual'] == 0 ? "disabled" : "");  ?> >
+                  <input type="text" class="form-control form-control-sm input_date" name="start_date" id="start_date" value="<?php if (($this->session->userdata('start_date') != NULL && ((time() - $this->session->userdata('time_stamp')) < 24 * 60 * 60))) { echo $this->session->userdata('start_date'); } else { echo date('d-m-Y');}?>" <?php echo ($_GET['manual'] == 0 ? "disabled" : "");  ?> >
                 </div>
 
                 <div class="form-group col-md-6">
                   <label for="start_time">Time</label>
-                  <input type="text" class="form-control form-control-sm input_time" name="start_time" id="start_time" value="
-                  <?php
-                  if (($this->session->userdata('start_time') != NULL && ((time() - $this->session->userdata('time_stamp')) < 24 * 60 * 60))) {
-                      echo $this->session->userdata('start_time');
-                  } else {
-                      echo date('H:i');
-                  }
-                  ?>
-                    " size="7" <?php echo ($_GET['manual'] == 0 ? "disabled" : "");  ?>>
+                  <input type="text" class="form-control form-control-sm input_time" name="start_time" id="start_time" value="<?php if (($this->session->userdata('start_time') != NULL && ((time() - $this->session->userdata('time_stamp')) < 24 * 60 * 60))) { echo $this->session->userdata('start_time'); } else {echo date('H:i'); } ?>" size="7" <?php echo ($_GET['manual'] == 0 ? "disabled" : "");  ?>>
                 </div>
 
                 <?php if ( $_GET['manual'] == 0 ) { ?>
