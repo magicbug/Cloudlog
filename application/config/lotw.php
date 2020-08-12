66f40bc0ebf9b8a54d5c8d65e339d8c1eb5dd901
Peter Goodhall <peter@magicbug.co.uk>
2020-08-13 00:15:23 +0100

    forgot the discription its late.

--- application/config/lotw.php
+++ application/config/lotw.php
@@ -12,8 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Folder location for storing P12 certficiate files on the system
 |--------------------------------------------------------------------------
 | 
-| If you want to show QSO-times (additional to the date) on the dashboard
-| and on QSO-detail-view, set this to TRUE, otherwise set it to FALSE
+| This folder must be outside of your www root for security reasons
 |
 */
 $config['lotw_keys_folder'] = "";
