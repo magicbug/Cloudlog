adc4f896d334b71747b86c9af36a56375aeb0fca
Peter Goodhall <peter@magicbug.co.uk>
2020-08-13 00:14:44 +0100

    Created an LOTW config file.

---
+++ application/config/lotw.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+*
+* Configuration file for all things relating to LOTW
+*
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Folder location for storing P12 certficiate files on the system
+|--------------------------------------------------------------------------
+| 
+| If you want to show QSO-times (additional to the date) on the dashboard
+| and on QSO-detail-view, set this to TRUE, otherwise set it to FALSE
+|
+*/
+$config['lotw_keys_folder'] = "";
+
