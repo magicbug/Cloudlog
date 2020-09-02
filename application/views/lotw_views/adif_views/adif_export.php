19ef98f0321452cfa1c67e82ce0506f1d771488a
Peter Goodhall <peter@magicbug.co.uk>
2020-09-02 13:50:14 +0100

    Added back state and county to lotw

--- application/views/lotw_views/adif_views/adif_export.php
+++ application/views/lotw_views/adif_views/adif_export.php
@@ -26,6 +26,10 @@ $cert2 = str_replace("-----END CERTIFICATE-----", "", $cert1);
 
 <?php if(isset($station_profile->station_iota)) { ?><IOTA:<?php echo strlen($station_profile->station_iota); ?>><?php echo $station_profile->station_iota; ?><?php } ?>
 
+<?php if(isset($station_profile->state) && $station_profile->station_country = "UNITED STATES OF AMERICA") { ?><US_STATE:<?php echo strlen($station_profile->state); ?>><?php echo $station_profile->state; ?><?php } ?>
+
+<?php if(isset($station_profile->station_cnty) && $station_profile->station_country = "UNITED STATES OF AMERICA") { ?><US_COUNTY:<?php echo strlen($station_profile->station_cnty); ?>><?php echo $station_profile->station_cnty; ?><?php } ?>
+
 <eor>
 
 <?php foreach ($qsos->result() as $qso) { ?>
