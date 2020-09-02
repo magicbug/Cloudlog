b128f50cb22f197a02b5e6549634750ffabfff52
Peter Goodhall <peter@magicbug.co.uk>
2020-09-02 13:57:52 +0100

    US County added to the signing if available

--- application/views/lotw_views/adif_views/adif_export.php
+++ application/views/lotw_views/adif_views/adif_export.php
@@ -80,6 +80,10 @@ if($station_profile->station_itu) {
 	$sign_string .= $station_profile->station_itu;
 }
 
+if($station_profile->station_cnty != "" && $station_profile->station_country = "UNITED STATES OF AMERICA") {
+	$sign_string .= strtoupper($station_profile->station_cnty);
+}
+
 if($station_profile->state != "" && $station_profile->station_country = "UNITED STATES OF AMERICA") {
 	$sign_string .= strtoupper($station_profile->state);
 }
