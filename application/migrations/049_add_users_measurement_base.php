ccc8dbbc3c4f3855e33e37576955c0754728dcf4
Peter Goodhall <peter@magicbug.co.uk>
2020-09-15 16:30:16 +0100

    Class type typo in the migration class should be "add_users_measurement_base"

--- application/migrations/049_add_users_measurement_base.php
+++ application/migrations/049_add_users_measurement_base.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_add_stationprofile_measurement_base extends CI_Migration {
+class Migration_add_users_measurement_base extends CI_Migration {
 
     public function up()
     {
