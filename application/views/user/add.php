b5774a76b0f250662451a77324b91e1cf0eac94c
Peter Goodhall <peter@magicbug.co.uk>
2020-09-16 21:00:16 +0100

    Cleaned up the HTML for adding users

--- application/views/user/add.php
+++ application/views/user/add.php
@@ -19,82 +19,81 @@
 
     <form method="post" action="<?php echo site_url('user/add'); ?>" name="users">
 
-		  <div class="form-group">
-		    <label>Username</label>
+		<div class="form-group">
+			<label>Username</label>
 		    <input class="form-control" type="text" name="user_name" value="<?php if(isset($user_name)) { echo $user_name; } ?>" />
-				<?php if(isset($username_error)) { echo "<div class=\"small error\">".$username_error."</div>"; } ?>
-		  </div>
+			<?php if(isset($username_error)) { echo "<div class=\"small error\">".$username_error."</div>"; } ?>
+		</div>
 
-		  <div class="form-group">
-		    <label>User Role</label>
+		<div class="form-group">
+			<label>User Role</label>
 		    <select class="custom-select" name="user_type">
-						<?php
-						
-						$levels = $this->config->item('auth_level');
-						while (list($key, $val) = each($levels)) {
-						?>
-						<option value="<?php echo $key; ?>" <?php if(isset($user_type)) { if($user_type == $key) { echo "selected=\"selected\""; } } ?>><?php echo $val; ?></option>
-						<?php } ?>
-					</select>
+			<?php			
+				$levels = $this->config->item('auth_level');
+				while (list($key, $val) = each($levels)) {
+			?>
+					<option value="<?php echo $key; ?>" <?php if(isset($user_type)) { if($user_type == $key) { echo "selected=\"selected\""; } } ?>><?php echo $val; ?></option>
+				<?php } ?>
+			</select>
 		  </div>
 
-		  <div class="form-group">
-		    <label>Email Address</label>
-		    <input class="form-control" type="text" name="user_email" value="<?php if(isset($user_email)) { echo $user_email; } ?>" />
-				<?php if(isset($email_error)) { echo "<div class=\"small error\">".$email_error."</div>"; } ?>
-		  </div>
+		<div class="form-group">
+			<label>Email Address</label>
+			<input class="form-control" type="text" name="user_email" value="<?php if(isset($user_email)) { echo $user_email; } ?>" />
+			<?php if(isset($email_error)) { echo "<div class=\"small error\">".$email_error."</div>"; } ?>
+		</div>
 
-		  <div class="form-group">
-		    <label>Password</label>
+		<div class="form-group">
+			<label>Password</label>
 		    <input class="form-control" type="password" name="user_password" value="<?php if(isset($user_password)) { echo $user_password; } ?>" />
 				<?php if(isset($password_error)) { echo "<div class=\"small error\">".$password_error."</div>"; } ?>
-		  </div>
+		</div>
 
-		  <div class="form-group">
-		    <label>First Name</label>
+		<div class="form-group">
+			<label>First Name</label>
 		    <input class="form-control" type="text" name="user_firstname" value="<?php if(isset($user_firstname)) { echo $user_firstname; } ?>" />
-				<?php if(isset($firstname_error)) { echo "<div class=\"small error\">".$firstname_error."</div>"; } ?>
-		  </div>
+			<?php if(isset($firstname_error)) { echo "<div class=\"small error\">".$firstname_error."</div>"; } ?>
+		</div>
 
-			<div class="form-group">
+		<div class="form-group">
 		    <label>Last Name</label>
 		    <input class="form-control" type="text" name="user_lastname" value="<?php if(isset($user_lastname)) { echo $user_lastname; } ?>" />
-				<?php if(isset($lastname_error)) { echo "<div class=\"small error\">".$lastname_error."</div>"; } ?>
-		  </div>
+			<?php if(isset($lastname_error)) { echo "<div class=\"small error\">".$lastname_error."</div>"; } ?>
+		</div>
 
-		  <div class="form-group">
+		<div class="form-group">
 		    <label>Callsign</label>
 		    <input class="form-control" type="text" name="user_callsign" value="<?php if(isset($user_callsign)) { echo $user_callsign; } ?>" />
-				<?php if(isset($callsign_error)) { echo "<div class=\"small error\">".$callsign_error."</div>"; } ?>
-		  </div>
+			<?php if(isset($callsign_error)) { echo "<div class=\"small error\">".$callsign_error."</div>"; } ?>
+		</div>
 			
-		  <div class="form-group">
+		<div class="form-group">
 		    <label>Locator</label>
 		    <input class="form-control" type="text" name="user_locator" value="<?php if(isset($user_locator)) { echo $user_locator; } ?>" />
-				<?php if(isset($locator_error)) { echo "<div class=\"small error\">".$locator_error."</div>"; } ?>
-		  </div>
+			<?php if(isset($locator_error)) { echo "<div class=\"small error\">".$locator_error."</div>"; } ?>
+		</div>
 
-		  <div class="form-group">
+		<div class="form-group">
 		    <label>Timezone</label>
 			<?php 
 				if(!isset($user_timezone)) { $user_timezone = 0; }
 				echo form_dropdown('user_timezone', $timezones, $user_timezone); 
 			?>
-		  </div>
+		</div>
 
-
-                                        <div class="form-group">
-                                        <label for="SelectDateFormat">Date Format</label>
-                                                <select name="user_date_format" class="custom-select" id="SelectDateFormat" aria-describedby="SelectDateFormatHelp">
-                                                        <option value="d/m/y"><?php echo date('d/m/y'); ?></option>
-                                                        <option value="d/m/Y"><?php echo date('d/m/Y'); ?></option>
-                                                        <option value="m/d/y"><?php echo date('m/d/y'); ?></option>
-                                                        <option value="m/d/Y"><?php echo date('m/d/Y'); ?></option>
-                                                        <option value="d.m.Y"><?php echo date('d.m.Y'); ?></option>
-                                                        <option value="Y-m-d"><?php echo date('Y-m-d'); ?></option>
-                                                </select>
-                                                <small id="SelectDateFormatHelp" class="form-text text-muted">Select how you would like dates shown when logged into your account.</small>
-                                        </div>
+		<div class="form-group">
+        	<label for="SelectDateFormat">Date Format</label>
+            <select name="user_date_format" class="custom-select" id="SelectDateFormat" aria-describedby="SelectDateFormatHelp">
+            	<option value="d/m/y"><?php echo date('d/m/y'); ?></option>
+            	<option value="d/m/Y"><?php echo date('d/m/Y'); ?></option>
+            	<option value="m/d/y"><?php echo date('m/d/y'); ?></option>
+            	<option value="m/d/Y"><?php echo date('m/d/Y'); ?></option>
+            	<option value="d.m.Y"><?php echo date('d.m.Y'); ?></option>
+            	<option value="Y-m-d"><?php echo date('Y-m-d'); ?></option>
+            </select>
+            
+            <small id="SelectDateFormatHelp" class="form-text text-muted">Select how you would like dates shown when logged into your account.</small>
+        </div>
 
 
         <div class="form-group">
@@ -108,8 +107,8 @@
             <small id="user_measurement_base_Help" class="form-text text-muted">Choose which unit distances will be shown in.</small>
         </div>
 
-		  <input type="hidden" name="id" value="<?php echo $this->uri->segment(3); ?>" />
-			<button type="submit" class="btn btn-primary">Create Account</button>
+		<input type="hidden" name="id" value="<?php echo $this->uri->segment(3); ?>" />
+		<button type="submit" class="btn btn-primary">Create Account</button>
     </form>
   </div>
 </div>
