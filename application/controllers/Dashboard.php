9abc79081d033221b4bb33bc1107c0d9a96b7a12
Peter Goodhall <peter@magicbug.co.uk>
2020-10-12 16:14:28 +0100

    [Dashboard][SetupChecklist] Started code to bring a setup checklist for new installs to cloudlog

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -34,29 +34,40 @@ class Dashboard extends CI_Controller {
  
 		$this->load->model('stations');
 		$data['current_active'] = $this->stations->find_active();
-		 
-		// Store info
-		$data['todays_qsos'] = $this->logbook_model->todays_qsos();
-		$data['total_qsos'] = $this->logbook_model->total_qsos();
-		$data['month_qsos'] = $this->logbook_model->month_qsos();
-		$data['year_qsos'] = $this->logbook_model->year_qsos();
-		
-		$data['total_countrys'] = $this->logbook_model->total_countrys();
-		$data['total_countrys_confirmed_paper'] = $this->logbook_model->total_countrys_confirmed_paper();
-		$data['total_countrys_confirmed_eqsl'] = $this->logbook_model->total_countrys_confirmed_eqsl();
-		$data['total_countrys_confirmed_lotw'] = $this->logbook_model->total_countrys_confirmed_lotw();
-		
-		$data['total_qsl_sent'] = $this->logbook_model->total_qsl_sent();
-		$data['total_qsl_recv'] = $this->logbook_model->total_qsl_recv();
-		$data['total_qsl_requested'] = $this->logbook_model->total_qsl_requested();
-				
-		$data['last_five_qsos'] = $this->logbook_model->get_last_qsos('11');
 
-		$data['page_title'] = "Dashboard";
+		$setup_required = false;
 
-		$this->load->view('interface_assets/header', $data);
-		$this->load->view('dashboard/index');
-		$this->load->view('interface_assets/footer');
+		if($setup_required) {
+			$data['page_title'] = "Cloudlog Setup Checklist";
+
+			$this->load->view('interface_assets/header', $data);
+			$this->load->view('setup/check_list');
+			$this->load->view('interface_assets/footer');
+		} else {
+			// Store info
+			$data['todays_qsos'] = $this->logbook_model->todays_qsos();
+			$data['total_qsos'] = $this->logbook_model->total_qsos();
+			$data['month_qsos'] = $this->logbook_model->month_qsos();
+			$data['year_qsos'] = $this->logbook_model->year_qsos();
+			
+			$data['total_countrys'] = $this->logbook_model->total_countrys();
+			$data['total_countrys_confirmed_paper'] = $this->logbook_model->total_countrys_confirmed_paper();
+			$data['total_countrys_confirmed_eqsl'] = $this->logbook_model->total_countrys_confirmed_eqsl();
+			$data['total_countrys_confirmed_lotw'] = $this->logbook_model->total_countrys_confirmed_lotw();
+			
+			$data['total_qsl_sent'] = $this->logbook_model->total_qsl_sent();
+			$data['total_qsl_recv'] = $this->logbook_model->total_qsl_recv();
+			$data['total_qsl_requested'] = $this->logbook_model->total_qsl_requested();
+					
+			$data['last_five_qsos'] = $this->logbook_model->get_last_qsos('11');
+
+			$data['page_title'] = "Dashboard";
+
+			$this->load->view('interface_assets/header', $data);
+			$this->load->view('dashboard/index');
+			$this->load->view('interface_assets/footer');
+		}
+
 	}
 	
 	function map() {
