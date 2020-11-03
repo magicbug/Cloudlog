f6c8a03c5d174fed38698f80d062903d142b52d9
Peter Goodhall <peter@magicbug.co.uk>
2020-11-03 13:52:14 +0000

    [QSL Card] When selecting file to upload offer file browser or camera.

--- application/views/view_log/qso.php
+++ application/views/view_log/qso.php
@@ -325,12 +325,12 @@
 
                     <div class="form-group">
                         <label for="qslcardfront">Upload QSL Card front image</label>
-                        <input class="form-control-file" type="file" id="qslcardfront" name="qslcardfront" accept="image/*" capture="environment">
+                        <input class="form-control-file" type="file" id="qslcardfront" name="qslcardfront" accept="image/*" >
                     </div>
 
                     <div class="form-group">
                         <label for="qslcardback">Upload QSL card back image</label>
-                        <input class="form-control-file" type="file" id="qslcardback" name="qslcardback" accept="image/*" capture="environment">
+                        <input class="form-control-file" type="file" id="qslcardback" name="qslcardback" accept="image/*">
                     </div>
 
                     <input type="hidden" class="form-control" id="qsoinputid" name="qsoid" value="<?php echo $row->COL_PRIMARY_KEY; ?>">
