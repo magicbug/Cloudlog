5e9542e73f01964edaf7aa603061b1435664b0fd
Adam <adam@ofadam.com>
2020-11-15 20:20:35 -0600

    Change to show submode if available - otherwise mode
    This change matches the initial dashboard view.

--- application/views/view_log/partial/log_ajax.php
+++ application/views/view_log/partial/log_ajax.php
@@ -43,7 +43,7 @@
             <td>
                 <a id="edit_qso" href="javascript:displayQso(<?php echo $row->COL_PRIMARY_KEY; ?>)"><?php echo str_replace("0","&Oslash;",strtoupper($row->COL_CALL)); ?></a>
             </td>
-            <td><?php echo $row->COL_MODE; ?></td>
+            <td><?php echo $row->COL_SUBMODE==null?$row->COL_MODE:$row->COL_SUBMODE; ?></td>
             <td><?php echo $row->COL_RST_SENT; ?> <?php if ($row->COL_STX) { ?><span class="badge badge-light"><?php echo $row->COL_STX;?></span><?php } ?><?php if ($row->COL_STX_STRING) { ?><span class="badge badge-light"><?php echo $row->COL_STX_STRING;?></span><?php } ?></td>
             <td><?php echo $row->COL_RST_RCVD; ?> <?php if ($row->COL_SRX) { ?><span class="badge badge-light"><?php echo $row->COL_SRX;?></span><?php } ?><?php if ($row->COL_SRX_STRING) { ?><span class="badge badge-light"><?php echo $row->COL_SRX_STRING;?></span><?php } ?></td>
             <?php if($row->COL_SAT_NAME != null) { ?>
