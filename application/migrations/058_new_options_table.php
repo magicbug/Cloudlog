be6550723c8c01d862b35eff38f3e3adb6e43746
Peter Goodhall <peter@magicbug.co.uk>
2020-12-10 17:45:44 +0000

    [Global Options] Created Migration for table "options" and added theme and default
    This will be for global variables, anything that needs to stick for every user, default theme, dates etc etc etc

---
+++ application/migrations/058_new_options_table.php
@@ -0,0 +1,49 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+*   This migration creates a table called options which will hold global options needed within cloudlog
+*   removing the need for lots of configuration files.
+*/
+
+class Migration_new_options_table extends CI_Migration {
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'option_id' => array(
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            
+            'option_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '191',
+                'null' => TRUE,
+                'unique' => TRUE,
+            ),
+            
+            'option_value' => array(
+                'type' => 'longtext',
+            ),
+
+            'autoload' => array(
+                'type' => 'varchar',
+                'constraint' => '20',
+                'null' => TRUE,
+            )
+        ));
+
+        $this->dbforge->add_key('option_id', TRUE);
+
+        $this->dbforge->create_table('options');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('options');
+    }
+}
