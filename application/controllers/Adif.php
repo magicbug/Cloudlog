ae1c82815f5c8889ce3810a8e76a9bed4a6ff508
Andreas <andreask@online.no>
2020-12-11 10:49:33 +0100

    [ADIF import] Skip QRZ Logbook upload when importing ADIF from import, and LoTW.

--- application/controllers/Adif.php
+++ application/controllers/Adif.php
@@ -221,7 +221,7 @@ class adif extends CI_Controller {
 
 
 				$custom_errors .= $this->logbook_model->import($record, $this->input->post('station_profile'),
-					$this->input->post('skipDuplicate'), $this->input->post('markLotw'), $this->input->post('dxccAdif'), $this->input->post('markQrz'));
+					$this->input->post('skipDuplicate'), $this->input->post('markLotw'), $this->input->post('dxccAdif'), $this->input->post('markQrz'), true);
 
 			};
 
