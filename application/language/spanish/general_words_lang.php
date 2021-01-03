a7043157dd87e1c949486de210f5943aa7317d9f
Leroy Diaz <leroydiazg@gmail.com>
2021-01-03 16:10:10 +0000

    General words translation to Spanish language.

---
+++ application/language/spanish/general_words_lang.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['error_no_active_station_profile'] = 'Attention: you need to set an active station profile.';
+
+$lang['notice_turn_the_radio_on'] = 'No has realizado ninguna QSO hoy...¡Hora de encender la radio!';
+
+$lang['general_word_important'] = 'Importante';
+
+$lang['general_word_date'] = 'Fecha';
+$lang['general_word_time'] = 'Hora';
+
+$lang['general_word_total'] = 'Total';
+$lang['general_word_year'] = 'Año';
+$lang['general_word_month'] = 'Mes';
+
+$lang['general_word_worked'] = '.Trabajado';
+$lang['general_word_confirmed'] = 'Confirmado';
+$lang['general_word_needed'] = '.Necesitado';
+
+$lang['general_word_sent'] = 'Enviado';
+$lang['general_word_received'] = 'Recibido';
+$lang['general_word_requested'] = 'Solicitadas';
+$lang['general_word_qslcards'] = 'Tarjetas QSL';
+
+// ham radio terms
+$lang['gen_hamradio_call'] = 'Llamada';
+$lang['gen_hamradio_callsign'] = 'Indicativo';
+$lang['gen_hamradio_mode'] = 'Modo';
+$lang['gen_hamradio_rst_sent'] = 'Enviado';
+$lang['gen_hamradio_rst_recv'] = 'Recibido';
+$lang['gen_hamradio_band'] = 'Banda';
+
+// Dashboard Words
+$lang['dashboard_you_have_had'] = '¡Has tenido';
+$lang['dashboard_qsos_today'] = 'QSOs hoy!';
+$lang['dashboard_qso_breakdown'] = 'Listado de QSOs';
+$lang['dashboard_countries_breakdown'] = 'Listado de países';
