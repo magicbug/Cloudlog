b54e80b9bc358adfcc9760c7481dcf5ceb730dd1
Leroy Diaz <leroydiazg@gmail.com>
2021-01-03 20:02:17 +0000

    Add Spanish Notes translations

---
+++ application/language/spanish/notes_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['notes'] = 'Notas';
+$lang['notes_note'] = 'Nota';
+$lang['notes_create'] = 'Crear nota';
+$lang['notes_heading'] = 'Sus notas';
+$lang['notes_none_yet'] = <<<EOD
+Actualmente no tiene notas, las cuales son un fantástico recurso para guardar
+datos e información general. Además, son mejores que el papel ya que no puedes
+perderlas :)
+EOD;
+
+$lang['notes_info_message'] = <<<EOD
+El contenido de la nota será usado únicamente en Cloudlog y no será exportado a
+otros servicios.
+EOD;
+$lang['notes_title'] = 'Título';
+$lang['notes_category'] = 'Categoría';
+$lang['notes_category_general'] = 'General';
+$lang['notes_category_antennas'] = 'Antenas';
+$lang['notes_category_satellites'] = 'Satélites';
+$lang['notes_content'] = 'Cuerpo';
+$lang['notes_save'] = 'Guardar nota';
+$lang['notes_edit'] = 'Editar nota';
+$lang['notes_delete'] = 'Eliminar nota';
