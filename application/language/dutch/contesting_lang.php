379fadc043f6c8319cd0013efa4b66de5535c73f
keptenkurk <paulmerkx@gmail.com>
2021-01-08 08:35:27 +0100

    Add code igniter languages

---
+++ application/language/dutch/contesting_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('Directe toegang tot de scripts is niet toegestaan');
+
+$lang['contesting_page_title'] = 'Contest Logging';
+$lang['contesting_button_reset_contest_session'] = 'Herstel Contest Sessie';
+
+$lang['contesting_exchange_type'] = 'Soort uitwisseling';
+$lang['contesting_exchange_type_serial'] = 'Volgnummer';
+$lang['contesting_exchange_type_other'] = 'Anders';
+
+$lang['contesting_contest_name'] = 'Contest Naam';
+
+$lang['contesting_btn_reset_qso'] = 'Wis QSO';
+$lang['contesting_btn_save_qso'] = 'Bewaar QSO';
+
+$lang['contesting_title_callsign_suggestions'] = 'Roepnaam suggesties';
+$lang['contesting_title_contest_logbook'] = 'Contest Logboek';
---
+++ application/language/dutch/qso_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('Directe toegang tot de scripts is niet toegestaan');
+
+// Tiles
+$lang['qso_title_qso_map'] = 'QSO Kaart';
+$lang['qso_title_suggestions'] = 'Suggesties';
+$lang['qso_title_pervious_contacts'] = 'Eerdere verbindingen';
+
+// Input Help Text on the /QSO Display
+$lang['qso_transmit_power_helptext'] = 'Geef vermogen in Watt. Vermeld alleen cijfers.';
+
+$lang['qso_sota_ref_helptext'] = 'Bijvoorbeeld: GM/NS-001.';
+
+$lang['qso_sig_helptext'] = 'Bijvoorbeeld: WWFF of POTA';
+$lang['qso_sig_info_helptext'] = 'Bijvoorbeeld: DLFF-0029';
+
+$lang['qso_dok_helptext'] = 'Bijvoorbeeld: Q03';
+
+$lang['qso_notes_helptext'] = 'Notities worden alleen in Cloudlog gebruikt en worden niet geexporteerd naar andere diensten.';
+
+// Button Text on /qso Display
+
+$lang['qso_btn_reset_qso'] = 'Wis QSO';
+$lang['qso_btn_save_qso'] = 'Bewaar QSO';
