55ce77664e0424416d3e04a3e8b0d1df88fa4208
Peter Goodhall <magicbug@users.noreply.github.com>
2021-01-08 22:25:28 +0000

    Station Profile hide county till it's populated via a list

--- application/views/station_profile/create.php
+++ application/views/station_profile/create.php
@@ -117,7 +117,7 @@
 		    <small id="StateHelp" class="form-text text-muted">Station state. Applies to certain countries only. Leave blank if not applicable.</small>
 		  </div>
 
-		  <div class="form-group">
+		  <div style="display: none" class="form-group">
 		    <label for="stationCntyInput">Station County</label>
 		    <input type="text" class="form-control" name="station_cnty" id="stationCntyInput" aria-describedby="stationCntyInputHelp">
 		    <small id="stationCntyInputHelp" class="form-text text-muted">Station County (Only used for USA/Alaska/Hawaii)</small>
