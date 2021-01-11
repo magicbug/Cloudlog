b4ad80b244a655aa7d669aa068243a53e7723a93
Florian Wolters <github@florian-wolters.de>
2021-01-11 22:21:05 +0100

    Add more German translations

---
+++ application/language/german/notes_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('BASEPATH') OR exit('Direkter Zugriff auf Skripte ist nicht erlaubt');
+
+$lang['notes_menu_notes'] = 'Notizen';
+$lang['notes_edit_note'] = 'Editiere Notiz';
+$lang['notes_your_notes'] = 'Deine Notizen';
+
+$lang['notes_welcome'] = "Du hast derzeit keine Notizen. Diese sind eine fantastische Methode, Informationen wie Tunereinstellungen, Baken und allgemeine Stationsinformationen zu speichern. Und sie sind besser als Papier, da du sie nicht verlieren kannst!";
+
+$lang['notes_create_note'] = 'Lege Notiz an';
+
+$lang['notes_input_title'] = 'Titel';
+$lang['notes_input_category'] = 'Kategorie';
+$lang['notes_input_notes_content'] = 'Notizeninhalt';
+$lang['notes_input_btn_save_note'] = 'Speichere Notiz';
+$lang['notes_input_btn_edit_note'] = 'Editiere Notiz';
+$lang['notes_input_btn_delete_note'] = 'Lösche Notiz';
+
+$lang['notes_selection_general'] = 'Allgemein';
+$lang['notes_selection_antennas'] = 'Antennen';
+$lang['notes_selection_satellites'] = 'Satelliten';
+
---
+++ application/language/german/qso_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('Direkter Zugriff auf Skripte ist nicht erlaubt');
+
+// Tiles
+$lang['qso_title_qso_map'] = 'QSO Karte';
+$lang['qso_title_suggestions'] = 'Vorschläge';
+$lang['qso_title_pervious_contacts'] = 'Vorherige Kontakte';
+
+// Input Help Text on the /QSO Display
+$lang['qso_transmit_power_helptext'] = 'Gib die Ausgangsleistung in Watt an. Erfasse nur Zahlen bei der Eingabe.';
+
+$lang['qso_sota_ref_helptext'] = 'Zum Beispiel: GM/NS-001.';
+
+$lang['qso_sig_helptext'] = 'Zum Beispiel: WWFF or POTA';
+$lang['qso_sig_info_helptext'] = 'Zum Beispiel: DLFF-0029';
+
+$lang['qso_dok_helptext'] = 'Zum Beispiel: Q03';
+
+$lang['qso_notes_helptext'] = 'Notizeninhalt wird nur innerhalb von Cloudlog genutzt und nicht an andere Dienste weitergegeben.';
+
+// Button Text on /qso Display
+
+$lang['qso_btn_reset_qso'] = 'Reset';
+$lang['qso_btn_save_qso'] = 'Speichere QSO';
