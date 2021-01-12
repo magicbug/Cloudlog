25f08b79080dd9c0e18b7f5813deefbccbb44dcd
o1hjw <77230101+o1hjw@users.noreply.github.com>
2021-01-12 23:29:01 +0200

    Create qso_lang.php

---
+++ application/language/finnish/qso_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Tiles
+$lang['qso_title_qso_map'] = 'QSO-kartta';
+$lang['qso_title_suggestions'] = 'Ehdotukset';
+$lang['qso_title_pervious_contacts'] = 'Aikaisemmat yhteydet';
+
+// Input Help Text on the /QSO Display
+$lang['qso_transmit_power_helptext'] = 'Anna teholukema Watteina käyttäen vain numeroita.';
+
+$lang['qso_sota_ref_helptext'] = 'Esimerkiksi: OH/JS-004.';
+
+$lang['qso_sig_helptext'] = 'Esimerkiksi: WWFF tai POTA';
+$lang['qso_sig_info_helptext'] = 'Esimerkiksi: OHFF-0029';
+
+$lang['qso_dok_helptext'] = 'Esimerkiksi: Q03';
+
+$lang['qso_notes_helptext'] = 'Muistion sisältö tallentuu vain Cloudlogiin, tämän kuson yhteyteen, eikä siirry eteenpäin muihin palveluihin.';
+
+// Button Text on /qso Display
+
+$lang['qso_btn_reset_qso'] = 'Tyhjennä';
+$lang['qso_btn_save_qso'] = 'Tallenna QSO';
