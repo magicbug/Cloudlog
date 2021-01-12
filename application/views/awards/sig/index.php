b66a90f13c5c926b59b753b47694cb8a0b844a79
Daniel Clerc <mail@clerc.eu>
2021-01-12 21:36:17 +0100

    add scaffolding view for sig award

--- application/views/awards/sig/index.php
+++ application/views/awards/sig/index.php
@@ -1,6 +1,37 @@
 <div class="container">
-	<h2><?php echo $page_title; ?></h2>
-
-    <!-- Why don't you show up!?! -->
+<h2><?php echo $page_title; ?></h2>
+	
+	<table class="table table-sm table-striped table-hover">
+		
+	<tr>
+		<td>Reference</td>
+		<td>Date/Time</td>
+		<td>Callsign</td>
+		<td>Band</td>
+		<td>RST Sent</td>
+		<td>RST Received</td>
+	</tr>
+	
+	<?php
+		if ($sig_all->num_rows() > 0) {
+			foreach ($sig_all->result() as $row) {
+	?>
+	
+	<tr>
+		<td>	
+			<?php echo $row->COL_SIG_INFO; ?>
+		</td>
+		<td><?php $timestamp = strtotime($row->COL_TIME_ON); echo date('d/m/y', $timestamp); ?> - <?php $timestamp = strtotime($row->COL_TIME_ON); echo date('H:i', $timestamp); ?></td>
+		<td><?php echo $row->COL_CALL; ?></td>
+		<td><?php echo $row->COL_BAND; ?></td>
+		<td><?php echo $row->COL_RST_SENT; ?></td>
+		<td><?php echo $row->COL_RST_RCVD; ?></td>
+	</tr>
+	<?php
+		  }
+		}
+	?>
+	
+	</table>
 
 </div>
