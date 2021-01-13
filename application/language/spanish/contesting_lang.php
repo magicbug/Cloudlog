39641ce27890a76eb47d0481d4fe01fdc9ae6f60
Leroy Diaz <leroydiazg@gmail.com>
2021-01-13 20:36:56 +0000

    Translate contesting lang file to Spanish

---
+++ application/language/spanish/contesting_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('Acceso directo a los scripts restringido');
+
+$lang['contesting_page_title'] = 'Registro de concurso';
+$lang['contesting_button_reset_contest_session'] = 'Resetar la sesión del concurso';
+
+$lang['contesting_exchange_type'] = 'Tipo de intercambio';
+$lang['contesting_exchange_type_serial'] = 'Serial';
+$lang['contesting_exchange_type_other'] = 'Other';
+
+$lang['contesting_contest_name'] = 'Nombre del concurso';
+
+$lang['contesting_btn_reset_qso'] = 'Resetear QSO';
+$lang['contesting_btn_save_qso'] = 'Guardar QSO';
+
+$lang['contesting_title_callsign_suggestions'] = 'Indicativos sugeridos';
+$lang['contesting_title_contest_logbook'] = 'Logbook del concurso';
