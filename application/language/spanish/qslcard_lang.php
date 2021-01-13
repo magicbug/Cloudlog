d65774c6ad0267212938cb45fddf931cb1c2bcc6
Leroy Diaz <leroydiazg@gmail.com>
2021-01-13 20:35:23 +0000

    Translate QSL lang file to Spanish

---
+++ application/language/spanish/qslcard_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('BASEPATH') OR exit('Acceso directo a los scripts restringido');
+
+// Tiles
+$lang['qslcard_string_your_are_using'] = 'Está usando';
+$lang['qslcard_string_disk_space'] = 'de espacio en disco para almacenar recursos relacionados con las tarjetas QSL';
