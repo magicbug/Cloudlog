67ba28d14527a2fd80d0f084871e5adb2385208e
Peter Goodhall <peter@magicbug.co.uk>
2021-01-31 13:24:25 +0000

    [Migration] 061 adds language to the options table default to english

--- application/config/migration.php
+++ application/config/migration.php
@@ -21,7 +21,7 @@ $config['migration_enabled'] = TRUE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 60;
+$config['migration_version'] = 61;
 
 /*
 |--------------------------------------------------------------------------
