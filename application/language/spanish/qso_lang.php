c299ac4e7ba81a8bc5dca700b6e59e882fb8bc3d
Leroy Diaz <leroydiazg@gmail.com>
2021-02-08 21:20:49 +0000

    Add Spanish translations for QSO Details lightbox

--- application/language/spanish/qso_lang.php
+++ application/language/spanish/qso_lang.php
@@ -23,3 +23,7 @@ $lang['qso_notes_helptext'] = 'El contenido es usado solo dentro de Cloudlog y n
 
 $lang['qso_btn_reset_qso'] = 'Resetear';
 $lang['qso_btn_save_qso'] = 'Guardar QSO';
+
+// QSO Details
+
+$lang['qso_details'] = 'Detalles de QSO';
