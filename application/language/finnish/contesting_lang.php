8adefd8512944da42af65f0560262597a0b87c5a
oh1jw <77230101+oh1jw@users.noreply.github.com>
2021-03-01 00:26:39 +0200

    Update contesting_lang.php

--- application/language/finnish/contesting_lang.php
+++ application/language/finnish/contesting_lang.php
@@ -5,14 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['contesting_page_title'] = 'Kilpailuloki';
 $lang['contesting_button_reset_contest_session'] = 'Nollaa kilpailuloki';
 
-$lang['contesting_exchange_type'] = 'Exchange Type';
-$lang['contesting_exchange_type_serial'] = 'Sarja / Serial';
-$lang['contesting_exchange_type_other'] = 'Muu / Other';
+$lang['contesting_exchange_type'] = 'Annettava sanoma';
+$lang['contesting_exchange_type_serial'] = 'Sarjanumero (QSO:n numero)';
+$lang['contesting_exchange_type_other'] = 'Muu';
 
 $lang['contesting_contest_name'] = 'Kilpailun nimi';
 
 $lang['contesting_btn_reset_qso'] = 'Nollaa QSO';
-$lang['contesting_btn_save_qso'] = 'Tallenna QSO';
+$lang['contesting_btn_save_qso'] = 'TALLENNA QSO';
 
 $lang['contesting_title_callsign_suggestions'] = 'Ehdotettu kutsu';
-$lang['contesting_title_contest_logbook'] = 'Kilpailun loki';
+$lang['contesting_title_contest_logbook'] = 'Kilpailussa pidetyt yhteydet';
