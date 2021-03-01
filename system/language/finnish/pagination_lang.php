896b69fd71f104a000ed2215c0ea6219db565f15
oh1jw <77230101+oh1jw@users.noreply.github.com>
2021-03-01 10:17:37 +0200

    Update pagination_lang.php

--- system/language/finnish/pagination_lang.php
+++ system/language/finnish/pagination_lang.php
@@ -40,4 +40,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['pagination_first_link'] = '&lsaquo; ensimmäinen';
 $lang['pagination_next_link'] = '&gt;';
 $lang['pagination_prev_link'] = '&lt;';
-$lang['pagination_last_link'] = 'Viiminen &rsaquo;';
+$lang['pagination_last_link'] = 'Viimeinen &rsaquo;';
