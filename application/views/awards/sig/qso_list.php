0dca869cb37347293688a0b344379e0a7a5d8b2f
oh1jw <77230101+oh1jw@users.noreply.github.com>
2021-03-21 12:49:50 +0200

    MODE column to SIG award
    Added MODE column to SIG award search results. Sometimes different modes are treated as different QSOs.

--- application/views/awards/sig/qso_list.php
+++ application/views/awards/sig/qso_list.php
@@ -9,6 +9,7 @@
             <td>Reference</td>
             <td>Date/Time</td>
             <td>Callsign</td>
+            <td>Mode</td>
             <td>Band</td>
             <td>RST Sent</td>
             <td>RST Received</td>
@@ -20,6 +21,7 @@
                 </td>
                 <td><?php $timestamp = strtotime($row->COL_TIME_ON); echo date('d/m/y', $timestamp); ?> - <?php $timestamp = strtotime($row->COL_TIME_ON); echo date('H:i', $timestamp); ?></td>
                 <td><?php echo $row->COL_CALL; ?></td>
+                <td><?php echo $row->COL_MODE; ?></td>
                 <td><?php echo $row->COL_BAND; ?></td>
                 <td><?php echo $row->COL_RST_SENT; ?></td>
                 <td><?php echo $row->COL_RST_RCVD; ?></td>
