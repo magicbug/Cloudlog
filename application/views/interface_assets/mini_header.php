7ed5a89253b7301ad1559d69241cbf3fac7795ad
abarrau <hesky378@gmail.com>
2021-04-10 05:21:22 +0200

    load custom.css from assets path

--- application/views/interface_assets/mini_header.php
+++ application/views/interface_assets/mini_header.php
@@ -18,7 +18,7 @@
     <!-- Maps -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/js/leaflet/leaflet.css" />
 
-	<?php if (file_exists(APPPATH.'third_party/assets/css/custom.css')) { echo '<link rel="stylesheet" href="'.base_url().'third_party/assets/css/custom.css">'; } ?>
+	<?php if (file_exists(APPPATH.'../assets/css/custom.css')) { echo '<link rel="stylesheet" href="'.base_url().'assets/css/custom.css">'; } ?>
 
     <link rel="icon" href="<?php echo base_url(); ?>favicon.ico">
 
