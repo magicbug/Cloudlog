ded6a142caaa5772a55d47987db05fa8cd534a64
Leroy Diaz <leroydiazg@gmail.com>
2021-05-17 18:54:56 +0100

    Translate account_lang to Spanish

---
+++ application/language/spanish/account_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['account_logbook_fields'] = 'Campos a mostrar en el Logbook';
+$lang['account_column1_text'] = 'Columna 1';
+$lang['account_column2_text'] = 'Columna 2';
+$lang['account_column3_text'] = 'Columna 3';
+$lang['account_column4_text'] = 'Columna 4';
+$lang['account_column5_text'] = 'Columna 5 (solo en logbook)';
