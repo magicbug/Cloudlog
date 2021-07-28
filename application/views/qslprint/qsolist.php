f96b608b52b172103d95930db46e4b23e682050e
Andreas <6977712+AndreasK79@users.noreply.github.com>
2021-07-28 11:10:15 +0200

    [QSL Print] Added button to see list of QSOs with callsign. Can add them to print queue from this list.

--- application/views/qslprint/qsolist.php
+++ application/views/qslprint/qsolist.php
@@ -1,6 +1,6 @@
 <?php
 if ($qsos->result() != NULL) {
-	echo '<table style="width:100%" class="table table-sm table-bordered table-hover table-striped table-condensed">
+	echo '<table style="width:100%" class="qsolist table-sm table-bordered table-hover table-striped table-condensed">
 	<thead>
 	<tr>
 	<th style=\'text-align: center\'>'.$this->lang->line('gen_hamradio_callsign').'</th>
@@ -9,7 +9,6 @@ if ($qsos->result() != NULL) {
 	<th style=\'text-align: center\'>' . $this->lang->line('gen_hamradio_mode') . '</th>
 	<th style=\'text-align: center\'>' . $this->lang->line('gen_hamradio_band') . '</th>
 	<th style=\'text-align: center\'>' . $this->lang->line('gen_hamradio_station') . '</th>
-	<th style=\'text-align: center\'></th>
 	<th style=\'text-align: center\'></th>
 	</tr>
 	</thead><tbody>';
@@ -24,29 +23,22 @@ if ($qsos->result() != NULL) {
 	}
 
 	foreach ($qsos->result() as $qsl) {
-		echo '<tr id="qslprint_'.$qsl->COL_PRIMARY_KEY.'">';
+		echo '<tr id ="qsolist_'.$qsl->COL_PRIMARY_KEY.'">';
 		echo '<td style=\'text-align: center\'>' . $qsl->COL_CALL . '</td>';
 		echo '<td style=\'text-align: center\'>'; $timestamp = strtotime($qsl->COL_TIME_ON); echo date($custom_date_format, $timestamp); echo '</td>';
 		echo '<td style=\'text-align: center\'>'; $timestamp = strtotime($qsl->COL_TIME_ON); echo date('H:i', $timestamp); echo '</td>';
 		echo '<td style=\'text-align: center\'>'; echo $qsl->COL_SUBMODE==null?$qsl->COL_MODE:$qsl->COL_SUBMODE; echo '</td>';
 		echo '<td style=\'text-align: center\'>'; if($qsl->COL_SAT_NAME != null) { echo $qsl->COL_SAT_NAME; } else { echo strtolower($qsl->COL_BAND); }; echo '</td>';
 		echo '<td style=\'text-align: center\'><span class="badge badge-light">' . $qsl->station_callsign . '</span></td>';
-		echo '<td style=\'text-align: center\'><button onclick="deleteFromQslQueue(\''.$qsl->COL_PRIMARY_KEY.'\')" class="btn btn-sm btn-danger">Delete from queue</button></td>';
-		echo '<td style=\'text-align: center\'><button onclick="openQsoList(\''.$qsl->COL_CALL.'\')" class="btn btn-sm btn-success">Open QSO list</button></td>';
+		echo '<td id="'.$qsl->COL_PRIMARY_KEY.'" style=\'text-align: center\'><button onclick="addQsoToPrintQueue(\''.$qsl->COL_PRIMARY_KEY.'\')" class="btn btn-sm btn-success">Add to print queue</button></td>';
 		echo '</tr>';
 	}
 
 	echo '</tbody></table>';
 	?>
 
-	<p><a href="<?php echo site_url('qslprint/exportcsv/' . $station_id); ?>" title="Export CSV-file" target="_blank" class="btn btn-primary">Export requested QSLs to CSV-file</a></p>
-
-	<p><a href="<?php echo site_url('qslprint/exportadif/' . $station_id); ?>" title="Export ADIF" target="_blank" class="btn btn-primary">Export requested QSLs to ADIF-file</a></p>
-
-	<p><a href="<?php echo site_url('qslprint/qsl_printed/' . $station_id); ?>" title="Mark QSLs as printed" target="_blank" class="btn btn-primary">Mark requested QSLs as sent</a></p>
-
-<?php
+	<?php
 } else {
-	echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>No QSL\'s to print were found!</div>';
+	echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>No additional QSO\'s were found. That means they are probably already in the queue.</div>';
 }
 ?>
