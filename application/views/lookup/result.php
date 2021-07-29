608b09c973c84f9760e9cd624caebfe3e53520ae
Andreas <6977712+AndreasK79@users.noreply.github.com>
2021-07-29 12:38:41 +0200

    [Quick lookup] Rewrote the view for easier editing.

--- application/views/lookup/result.php
+++ application/views/lookup/result.php
@@ -12,39 +12,33 @@ echo '
 		<tbody>';
 foreach ($result as $mode => $value) {
 	echo '<tr>
-                <td>'. strtoupper($mode) .'</td>';
+			<td>'. strtoupper($mode) .'</td>';
 	foreach ($value as $key => $val) {
+		switch($type) {
+			case 'dxcc': $linkinfo = '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $dxcc).'","' . $key . '","' . $mode . '","DXCC2")\'>'  . $val . '</a>'; break;
+			case 'iota': $linkinfo = '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $iota).'","' . $key . '","' . $mode . '","IOTA")\'>'   . $val . '</a>'; break;
+			case 'grid': $linkinfo = '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $grid).'","' . $key . '","' . $mode . '","VUCC")\'>'   . $val . '</a>'; break;
+			case 'cqz':  $linkinfo = '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $cqz).'","'  . $key . '","' . $mode . '","CQZone")\'>' . $val . '</a>'; break;
+			case 'was':  $linkinfo = '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $was).'","'  . $key . '","' . $mode . '","WAS")\'>'    . $val . '</a>'; break;
+			case 'sota': $linkinfo = '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $sota).'","' . $key . '","' . $mode . '","SOTA")\'>'   . $val . '</a>'; break;
+			case 'wwff': $linkinfo = '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $wwff).'","' . $key . '","' . $mode . '","WWFF")\'>'   . $val . '</a>'; break;
+		}
+
+		$info = '<td>';
+
 		if ($val == 'W') {
-			$info = '<td><div class=\'alert-danger\'>';
-			switch($type) {
-				case 'dxcc': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $dxcc).'","' . $key . '","' . $mode . '","DXCC2")\'>W</a>'; break;
-				case 'iota': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $iota).'","' . $key . '","' . $mode . '","IOTA")\'>W</a>'; break;
-				case 'grid': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $grid).'","' . $key . '","' . $mode . '","VUCC")\'>W</a>'; break;
-				case 'cqz':  $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $cqz).'","' . $key . '","' . $mode . '","CQZone")\'>W</a>'; break;
-				case 'was':  $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $was).'","' . $key . '","' . $mode . '","WAS")\'>W</a>'; break;
-				case 'sota': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $sota).'","' . $key . '","' . $mode . '","SOTA")\'>W</a>'; break;
-				case 'wwff': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $wwff).'","' . $key . '","' . $mode . '","WWFF")\'>W</a>'; break;
-			}
-			$info .= '</div></td>';
-			echo $info;
+			$info .= '<div class=\'alert-danger\'>' . $linkinfo . '</div>';
 		}
 		else if ($val == 'C') {
-			$info = '<td><div class=\'alert-success\'>';
-			switch($type) {
-				case 'dxcc': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $dxcc).'","' . $key . '","' . $mode . '","DXCC2")\'>C</a>'; break;
-				case 'iota': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $iota).'","' . $key . '","' . $mode . '","IOTA")\'>C</a>'; break;
-				case 'grid': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $grid).'","' . $key . '","' . $mode . '","VUCC")\'>C</a>'; break;
-				case 'cqz':  $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $cqz).'","' . $key . '","' . $mode . '","CQZone")\'>C</a>'; break;
-				case 'was':  $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $was).'","' . $key . '","' . $mode . '","WAS")\'>C</a>'; break;
-				case 'sota': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $sota).'","' . $key . '","' . $mode . '","SOTA")\'>C</a>'; break;
-				case 'wwff': $info .= '<a href=\'javascript:displayContacts("'.str_replace("&", "%26", $wwff).'","' . $key . '","' . $mode . '","WWFF")\'>C</a>'; break;
-			}
-			$info .= '</div></td>';
-			echo $info;
+			$info .= '<div class=\'alert-success\'>' . $linkinfo . '</div>';
 		}
 		else {
-			echo '<td>' . $val . '</td>';
+			$info .= $val;
 		}
+
+		$info .= '</td>';
+
+		echo $info;
 	}
 	echo '</tr>';
 }
