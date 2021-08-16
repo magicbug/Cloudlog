a80089f97c83e5f3f7c213a1f1c0ca8b7a3a4c5a
Peter Goodhall <peter@magicbug.co.uk>
2021-08-16 11:24:16 +0100

    Make sure tables dont exist before running migration

--- application/migrations/071_create_station_logbook_table.php
+++ application/migrations/071_create_station_logbook_table.php
@@ -11,38 +11,40 @@ class Migration_create_station_logbook_table extends CI_Migration {
 
     public function up()
     {
-        $this->dbforge->add_field(array(
-            'logbook_id' => array(
-                'type' => 'BIGINT',
-                'constraint' => 20,
-                'unsigned' => TRUE,
-                'auto_increment' => TRUE,
-                'unique' => TRUE
-            ),
+        if (!$this->db->table_exists('station_logbooks')) {
+            $this->dbforge->add_field(array(
+                'logbook_id' => array(
+                    'type' => 'BIGINT',
+                    'constraint' => 20,
+                    'unsigned' => TRUE,
+                    'auto_increment' => TRUE,
+                    'unique' => TRUE
+                ),
 
-            'user_id' => array(
-                'type' => 'BIGINT',
-                'constraint' => 20,
-                'unsigned' => TRUE,
-                'auto_increment' => FALSE
-            ),
-            
-            'logbook_name' => array(
-                'type' => 'VARCHAR',
-                'constraint' => '191',
-                'null' => TRUE
-            ),
+                'user_id' => array(
+                    'type' => 'BIGINT',
+                    'constraint' => 20,
+                    'unsigned' => TRUE,
+                    'auto_increment' => FALSE
+                ),
+                
+                'logbook_name' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '191',
+                    'null' => TRUE
+                ),
 
-            'modified' => array(
-                'type' => 'timestamp',
-                'null' => TRUE,
-            )
-        ));
+                'modified' => array(
+                    'type' => 'timestamp',
+                    'null' => TRUE,
+                )
+            ));
 
-        $this->dbforge->add_key('logbook_id', TRUE);
-        $this->dbforge->add_key('user_id', TRUE);
+            $this->dbforge->add_key('logbook_id', TRUE);
+            $this->dbforge->add_key('user_id', TRUE);
 
-        $this->dbforge->create_table('station_logbooks');
+            $this->dbforge->create_table('station_logbooks');
+        }
     }
 
     public function down()
--- application/migrations/072_create_station_logbook_relationship_table.php
+++ application/migrations/072_create_station_logbook_relationship_table.php
@@ -11,40 +11,42 @@ class Migration_create_station_logbook_relationship_table extends CI_Migration {
 
     public function up()
     {
-        $this->dbforge->add_field(array(
-            'logbook_relation_id' => array(
-                'type' => 'BIGINT',
-                'constraint' => 20,
-                'unsigned' => TRUE,
-                'auto_increment' => TRUE,
-                'unique' => TRUE
-            ),
+        if (!$this->db->table_exists('station_logbooks_relationship')) {
+            $this->dbforge->add_field(array(
+                'logbook_relation_id' => array(
+                    'type' => 'BIGINT',
+                    'constraint' => 20,
+                    'unsigned' => TRUE,
+                    'auto_increment' => TRUE,
+                    'unique' => TRUE
+                ),
 
-            'station_logbook_id' => array(
-                'type' => 'BIGINT',
-                'constraint' => 20,
-                'unsigned' => TRUE,
-                'auto_increment' => FALSE
-            ),
+                'station_logbook_id' => array(
+                    'type' => 'BIGINT',
+                    'constraint' => 20,
+                    'unsigned' => TRUE,
+                    'auto_increment' => FALSE
+                ),
 
-            'station_location_id' => array(
-                'type' => 'BIGINT',
-                'constraint' => 20,
-                'unsigned' => TRUE,
-                'auto_increment' => FALSE
-            ),
+                'station_location_id' => array(
+                    'type' => 'BIGINT',
+                    'constraint' => 20,
+                    'unsigned' => TRUE,
+                    'auto_increment' => FALSE
+                ),
 
-            'modified' => array(
-                'type' => 'timestamp',
-                'null' => TRUE,
-            )
-        ));
+                'modified' => array(
+                    'type' => 'timestamp',
+                    'null' => TRUE,
+                )
+            ));
 
-        $this->dbforge->add_key('logbook_relation_id', TRUE);
-        $this->dbforge->add_key('station_logbook_id', TRUE);
-        $this->dbforge->add_key('station_location_id', TRUE);
+            $this->dbforge->add_key('logbook_relation_id', TRUE);
+            $this->dbforge->add_key('station_logbook_id', TRUE);
+            $this->dbforge->add_key('station_location_id', TRUE);
 
-        $this->dbforge->create_table('station_logbooks_relationship');
+            $this->dbforge->create_table('station_logbooks_relationship');
+        }
     }
 
     public function down()
