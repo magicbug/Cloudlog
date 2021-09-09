365e79c3849ac2560c9dc06829ef54bd29fdd68f
Andreas <6977712+AndreasK79@users.noreply.github.com>
2021-09-09 22:35:25 +0200

    [SOTA Award] Updated code to support station logbooks

--- application/models/Sota.php
+++ application/models/Sota.php
@@ -6,16 +6,16 @@ class Sota extends CI_Model {
 	{
 		parent::__construct();
 	}
-	
+
 	function get_all() {
 		$CI =& get_instance();
-      	$CI->load->model('Stations');
-      	$station_id = $CI->Stations->find_active();
+		$CI->load->model('logbooks_model');
+		$logbooks_locations_array = $CI->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
 
-		$this->db->where("station_id", $station_id);
-		$this->db->order_by("COL_SOTA_REF", "ASC"); 
+		$this->db->where_in("station_id", $logbooks_locations_array);
+		$this->db->order_by("COL_SOTA_REF", "ASC");
 		$this->db->where('COL_SOTA_REF !=', '');
-		
+
 		return $this->db->get($this->config->item('table_name'));
 	}
 }
