43f96fab9dea82b3ed64b60816f34e7eee8f5062
Thomas Werzmirzowsky <werzi2001@gmx.de>
2021-09-29 21:51:42 +0200

    "Mark QSL Card Not Required" sets status to Ignore but sets red arrow which reflects status No. Changed color to grey.

--- application/views/interface_assets/footer.php
+++ application/views/interface_assets/footer.php
@@ -1239,7 +1239,7 @@ $(document).ready(function(){
                 },
                 success: function(data) {
                     if (data.message == 'OK') {
-                        $("#qso_" + id).find("td:eq(8)").find("span:eq(0)").attr('class', 'qsl-red'); // Paints arrow green
+                        $("#qso_" + id).find("td:eq(8)").find("span:eq(0)").attr('class', 'qsl-grey'); // Paints arrow grey
                     }
                     else {
                         $(".bootstrap-dialog-message").append('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>You are not allowed to update QSL status!</div>');
