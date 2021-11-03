591b7d2c4b62c1a92c52fd56b5ca31cdf18f8104
Ivan <ivan.ivanov@scoutbg.org>
2021-11-03 12:02:52 +0200

    Added Bulgarian language translation

---
+++ application/language/bulgarian/qslcard_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('Не е разрешен директен достъп до скрипта');
+
+// Tiles
+$lang['qslcard_string_your_are_using'] = 'Вие използвате';
+$lang['qslcard_string_disk_space'] = 'дисково пространство за съхранение на QSL картички';
+
+$lang['qslcard_info'] = 'QSL информация';
+$lang['qslcard_sent_bureau'] = 'QSL картичката е изпратена чрез бюрото';
+$lang['qslcard_sent_direct'] = 'QSL картичката е изпратена директно';
+$lang['qslcard_recv_bureau'] = 'QSL картичката е получена през бюрото';
+$lang['qslcard_recv_direct'] = 'QSL картичката е получена директно';
+
+$lang['qslcard_upload_front'] = 'Качване на лице на QSL картичка';
+$lang['qslcard_upload_back'] = 'Качване на гръб на QSL картичка';
+
+$lang['qslcard_upload_button'] = 'Качване на изображения на QSL картичка';
