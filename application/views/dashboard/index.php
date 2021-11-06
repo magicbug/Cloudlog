a9f9916315a7757c83a62094e11e2df8e77396d3
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2021-11-06 15:53:04 +0100

    Update index.php
    Changed TD to TH tag in the header.

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -10,7 +10,7 @@ function echo_table_header_col($ctx, $name) {
 		case 'State': echo '<th>'.$ctx->lang->line('gen_hamradio_state').'</th>'; break;
 		case 'Grid': echo '<th>'.$ctx->lang->line('gen_hamradio_gridsquare').'</th>'; break;
 		case 'Band': echo '<th>'.$ctx->lang->line('gen_hamradio_band').'</th>'; break;
-		case 'Operator': echo '<td>'.$ctx->lang->line('gen_hamradio_operator').'</td>'; break;
+		case 'Operator': echo '<th>'.$ctx->lang->line('gen_hamradio_operator').'</th>'; break;
 	}
 }
 
