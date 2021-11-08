0ffe57dc6363e82e8191dd97e95b499150759afa
Peter Goodhall <peter@magicbug.co.uk>
2021-11-08 15:28:16 +0000

    [Auth] Changed Roles either Administrator or Operator
    This is the first step in changes to the AUTH system it will have two roles
    
    - Admin - who can access all the admin tools plus log qsos
    - Operator - who can just access the main Cloudlog functions and their own logbooks and locations

--- application/controllers/Logbook.php
+++ application/controllers/Logbook.php
@@ -17,15 +17,13 @@ class Logbook extends CI_Controller {
 
 	function index()
 	{
-				$this->load->model('user_model');
-				if(!$this->user_model->authorize($this->config->item('auth_mode'))) {
-						if($this->user_model->validate_session()) {
-								$this->user_model->clear_session();
-								show_error('Access denied<p>Click <a href="'.site_url('user/login').'">here</a> to log in as another user', 403);
-						} else {
-								redirect('user/login');
-						}
-				}
+
+		// Check if users logged in
+		$this->load->model('user_model');
+		if($this->user_model->validate_session() == 0) {
+			// user is not logged in
+			redirect('user/login');
+		}
 
 		$this->load->model('logbook_model');
 
