341e8f4a3372391587b51af6989464749a2d149d
Andreas <6977712+AndreasK79@users.noreply.github.com>
2021-11-14 17:49:35 +0100

    [Days with QSOs] Fix if no QSOs found

--- assets/js/sections/dayswithqso.js
+++ assets/js/sections/dayswithqso.js
@@ -1,46 +1,48 @@
 $.ajax({
-	url: base_url+'index.php/dayswithqso/get_days',
-	success: function(data) {
-		var labels = [];
-		var dataDxcc = [];
-		$.each(data, function(){
-			labels.push(this.Year);
-			dataDxcc.push(this.Days);
-		});
-		var ctx = document.getElementById("myChartDiff").getContext('2d');
-		var color = ifDarkModeThemeReturn('white', 'grey');
-		var myChart = new Chart(ctx, {
-			type: 'bar',
-			data: {
-				labels: labels,
-				datasets: [{
-					label: 'Days with QSOs',
-					data: dataDxcc,
-					backgroundColor: 'rgba(54, 162, 235, 0.2)',
-					borderColor: 'rgba(54, 162, 235, 1)',
-					borderWidth: 2
-				}]
-			},
-			options: {
-				scales: {
-					yAxes: [{
-						ticks: {
-							beginAtZero:true,
-							fontColor: color
-						}
-					}],
-					xAxes: [{
-						ticks: {
-							fontColor: color
-						}
+	url: base_url + 'index.php/dayswithqso/get_days',
+	success: function (data) {
+		if ($.trim(data)) {
+			var labels = [];
+			var dataDxcc = [];
+			$.each(data, function () {
+				labels.push(this.Year);
+				dataDxcc.push(this.Days);
+			});
+			var ctx = document.getElementById("myChartDiff").getContext('2d');
+			var color = ifDarkModeThemeReturn('white', 'grey');
+			var myChart = new Chart(ctx, {
+				type: 'bar',
+				data: {
+					labels: labels,
+					datasets: [{
+						label: 'Days with QSOs',
+						data: dataDxcc,
+						backgroundColor: 'rgba(54, 162, 235, 0.2)',
+						borderColor: 'rgba(54, 162, 235, 1)',
+						borderWidth: 2
 					}]
 				},
-				legend: {
-					labels: {
-						fontColor: color
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero: true,
+								fontColor: color
+							}
+						}],
+						xAxes: [{
+							ticks: {
+								fontColor: color
+							}
+						}]
+					},
+					legend: {
+						labels: {
+							fontColor: color
+						}
 					}
 				}
-			}
-		});
+			});
+		}
 	}
 });
