cd71d48656bb75e6292f799de951987a18942d61
Andreas <6977712+AndreasK79@users.noreply.github.com>
2021-11-14 17:57:33 +0100

    [Distances Worked] Fix for QSOs not found

--- application/models/Distances_model.php
+++ application/models/Distances_model.php
@@ -10,6 +10,12 @@ class Distances_model extends CI_Model
 		$CI->load->model('logbooks_model');
 		$logbooks_locations_array = $CI->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
 
+        if (!$logbooks_locations_array) {
+            header('Content-Type: application/json');
+            echo json_encode(array('Error' => 'No QSOs found to plot.'));
+            return;
+        }
+
 		$result = array();
 
 		foreach ($logbooks_locations_array as $station_id) {
