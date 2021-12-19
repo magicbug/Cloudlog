8d6466334b8372290285656724a7d990d1d311b5
phl0 <florian@florian-wolters.de>
2021-12-19 22:07:51 +0100

    Added hamqth.com and some tooltips

--- application/views/qso/index.php
+++ application/views/qso/index.php
@@ -60,13 +60,14 @@
 
               <!-- Callsign Input -->
               <div class="form-row">
-                <div class="form-group col-md-9">
+                <div class="form-group col-md-7">
                   <label for="callsign"><?php echo $this->lang->line('gen_hamradio_callsign'); ?></label>
                   <input type="text" class="form-control" id="callsign" name="callsign" required>
                   <small id="callsign_info" class="badge badge-secondary"></small> <small id="lotw_info" class="badge badge-light"></small>
                 </div>
-                <div class="form-group col-md-1 align-self-center">
+                <div class="form-group col-md-3 align-self-center">
                   <small id="qrz_info" class="badge badge-secondary"></small>
+                  <small id="hamqth_info" class="badge badge-secondary"></small>
                 </div>
               </div>
 
