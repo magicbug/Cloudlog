1e4f6283e006c2def05279eed805d508037d304f
Peter Goodhall <peter@magicbug.co.uk>
2022-01-04 11:27:36 +0000

    [SAT] Change Sat names stored as XW-3(CAS-9) to HO-113

---
+++ application/migrations/081_update_xw2_qsos.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_update_xw2_qsos extends CI_Migration
+{
+	public function up()
+	{
+        $this->db->set('COL_SAT_NAME', 'HO-113');
+        $this->db->set('COL_LOTW_QSL_SENT', 'N');
+        $this->db->where('COL_SAT_NAME', 'XW-3(CAS-9)');
+        $this->db->update($this->config->item('table_name'));
+	}
+
+	public function down()
+	{
+        // Not Possible
+	}
+}
