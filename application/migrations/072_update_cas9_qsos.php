1406364b9c249f60416f3b3c98c89d9036064219
Peter Goodhall <peter@magicbug.co.uk>
2022-01-04 00:48:16 +0000

    Update 072_update_cas9_qsos.php

---
+++ application/migrations/072_update_cas9_qsos.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_update_cas9_qsos extends CI_Migration
+{
+	public function up()
+	{
+        $this->db->set('COL_SAT_NAME', 'HO-113');
+        $this->db->set('COL_LOTW_QSL_SENT', 'N');
+        $this->db->where('COL_SAT_NAME', 'CAS-9');
+        $this->db->update($this->config->item('table_name')); // gives UPDATE `mytable` SET `field` = 'field+1' WHERE `id` = 2
+	}
+
+	public function down()
+	{
+        // Not Possible
+	}
+}
