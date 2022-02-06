b9291094288b2fb962c097d99c439dc9de5ddaea
Thomas Werzmirzowsky <werzi2001@gmx.de>
2022-02-06 09:41:11 +0100

    changed search filter from active logbook to all stations of the user also changed get_qso($id) accordingly (otherwise no details of the QSOs from other logbooks can be shown)

--- application/controllers/Logbook.php
+++ application/controllers/Logbook.php
@@ -568,10 +568,6 @@ class Logbook extends CI_Controller {
 
 		if(!$this->user_model->authorize($this->config->item('auth_mode'))) { return; }
 
-		$CI =& get_instance();
-		$CI->load->model('logbooks_model');
-		$logbooks_locations_array = $CI->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
-
 		//$this->db->select(''.$this->config->item('table_name').'.COL_CALL, '.$this->config->item('table_name').'.COL_BAND, '.$this->config->item('table_name').'.COL_TIME_ON, '.$this->config->item('table_name').'.COL_RST_RCVD, '.$this->config->item('table_name').'.COL_RST_SENT, '.$this->config->item('table_name').'.COL_MODE, '.$this->config->item('table_name').'.COL_SUBMODE, '.$this->config->item('table_name').'.COL_NAME, '.$this->config->item('table_name').'.COL_COUNTRY, '.$this->config->item('table_name').'.COL_PRIMARY_KEY, '.$this->config->item('table_name').'.COL_SAT_NAME, '.$this->config->item('table_name').'.COL_GRIDSQUARE, '.$this->config->item('table_name').'.COL_QSL_RCVD, '.$this->config->item('table_name').'.COL_EQSL_QSL_RCVD, '.$this->config->item('table_name').'.COL_EQSL_QSL_SENT, '.$this->config->item('table_name').'.COL_QSL_SENT, '.$this->config->item('table_name').'.COL_STX, '.$this->config->item('table_name').'.COL_STX_STRING, '.$this->config->item('table_name').'.COL_SRX, '.$this->config->item('table_name').'.COL_SRX_STRING, '.$this->config->item('table_name').'.COL_LOTW_QSL_SENT, '.$this->config->item('table_name').'.COL_LOTW_QSL_RCVD, '.$this->config->item('table_name').'.COL_VUCC_GRIDS, station_profile.*');
 
 		$this->db->from($this->config->item('table_name'));
@@ -581,7 +577,7 @@ class Logbook extends CI_Controller {
 		$this->db->or_like(''.$this->config->item('table_name').'.COL_GRIDSQUARE', $id);
 		$this->db->or_like(''.$this->config->item('table_name').'.COL_VUCC_GRIDS', $id);
 		$this->db->group_end();
-		$this->db->where_in(''.$this->config->item('table_name').'.station_id', $logbooks_locations_array);
+		$this->db->where('station_profile.user_id', $this->session->userdata('user_id'));
 		$this->db->order_by(''.$this->config->item('table_name').'.COL_TIME_ON', 'desc');
 		$query = $this->db->get();
 
--- application/models/Search.php
+++ application/models/Search.php
@@ -3,12 +3,9 @@
 class Search extends CI_Model {
 
 	function callsign_iota($reference) {
-		$CI =& get_instance();
-		$CI->load->model('logbooks_model');
-		$logbooks_locations_array = $CI->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
-
 		$this->db->where('COL_IOTA', $reference);
-		$this->db->where_in('station_id', $logbooks_locations_array);
+		$this->db->join('station_profile', 'station_profile.station_id = '.$this->config->item('table_name').'.station_id');
+		$this->db->where('station_profile.user_id', $this->session->userdata('user_id'));
 		$query = $this->db->get($this->config->item('table_name')); 
 		
 		return $query;
