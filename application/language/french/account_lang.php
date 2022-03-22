1b9fc4b1585bddaa4a076a526c1abe9d0ca4c365
ClaudeMa <claude.xxxxxxx@gmail.com>
2022-03-22 20:46:36 +0100

    French translations update

--- application/language/french/account_lang.php
+++ application/language/french/account_lang.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['account_logbook_fields'] = 'Logbook fields';
+$lang['account_logbook_fields'] = 'Champs Logbook';
 $lang['account_colonne1_text'] = 'Choisir colonne 1';
 $lang['account_colonne2_text'] = 'Choisir colonne 2';
 $lang['account_colonne3_text'] = 'Choisir colonne 3';
--- application/language/french/qslcard_lang.php
+++ application/language/french/qslcard_lang.php
@@ -5,3 +5,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // Tiles
 $lang['qslcard_string_your_are_using'] = 'Vous utilisez';
 $lang['qslcard_string_disk_space'] = 'd\'espace disque pour stocker les actifs des cartes QSL';
+$lang['qslcard_info'] = 'Info QSL';
+$lang['qslcard_sent_bureau'] = 'La carte QSL a été envoyé via le bureau';
+$lang['qslcard_sent_direct'] = 'La carte QSL a été envoyé via direct';
+$lang['qslcard_recv_bureau'] = 'La carte QSL a été reçue via the bureau';
+$lang['qslcard_recv_direct'] = 'La carte QSL a été reçue via direct';
+
+$lang['qslcard_upload_front'] = 'Image de la face de la carte QSL envoyée';
+$lang['qslcard_upload_back'] = 'Image du dos de la carte QSL envoyée';
+
+$lang['qslcard_upload_button'] = 'Envoyer image de la carte QSL';
