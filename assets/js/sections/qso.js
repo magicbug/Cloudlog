65486c49748332375cc906bab3519adad8b7ac4d
Peter Goodhall <peter@magicbug.co.uk>
2022-03-31 22:09:50 +0100

    [QSO Input] Handles showing the gridsquare popover on focus

--- assets/js/sections/qso.js
+++ assets/js/sections/qso.js
@@ -2,9 +2,13 @@ $( document ).ready(function() {
 
 	$("#locator")
 		.popover({ placement: 'top', title: 'Gridsquare Formatting', content: "Enter multiple (4-digit) grids separated with commas. For example: IO77,IO78" })
+		.focus(function () {
+			$('#locator').popover('show');
+		})
 		.blur(function () {
-		$(this).popover('hide');
-	});
+			$('#locator').popover('hide');
+		});
+
 	$("#sat_name").change(function(){
 		var sat = $("#sat_name").val();
 		if (sat == "") {
