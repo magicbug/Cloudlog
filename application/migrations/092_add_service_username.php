98dd08a6f8b10595f1041c933b557c009bb76633
Peter Goodhall <peter@magicbug.co.uk>
2022-05-05 15:11:33 +0100

    [Migration] Add a service_username to thirdparty_logins

---
+++ application/migrations/092_add_service_username.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+    Creates column service_username in table thirdparty_logins
+*/
+
+class Migration_add_service_username extends CI_Migration {
+
+    public function up()
+    {
+        if ($this->db->table_exists('thirdparty_logins')) {
+
+            $fields = array(
+                'service_username' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '255',
+                    'null' => TRUE
+                )
+            );
+
+            if (!$this->db->field_exists('public_slug', 'thirdparty_logins')) {
+                $this->dbforge->add_column('thirdparty_logins', $fields);
+            }
+            
+        }
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_column('thirdparty_logins', 'service_username');
+    }
+}
