bd6fe552f73dbb8e5db8c1d411b108c05104a10f
Andreas <6977712+AndreasK79@users.noreply.github.com>
2022-07-04 20:15:45 +0200

    [eQSL] Mark QSOs. Refined the update instead of updating everything without filter.

--- application/models/Eqslmethods_model.php
+++ application/models/Eqslmethods_model.php
@@ -12,6 +12,13 @@ class Eqslmethods_model extends CI_Model {
             'COL_EQSL_QSL_SENT' => 'Y',
             'COL_EQSL_QSLSDATE'  => date('Y-m-d')." 00:00:00",
         );
+
+        $this->db->group_start();
+		$this->db->where('COL_EQSL_QSL_SENT', 'N');
+        $this->db->or_where('COL_EQSL_QSL_SENT', 'R');
+        $this->db->or_where('COL_EQSL_QSL_SENT', 'Q');
+        $this->db->or_where('COL_EQSL_QSL_SENT', null);
+		$this->db->group_end();
         
         $this->db->update($this->config->item('table_name'), $data);
 	}
