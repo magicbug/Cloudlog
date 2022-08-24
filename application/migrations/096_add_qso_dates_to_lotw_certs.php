05ac1ff55edf14fff16252fff48ac005027d2941
phl0 <github@florian-wolters.de>
2022-08-24 14:34:44 +0200

    Extract and store QSO start/end date and store separately

--- application/migrations/096_add_qso_dates_to_lotw_certs.php
+++ application/migrations/096_add_qso_dates_to_lotw_certs.php
@@ -21,6 +21,20 @@ class Migration_add_qso_dates_to_lotw_certs extends CI_Migration {
                         );
                         $this->dbforge->add_column('lotw_certs', $fields);
                 }
+
+                // Extract QSO start and end date from x509 certs and insert into
+                // newly created columns
+
+                $query = $this->db->query("SELECT `lotw_cert_id`, `cert` FROM `lotw_certs` WHERE 1");
+                foreach ($query->result() as $cert) {
+                   $certdata = openssl_x509_parse($cert->cert,0);
+                   $data = array(
+                      'qso_start_date' => $certdata['extensions']['1.3.6.1.4.1.12348.1.2'],
+                      'qso_end_date' => $certdata['extensions']['1.3.6.1.4.1.12348.1.3'],
+                   );
+                   $this->db->where('lotw_cert_id', $cert->lotw_cert_id);
+                   $this->db->update('lotw_certs', $data);
+                }
         }
 
         public function down()
