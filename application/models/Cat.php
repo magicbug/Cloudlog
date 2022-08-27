37ea7049d7723198a20d9fdd564df0c80eac5911
kb-light <14014119+kb-light@users.noreply.github.com>
2022-08-27 17:00:23 +0000

    Radio API: Remove redundant data construction

--- application/models/Cat.php
+++ application/models/Cat.php
@@ -25,6 +25,35 @@
 			$this->db->where('user_id', $user_id);
 			$query = $this->db->get('cat');
 
+			if ($prop_mode == "SAT") {
+				$data = array(
+					'prop_mode' => $prop_mode,
+					'sat_name' => $result['sat_name'],
+					'downlink_freq' => $result['downlink_freq'],
+					'uplink_freq' => $result['uplink_freq'],
+					'downlink_mode' => $result['downlink_mode'],
+					'uplink_mode' => $result['uplink_mode'],
+					'timestamp' => $timestamp,
+					'mode' => NULL,
+					'frequency' => NULL,
+				);
+			} else {
+				$data = array(
+					'prop_mode' => $result['prop_mode'],
+					'mode' => $result['mode'],
+					'frequency' => $result['frequency'],
+					'downlink_freq' => NULL,
+					'downlink_mode' => NULL,
+					'uplink_freq' => NULL,
+					'uplink_mode' => NULL,
+					'timestamp' => $timestamp,
+				);
+			}
+
+			if (isset($result['power'])) {
+				$data['power'] = $result['power'];
+			}
+
 			if ($query->num_rows() > 0)
 			{
 				// Update the record
@@ -32,77 +61,16 @@
 				{
 					$radio_id = $row->id;
 
-					if ($prop_mode == "SAT") {
-						$data = array(
-							'prop_mode' => $prop_mode,
-							'sat_name' => $result['sat_name'],
-							'downlink_freq' => $result['downlink_freq'],
-							'uplink_freq' => $result['uplink_freq'],
-							'downlink_mode' => $result['downlink_mode'],
-							'uplink_mode' => $result['uplink_mode'],
-							'timestamp' => $timestamp,
-							'mode' => NULL,
-							'frequency' => NULL,
-						);
-					} else {
-						$data = array(
-							'prop_mode' => $result['prop_mode'],
-							'mode' => $result['mode'],
-							'frequency' => $result['frequency'],
-							'downlink_freq' => NULL,
-							'downlink_mode' => NULL,
-							'uplink_freq' => NULL,
-							'uplink_mode' => NULL,
-							'timestamp' => $timestamp,
-						);
-					}
-
-					if (isset($result['power'])) {
-						$data['power'] = $result['power'];
-					}
-
 					$this->db->where('id', $radio_id);
 					$this->db->where('user_id', $user_id);
 					$this->db->update('cat', $data);
 				}
 			} else {
 				// Add a new record
-
-				if ($prop_mode == "SAT") {
-					$data = array(
-						'radio' => $result['radio'],
-						'frequency' => NULL,
-						'mode' => NULL,
-						'sat_name' => $result['sat_name'],
-						'downlink_freq' => $result['downlink_freq'],
-						'uplink_freq' => $result['uplink_freq'],
-						'downlink_mode' => $result['downlink_mode'],
-						'uplink_mode' => $result['uplink_mode'],
-						'prop_mode' => $prop_mode,
-						'user_id' => $user_id,
-						'timestamp' => $timestamp,
-					);
-				} else {
-					$data = array(
-						'radio' => $result['radio'],
-						'prop_mode' => $prop_mode,
-						'mode' => $result['mode'],
-						'frequency' => $result['frequency'],
-						'downlink_freq' => NULL,
-						'downlink_mode' => NULL,
-						'uplink_freq' => NULL,
-						'uplink_mode' => NULL,
-						'user_id' => $user_id,
-						'timestamp' => $timestamp,
-					);
-				}
-
-				if (isset($result['power'])) {
-					$data['power'] = $result['power'];
-				}
+				$data['radio'] = $result['radio'];
+				$data['user_id'] = $user_id;
 
 				$this->db->insert('cat', $data);
-
 			}
 		}
 
