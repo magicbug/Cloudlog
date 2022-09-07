0eab4e1a6040643a70f5f16cb8c5b02d142fee71
Andreas <6977712+AndreasK79@users.noreply.github.com>
2022-09-07 09:19:18 +0200

    [Bands] SIG support added

--- application/models/Sig.php
+++ application/models/Sig.php
@@ -11,7 +11,12 @@ class Sig extends CI_Model {
             return null;
         }
 
+		$this->load->model('bands');
+
+		$bandslots = $this->bands->get_worked_bands('sig');
+
 		$this->db->where_in("station_id", $logbooks_locations_array);
+		$this->db->where_in("col_band", $bandslots);
 		$this->db->order_by("COL_SIG_INFO", "ASC");
 		$this->db->where('COL_SIG =', $type);
 
@@ -29,9 +34,16 @@ class Sig extends CI_Model {
 
 		$location_list = "'".implode("','",$logbooks_locations_array)."'";
 
+		$this->load->model('bands');
+
+		$bandslots = $this->bands->get_worked_bands('sig');
+
+		$bandslots_list = "'".implode("','",$bandslots)."'";
+
         $sql = "select col_sig, count(*) qsos, count(distinct col_sig_info) refs from " . $this->config->item('table_name') .
                 " where col_sig <> ''" .
                 " and station_id in (" . $location_list . ")" .
+				" and col_band in (" . $bandslots_list . ")" .
                 " group by col_sig";
 
         $query = $this->db->query($sql);
