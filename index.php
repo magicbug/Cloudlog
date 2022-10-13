bb1ad0d875eb7e399e0bb79857e2baf9f0693525
Peter Goodhall <magicbug@users.noreply.github.com>
2022-10-13 15:42:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	#define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	define('ENVIRONMENT', 'development');
+	define('ENVIRONMENT', 'production');
 
 /*
  *---------------------------------------------------------------
