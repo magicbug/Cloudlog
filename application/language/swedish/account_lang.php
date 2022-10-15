b7a91070f343a954d8d3fdd8c72213e39f75f3e6
Lars Thunberg <35871385+LarsThunberg@users.noreply.github.com>
2022-10-15 14:13:16 +0200

    Add files via upload

---
+++ application/language/swedish/account_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['account_logbook_fields'] = 'Logbok-fält';
+$lang['account_column1_text'] = 'Välj kolumn 1';
+$lang['account_column2_text'] = 'Välj kolumn 2';
+$lang['account_column3_text'] = 'Välj kolumn 3';
+$lang['account_column4_text'] = 'Välj kolumn 4';
+$lang['account_column5_text'] = 'Välj kolumn 5 (bara för logbok)';
