94e720a2786bd4c9eb27b02c062de09a0fd34445
Lars Thunberg <35871385+LarsThunberg@users.noreply.github.com>
2022-10-15 14:12:35 +0200

    Add files via upload

---
+++ system/language/swedish/calendar_lang.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cal_su'] = 'Sö';
+$lang['cal_mo'] = 'Må';
+$lang['cal_tu'] = 'Ti';
+$lang['cal_we'] = 'On';
+$lang['cal_th'] = 'To';
+$lang['cal_fr'] = 'Fr';
+$lang['cal_sa'] = 'Lö';
+$lang['cal_sun'] = 'Sön';
+$lang['cal_mon'] = 'Mån';
+$lang['cal_tue'] = 'Tis';
+$lang['cal_wed'] = 'Ons';
+$lang['cal_thu'] = 'Tor';
+$lang['cal_fri'] = 'Fre';
+$lang['cal_sat'] = 'Lör';
+$lang['cal_sunday'] = 'Söndag';
+$lang['cal_monday'] = 'Måndag';
+$lang['cal_tuesday'] = 'Tisdag';
+$lang['cal_wednesday'] = 'Onsdag';
+$lang['cal_thursday'] = 'Torsdag';
+$lang['cal_friday'] = 'Fridag';
+$lang['cal_saturday'] = 'Lördag';
+$lang['cal_jan'] = 'Jan';
+$lang['cal_feb'] = 'Feb';
+$lang['cal_mar'] = 'Mar';
+$lang['cal_apr'] = 'Apr';
+$lang['cal_may'] = 'Maj';
+$lang['cal_jun'] = 'Jun';
+$lang['cal_jul'] = 'Jul';
+$lang['cal_aug'] = 'Aug';
+$lang['cal_sep'] = 'Sep';
+$lang['cal_oct'] = 'Okt';
+$lang['cal_nov'] = 'Nov';
+$lang['cal_dec'] = 'Dec';
+$lang['cal_january'] = 'Januari';
+$lang['cal_february'] = 'Februari';
+$lang['cal_march'] = 'Mars';
+$lang['cal_april'] = 'April';
+$lang['cal_mayl'] = 'Maj';
+$lang['cal_june'] = 'Juni';
+$lang['cal_july'] = 'Juli';
+$lang['cal_august'] = 'Augusti';
+$lang['cal_september'] = 'September';
+$lang['cal_october'] = 'Oktober';
+$lang['cal_november'] = 'November';
+$lang['cal_december'] = 'December';
---
+++ system/language/swedish/form_validation_lang.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= 'Fältet {field} är obligatoriskt.';
+$lang['form_validation_isset']			= 'Fältet {field} måste ha ett värde.';
+$lang['form_validation_valid_email']		= 'Fältet {field} måste innehålla en godkänd e-postadress.';
+$lang['form_validation_valid_emails']		= 'Fältet {field} måste innehålla alla godkända e-postadresser.';
+$lang['form_validation_valid_url']		= 'Fältet {field} måste vara en godkänd URL.';
+$lang['form_validation_valid_ip']		= 'Fältet {field} måste vara en godkänd IP-adress.';
+$lang['form_validation_valid_mac']		= 'Fältet {field} måste vara en godkänd MAC-adress.';
+$lang['form_validation_valid_base64']		= 'Fältet {field} måste vara en godkänd Base64 sträng.';
+$lang['form_validation_min_length']		= 'Fältet {field} måste vara minst {param} char längd.';
+$lang['form_validation_max_length']		= 'Fältet {field} får inte vara längre än {param} char.';
+$lang['form_validation_exact_length']		= 'Fältet {field} måste vara exakt {param} char i längd.';
+$lang['form_validation_alpha']			= 'Fältet {field} får bara innehålla bokstäver.';
+$lang['form_validation_alpha_numeric']		= 'Fältet {field} får bara innehålla siffror.';
+$lang['form_validation_alpha_numeric_spaces']	= 'Fältet {field} får bara innehålla siffror och mellanslag.';
+$lang['form_validation_alpha_dash']		= 'Fältet {field} får bara innehålla siffror och mellanslag, underscore och punkter.';
+$lang['form_validation_numeric']		= 'Fältet {field} får bara innehålla siffror.';
+$lang['form_validation_is_numeric']		= 'Fältet {field} får bara innehålla siffror.';
+$lang['form_validation_integer']		= 'Fältet {field} måste vara en integer.';
+$lang['form_validation_regex_match']		= 'Fältet {field} är inte i korrekt format.';
+$lang['form_validation_matches']		= 'Fältet {field} är inte samma som {param} fältet.';
+$lang['form_validation_differs']		= 'Fältet {field} får int evara samma som {param} fältet.';
+$lang['form_validation_is_unique'] 		= 'Fältet {field} måste vara ett unikt värde.';
+$lang['form_validation_is_natural']		= 'Fältet {field} kan bara innehålla siffror.';
+$lang['form_validation_is_natural_no_zero']	= 'Fältet {field} måste vara siffror större än 0 (noll).';
+$lang['form_validation_decimal']		= 'Fältet {field} måste vara decimaler.';
+$lang['form_validation_less_than']		= 'Fältet {field} måste vara en siffra mindre än {param}.';
+$lang['form_validation_less_than_equal_to']	= 'Fältet {field} måste vara en siffra mindre än eller lika {param}.';
+$lang['form_validation_greater_than']		= 'Fältet {field} måste vara en siffra större än {param}.';
+$lang['form_validation_greater_than_equal_to']	= 'Fältet {field} måste vara en siffra större än eller lika {param}.';
+$lang['form_validation_error_message_not_set']	= 'Hittar inget felmeddelande för fältet {field}.';
+$lang['form_validation_in_list']		= 'Fältet {field} måste vara en av: {param}.';
