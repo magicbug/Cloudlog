a3d290d52aa63606073a0e88cfc7b6a00bdad3bf
francescocozzi <francesco@iz7khr.it>
2022-10-17 18:23:25 +0200

    Italian translation

---
+++ application/language/italian/qso_lang.php
@@ -0,0 +1,33 @@
+<?php
+
+defined('BASEPATH') OR exit('Non è permesso l"'"accesso diretto allo script');
+
+// Tiles
+$lang['qso_title_qso_map'] = 'Mappa QSO';
+$lang['qso_title_suggestions'] = 'Suggerimenti';
+$lang['qso_title_pervious_contacts'] = 'Contatti Precedenti';
+$lang['qso_title_image'] = 'Immagine Profilo';
+
+// Input Help Text on the /QSO Display
+$lang['qso_transmit_power_helptext'] = 'Dare valore di potenza in Watt. Includere solo numeri nell input.';
+
+$lang['qso_sota_ref_helptext'] = 'Per esempio: GM/NS-001.';
+$lang['qso_wwff_ref_helptext'] = 'Per esempio: DLFF-0069.';
+
+$lang['qso_sig_helptext'] = 'Per esempio: POTA';
+$lang['qso_sig_info_helptext'] = 'Per esempio: PA-0150';
+
+$lang['qso_dok_helptext'] = 'Per esempio: Q03';
+
+$lang['qso_notes_helptext'] = 'Il contenuto della nota viene utilizzato solo all interno di Cloudlog e non viene esportato in altri servizi.';
+$lang['qsl_notes_helptext'] = 'Il contenuto di questa nota è esportato a servizi QSL come eqsl.cc.';
+
+// Button Text on /qso Display
+
+$lang['qso_btn_reset_qso'] = 'Reset';
+$lang['qso_btn_save_qso'] = 'Salva QSO';
+$lang['qso_btn_edit_qso'] = 'Modifica QSO';
+
+// QSO Details
+
+$lang['qso_details'] = 'Dettagli QSO';
