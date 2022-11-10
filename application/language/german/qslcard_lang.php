c416e9b8e49be7c4d75995012988e4b82506f804
phl0 <github@florian-wolters.de>
2022-11-10 14:45:40 +0100

    Add more QSL details for E and M

--- application/language/german/qslcard_lang.php
+++ application/language/german/qslcard_lang.php
@@ -6,10 +6,16 @@ defined('BASEPATH') OR exit('Direkter Zugriff auf Skripte ist nicht erlaubt');
 $lang['qslcard_string_your_are_using'] = 'Du benutzt';
 $lang['qslcard_string_disk_space'] = 'an Speicherplatz, um QSL-Karteninformationen zu speichern';
 $lang['qslcard_info'] = 'QSL Info';
+$lang['qslcard_sent'] = 'QSL Karte wurde gesendet am';
 $lang['qslcard_sent_bureau'] = 'QSL Karte wurde via Büro gesendet am';
 $lang['qslcard_sent_direct'] = 'QSL Karte wurde direkt gesendet am';
+$lang['qslcard_sent_electronic'] = 'QSL Karte wurde elektronisch gesendet am';
+$lang['qslcard_sent_manager'] = 'QSL Karte wurde via Manager gesendet am';
+$lang['qslcard_rcvd'] = 'QSL Karte wurde empfangen am';
 $lang['qslcard_rcvd_bureau'] = 'QSL Karte wurde via Büro empfangen am';
 $lang['qslcard_rcvd_direct'] = 'QSL Karte wurde direkt empfangen am';
+$lang['qslcard_rcvd_electronic'] = 'QSL Karte wurde elektronisch empfangen am';
+$lang['qslcard_rcvd_manager'] = 'QSL Karte wurde via Manager empfangen am';
 
 $lang['qslcard_upload_front'] = 'QSL Kartenfront hochgeladen';
 $lang['qslcard_upload_back'] = 'QSL Kartenrückseite hochgeladen';
