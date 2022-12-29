93b1f847a3b8ed82d79be63f73d46bf5f403512a
phl0 <github@florian-wolters.de>
2022-12-29 18:03:48 +0100

    Add check for empty message

--- application/views/email/oqrs_request.php
+++ application/views/email/oqrs_request.php
@@ -1,9 +1,11 @@
 Hi,
 
 You got an OQRS request from <?php echo strtoupper($callsign); ?>.
+<?php if ($usermessage != "") { ?>
 The user entered the following message:
 
 <?php echo $usermessage."\n"; ?>
+<?php } ?>
 
 Please log into your Cloudlog and process it.
 
