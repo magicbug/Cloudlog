d81f6d6283d9fa617879cde016f848cae22877c5
slayingripper <kirav96@gmail.com>
2023-01-18 19:10:44 +0000

    Added Greek translation

---
+++ application/language/greek/qso_lang.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Tiles
+$lang['qso_title_qso_map'] = 'Χάρτης QSO';
+$lang['qso_title_suggestions'] = 'Εισήγησης';
+$lang['qso_title_previous_contacts'] = 'Προηγούμενες Επαφές';
+$lang['qso_title_image'] = 'Εικόνα προφίλ';
+
+// Input Help Text on the /QSO Display
+$lang['qso_transmit_power_helptext'] = 'Δώστε τιμή ισχύος σε Watts. Συμπεριλάβετε μόνο αριθμούς στην εισαγωγή.';
+
+$lang['qso_sota_ref_helptext'] = 'Για παράδειγμα: GM/NS-001.';
+$lang['qso_wwff_ref_helptext'] = 'Για παράδειγμα: DLFF-0069.';
+$lang['qso_pota_ref_helptext'] = 'Για παράδειγμα: PA-0150.';
+
+$lang['qso_sig_helptext'] = 'Για παράδειγμα: GMA';
+$lang['qso_sig_info_helptext'] = 'Για παράδειγμα: DA/NW-357';
+
+$lang['qso_dok_helptext'] = 'Για παράδειγμα: Q03';
+
+$lang['qso_notes_helptext'] = 'Το περιεχόμενο σημείωσης χρησιμοποιείται μόνο στο Cloudlog και δεν εξάγεται σε άλλες υπηρεσίες.';
+$lang['qsl_notes_helptext'] = 'Αυτό το περιεχόμενο σημείωσης εξάγεται σε υπηρεσίες QSL όπως το eqsl.cc.';
+
+// Button Text on /qso Display
+
+$lang['qso_btn_reset_qso'] = 'Επαναφορά';
+$lang['qso_btn_save_qso'] = 'Αποθήκευση QSO';
+$lang['qso_btn_edit_qso'] = 'Επεξεργασία QSO';
+
+// QSO Details
+
+$lang['qso_details'] = 'Στοιχεία QSO';
