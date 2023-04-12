695bf7caa727e61529d322916e2c8e8427326bbd
Andreas <6977712+AndreasK79@users.noreply.github.com>
2023-04-12 15:09:34 +0200

    [Cabrillo export] Increased station location width

--- application/views/cabrillo/index.php
+++ application/views/cabrillo/index.php
@@ -19,7 +19,7 @@
 			<form class="form" action="<?php echo site_url('cabrillo/export'); ?>" method="post" enctype="multipart/form-data">
 				<div class="form-group form-inline row">
 					<div class="col-md-3 control-label" for="station_id">Select Station Location: </div>
-					<select id="station_id" name="station_id" class="custom-select my-1 mr-sm-2 col-md-3">
+					<select id="station_id" name="station_id" class="custom-select my-1 mr-sm-2 col-md-4">
 					<?php foreach ($station_profile->result() as $station) { ?>
 						<option value="<?php echo $station->station_id; ?>" <?php if ($station->station_id == $this->stations->find_active()) { echo " selected =\"selected\""; } ?>>Callsign: <?php echo $station->station_callsign; ?> (<?php echo $station->station_profile_name; ?>)</option>
 					<?php } ?>
