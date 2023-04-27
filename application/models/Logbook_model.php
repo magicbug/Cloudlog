90f64e6d7a96839cae92d89bf355f352becf36b7
phl0 <github@florian-wolters.de>
2023-04-27 09:26:43 +0200

    Add DXCC entities details to activators details SQL

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -443,6 +443,7 @@ class Logbook_model extends CI_Model {
 		$logbooks_locations_array = $CI->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
 
       $this->db->join('station_profile', 'station_profile.station_id = '.$this->config->item('table_name').'.station_id');
+      $this->db->join('dxcc_entities', 'dxcc_entities.adif = '.$this->config->item('table_name').'.COL_DXCC', 'left outer');
         $this->db->where('COL_CALL', $call);
         if ($band != 'All') {
             if ($band == 'SAT') {
@@ -460,6 +461,7 @@ class Logbook_model extends CI_Model {
         }
 
         $this->db->where_in('station_profile.station_id', $logbooks_locations_array);
+        $this->db->order_by('COL_TIME_ON', 'DESC');
 
         return $this->db->get($this->config->item('table_name'));
     }
