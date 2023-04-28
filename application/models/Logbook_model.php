515fe68f6b0d31d9cb1ce68e4dfecf2710f175c9
phl0 <github@florian-wolters.de>
2023-04-28 14:30:24 +0200

    Use LEFT JOIN and COALESCE for station country

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -1296,10 +1296,11 @@ class Logbook_model extends CI_Model {
 
   function get_qso($id) {
     $this->db->select($this->config->item('table_name').'.*, station_profile.*, dxcc_entities.*, dxcc_entities_2.name as station_country, dxcc_entities_2.end as station_end');
+    $this->db->select($this->config->item('table_name').'.*, station_profile.*, dxcc_entities.*, coalesce(dxcc_entities_2.name, "None") as station_country, dxcc_entities_2.end as station_end');
     $this->db->from($this->config->item('table_name'));
     $this->db->join('dxcc_entities', $this->config->item('table_name').'.col_dxcc = dxcc_entities.adif', 'left');
     $this->db->join('station_profile', 'station_profile.station_id = '.$this->config->item('table_name').'.station_id', 'left');
-    $this->db->join('dxcc_entities as dxcc_entities_2', 'station_profile.station_dxcc = dxcc_entities_2.adif');
+    $this->db->join('dxcc_entities as dxcc_entities_2', 'station_profile.station_dxcc = dxcc_entities_2.adif', 'left outer');
     $this->db->where('COL_PRIMARY_KEY', $id);
 
     return $this->db->get();
