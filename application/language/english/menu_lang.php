97988496c9d79128f84c5655de239b4a09e59106
Andreas <6977712+AndreasK79@users.noreply.github.com>
2023-05-11 09:59:07 +0200

    [Translation] Translated the menu and header

---
+++ application/language/english/menu_lang.php
@@ -0,0 +1,78 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['menu_badge_developer_mode'] = 'Developer Mode';
+
+$lang['menu_logbook'] = 'Logbook';
+$lang['menu_overview'] = 'Overview';
+$lang['menu_advanced'] = 'Advanced';
+
+$lang['menu_qso'] = 'QSO';
+$lang['menu_live_qso'] = 'Live QSO';
+$lang['menu_post_qso'] = 'Post QSO';
+$lang['menu_live_contest_logging'] = 'Live Contest Logging';
+$lang['menu_post_contest_logging'] = 'Post Contest Logging';
+$lang['menu_view_qsl'] = 'View QSL';
+
+$lang['menu_analytics'] = 'Analytics';
+$lang['menu_statistics'] = 'Statistics';
+$lang['menu_gridsquares'] = 'Gridsquares';
+$lang['menu_activated_gridsquares'] = 'Activated Gridsquares';
+$lang['menu_gridsquare_activators'] = 'Gridsquare Activators';
+$lang['menu_distances_worked'] = 'Distances Worked';
+$lang['menu_days_with_qsos'] = 'Days with QSOs';
+$lang['menu_timeline'] = 'Timeline';
+$lang['menu_accumulated_statistics'] = 'Accumulated Statistics';
+$lang['menu_timeplotter'] = 'Timeplotter';
+$lang['menu_custom_maps'] = 'Custom Maps';
+$lang['menu_continents'] = 'Continents';
+
+$lang['menu_awards'] = 'Awards';
+$lang['menu_cq'] = 'CQ';
+$lang['menu_dok'] = 'DOK';
+$lang['menu_dxcc'] = 'DXCC';
+$lang['menu_iota'] = 'IOTA';
+$lang['menu_pota'] = 'POTA';
+$lang['menu_sig'] = 'SIG';
+$lang['menu_sota'] = 'SOTA';
+$lang['menu_us_counties'] = 'US Counties';
+$lang['menu_vucc'] = 'VUCC';
+$lang['menu_was'] = 'WAS';
+$lang['menu_wwff'] = 'WWFF';
+
+$lang['menu_admin'] = 'Admin';
+$lang['menu_user_account'] = 'User Accounts';
+$lang['menu_global_options'] = 'Global Options';
+$lang['menu_modes'] = 'Modes';
+$lang['menu_contests'] = 'Contests';
+$lang['menu_themes'] = 'Themes';
+$lang['menu_backup'] = 'Backup';
+$lang['menu_update_country_files'] = 'Update Country Files';
+$lang['menu_debug_information'] = 'Debug Information';
+
+$lang['menu_search_text'] = 'Search Callsign';
+
+$lang['menu_search_button'] = 'Search';
+$lang['menu_login_button'] = 'Login';
+
+$lang['menu_account'] = 'Account';
+$lang['menu_station_logbooks'] = 'Station Logbooks';
+$lang['menu_station_locations'] = 'Station Locations';
+$lang['menu_bands'] = 'Bands';
+$lang['menu_adif_import_export'] = 'ADIF Import / Export';
+$lang['menu_kml_export'] = 'KML Export';
+$lang['menu_dx_atlas_gridsquare_export'] = 'DX Atlas Gridsquare Export';
+$lang['menu_sota_csv_export'] = 'SOTA CSV Export';
+$lang['menu_cabrillo_export'] = 'Cabrillo Export';
+$lang['menu_oqrs_requests'] = 'OQRS Requests';
+$lang['menu_print_requested_qsls'] = 'Print Requested QSLs';
+$lang['menu_logbook_of_the_world'] = 'Logbook of the World';
+$lang['menu_eqsl_import_export'] = 'eQSL Import / Export';
+$lang['menu_qrz_logbook'] = 'QRZ Logbook';
+$lang['menu_qo_100_dx_club_upload'] = 'QO-100 Dx Club Upload';
+$lang['menu_api_keys'] = 'API Keys';
+$lang['menu_hardware_interfaces'] = 'Hardware Interfaces';
+$lang['menu_help'] = 'Help';
+$lang['menu_forum'] = 'Forum';
+$lang['menu_logout'] = 'Logout';
