bda839b0b2a20a3b48b1669680fb5d2379af2d77
phl0 <github@florian-wolters.de>
2023-05-21 22:50:30 +0200

    Show big banner re eQSL rate limit

--- application/views/eqsl/analysis.php
+++ application/views/eqsl/analysis.php
@@ -17,7 +17,7 @@ $custom_date_format = $this->session->userdata('user_date_format');
         <a class="nav-link" href="<?php echo site_url('eqsl/tools');?>">Tools</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo site_url('eqsl/download');?>">Download eQSL Images</a>
+        <a class="nav-link" href="<?php echo site_url('eqsl/download');?>">Download eQSL cards</a>
       </li>
     </ul>
   </div>
