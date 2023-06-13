b80980b42db4b67d6be60cf0b3c01a355fb9a427
Andreas <6977712+AndreasK79@users.noreply.github.com>
2023-06-13 20:38:29 +0200

    [OQRS] Added so that enter works for submit in email input

--- assets/js/sections/oqrs.js
+++ assets/js/sections/oqrs.js
@@ -71,6 +71,20 @@ function searchOqrsGrouped() {
                 "paging": false,
                 "scrollX": true,
             });
+
+            // Get the input field
+            var input = document.getElementById("emailInput");
+
+            // Execute a function when the user presses a key on the keyboard
+            input.addEventListener("keypress", function(event) {
+            // If the user presses the "Enter" key on the keyboard
+            if (event.key === "Enter") {
+                // Cancel the default action, if needed
+                event.preventDefault();
+                // Trigger the button element with a click
+                document.getElementById("requestGroupedSubmit").click();
+            }
+            });
         }
     });
 }
@@ -191,6 +205,19 @@ function requestOqrs() {
                 "scrollCollapse": true,
                 "paging": false,
                 "scrollX": true,
+            });
+            // Get the input field
+            var input = document.getElementById("emailInput");
+
+            // Execute a function when the user presses a key on the keyboard
+            input.addEventListener("keypress", function(event) {
+            // If the user presses the "Enter" key on the keyboard
+            if (event.key === "Enter") {
+                // Cancel the default action, if needed
+                event.preventDefault();
+                // Trigger the button element with a click
+                document.getElementById("requestSubmit").click();
+            }
             });
         }
     }); 
