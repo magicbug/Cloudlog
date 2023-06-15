b319b190f37b2598cf516d1f731494d4aa3c90dd
Peter Goodhall <peter@magicbug.co.uk>
2023-06-15 13:14:10 +0100

    [Station Location][Edit] Allows selecting and editing Canadian Provinces
    Fixes issue #2190

--- application/models/Stations.php
+++ application/models/Stations.php
@@ -97,6 +97,12 @@ class Stations extends CI_Model {
 	}
 
 	function edit() {
+
+		if ($this->input->post('dxcc') == 1 && $this->input->post('station_ca_state') !="") {
+			$state = $this->input->post('station_ca_state');
+		} else {
+			$state = $this->input->post('station_state');
+		}
 		$data = array(
 			'station_profile_name' => xss_clean($this->input->post('station_profile_name', true)),
 			'station_gridsquare' => xss_clean($this->input->post('gridsquare', true)),
@@ -113,7 +119,7 @@ class Stations extends CI_Model {
 			'station_cnty' => xss_clean($this->input->post('station_cnty', true)),
 			'station_cq' => xss_clean($this->input->post('station_cq', true)),
 			'station_itu' => xss_clean($this->input->post('station_itu', true)),
-			'state' => xss_clean($this->input->post('station_state', true)),
+			'state' => $state,
 			'eqslqthnickname' => xss_clean($this->input->post('eqslnickname', true)),
             'qrzapikey' => xss_clean($this->input->post('qrzapikey', true)),
             'qrzrealtime' => xss_clean($this->input->post('qrzrealtime', true)),
--- assets/js/sections/station_locations.js
+++ assets/js/sections/station_locations.js
@@ -1,5 +1,30 @@
 $(document).ready( function () {
+
+    // Use Jquery to hide div ca_state
+
     $('#station_locations_table').DataTable({
         "stateSave": true
+    });
+
+    $("#canada_state").hide();
+
+    var selectedDXCCID = $('#dxcc_select').find(":selected").val();
+
+    console.log(selectedDXCCID);
+
+    if(selectedDXCCID == '1'){
+        $("#canada_state").show();
+        $("#us_state").hide();
+    }
+
+    $('#dxcc_select').change(function(){
+        if($(this).val() == '1'){ // or this.value == 'volvo'
+          console.log("CANADA!");
+          $("#canada_state").show();
+          $("#us_state").hide();
+        } else {
+            $("#canada_state").hide();
+            $("#us_state").show();
+        }
     });
 } );
