c25a2272389c7ec883db3fb2afad0ff4ae3b23c1
phl0 <github@florian-wolters.de>
2023-06-21 13:16:12 +0200

    German translation for gridmap

--- application/language/german/gridsquares_lang.php
+++ application/language/german/gridsquares_lang.php
@@ -12,3 +12,15 @@ $lang['gridsquares_this_map_does_not_include_satellite_internet_or_repeater_qsos
 
 $lang['gridsquares_grid_squares'] = 'Planquadrate';
 $lang['gridsquares_total_count'] = 'Summe';
+
+$lang['gridsquares_band'] = 'Band';
+$lang['gridsquares_mode'] = 'Mode';
+$lang['gridsquares_sat'] = 'Satellit';
+$lang['gridsquares_confirmation'] = 'Bestätigung';
+
+$lang['gridsquares_button_plot'] = 'Kartieren';
+
+$lang['gridsquares_gridsquares'] = 'Planquadrate';
+$lang['gridsquares_gridsquares_confirmed'] = 'Planquadrate bestätigt';
+$lang['gridsquares_gridsquares_not_confirmed'] = 'Planquadrate nicht bestätigt';
+$lang['gridsquares_gridsquares_total_worked'] = 'Summe gearbeiteter Planquadrate';
