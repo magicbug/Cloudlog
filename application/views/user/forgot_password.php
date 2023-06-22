8a75a2bfdea529ec551abf6c4fa3569e62613614
Peter Goodhall <peter@magicbug.co.uk>
2023-06-22 16:12:31 +0100

    [Forgot Password] Set the email field as the automatic focus

--- application/views/user/forgot_password.php
+++ application/views/user/forgot_password.php
@@ -44,7 +44,7 @@ input[type="email"] {
                         <div class="form-group">
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
-                                <input id="email" name="email" placeholder="<?php echo lang('account_email_address'); ?>" class="form-control" type="email">
+                                <input id="email" name="email" placeholder="<?php echo lang('account_email_address'); ?>" class="form-control" type="email" autofocus>
                             </div>
                         </div>
                         <div class="form-group">
