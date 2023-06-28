e2b442d39ce0b51aaca51b1d6ea1b2bc31dfc401
Andreas <6977712+AndreasK79@users.noreply.github.com>
2023-06-28 09:17:48 +0200

    [Advanced Logbook] Fixed update query

--- application/models/Logbookadvanced_model.php
+++ application/models/Logbookadvanced_model.php
@@ -132,10 +132,11 @@ class Logbookadvanced_model extends CI_Model {
 		$order = $this->getSortorder($sortorder);
 
         $sql = "
-            SELECT *, dxcc_entities.name AS station_country
+            SELECT qsos.*, d2.*, lotw_users.*, station_profile.*, dxcc_entities.name AS station_country
 			FROM " . $this->config->item('table_name') . " qsos
 			INNER JOIN station_profile ON qsos.station_id = station_profile.station_id
 			LEFT OUTER JOIN dxcc_entities ON qsos.COL_MY_DXCC = dxcc_entities.adif
+			LEFT OUTER JOIN dxcc_entities d2 ON qsos.COL_DXCC = d2.adif
 			LEFT OUTER JOIN lotw_users ON qsos.col_call=lotw_users.callsign
 			WHERE station_profile.user_id =  ?
 			$where
