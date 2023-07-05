782857aef9f5b1b48950737a46c10354c9386397
int2001 <int2001@users.noreply.github.com>
2023-07-05 12:05:02 +0000

    Fixes Issue 2198

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -519,7 +519,8 @@ class Logbook_model extends CI_Model {
 
 			$adif = $CI->adifhelper->getAdifLine($qso[0]);
 			$result = $this->push_qso_to_qrz($result->qrzapikey, $adif);
-			if ($result['status'] == 'OK') {
+			log_message("error","Oink: ".$result['message']);
+			if ( ($result['status'] == 'OK') || ( ($result['status'] == 'error') && ($result['message'] == 'STATUS=FAIL&REASON=Unable to add QSO to database: duplicate&EXTENDED=')) ){
 				$this->mark_qrz_qsos_sent($last_id);
 			}
 		}
