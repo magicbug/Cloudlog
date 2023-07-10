23c75017ec2bc938b722e6667d69aa67bb361284
phl0 <github@florian-wolters.de>
2023-07-10 22:57:07 +0200

    Add forgotten file

---
+++ application/views/public_search/empty.php
@@ -0,0 +1,8 @@
+<div class="container publicsearch">
+<h1>Results <small class="text-muted">Searching for <?php echo str_replace("0","&Oslash;",strtoupper($callsign)); ?></small></h1>
+	<div class="card text-center">
+		<div class="card-body">
+			Nothing found. Please try again!
+		</div>
+	</div>
+</div>
