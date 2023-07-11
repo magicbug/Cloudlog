b54b13d9aa6ff8857a2b62979df03989ed8c2b01
phl0 <github@florian-wolters.de>
2023-07-11 08:42:08 +0200

    Show banner for empty result

--- application/views/public_search/empty.php
+++ application/views/public_search/empty.php
@@ -1,8 +1,4 @@
 <div class="container publicsearch">
 <h1>Results <small class="text-muted">Searching for <?php echo str_replace("0","&Oslash;",strtoupper($callsign)); ?></small></h1>
-	<div class="card text-center">
-		<div class="card-body">
-			Nothing found. Please try again!
-		</div>
-	</div>
+	<div class="alert alert-danger" role="alert"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Nothing found!</div>
 </div>
