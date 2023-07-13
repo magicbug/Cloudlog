57f5ea1c9261212080feebb4b4ea6db2af860a94
phl0 <github@florian-wolters.de>
2023-07-13 21:13:11 +0200

    Set QSL method for requested QSLs

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -1314,7 +1314,8 @@ class Logbook_model extends CI_Model {
 
     $data = array(
          'COL_QSLSDATE' => date('Y-m-d H:i:s'),
-         'COL_QSL_SENT' => 'R'
+         'COL_QSL_SENT' => 'R',
+         'COL_QSL_SENT_VIA' => $method
     );
 
     $this->db->where('COL_PRIMARY_KEY', $qso_id);
