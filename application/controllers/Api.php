cfc9b6c8a06f0649097cd9f741b4d1e2cde71bdc
int2001 <int2001@users.noreply.github.com>
2023-07-14 07:49:13 +0000

    Removed useless else at Api because script will never get there

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -462,8 +462,6 @@ class API extends CI_Controller {
 
 				if(isset($obj['station_profile_id'])) {
 					$this->logbook_model->import($record, $obj['station_profile_id'], NULL, NULL, NULL, NULL, NULL, false, false, true);
-				} else {
-					$this->logbook_model->import($record, 0, NULL, NULL, NULL, NULL, NULL, false, false, true);
 				}
 
 			};
