81dac90266d545ae2d92407c3b8a5dd1ad8234a1
int2001 <int2001@users.noreply.github.com>
2023-07-21 09:16:19 +0000

    Made Band chooseable

--- application/views/bandmap/index.php
+++ application/views/bandmap/index.php
@@ -1,9 +1,31 @@
+<script>
+	var dxcluster_provider="/index.php/dxcluster";
+</script>
+
 <div class="container">
 
 <br>
 
 <h2><?php echo $page_title; ?></h2>
+                <div class="form-group col-md-6">
+                  <label for="band"><?php echo lang('gen_hamradio_band'); ?></label>
 
+                  <select id="band" class="form-control form-control-sm" name="band">
+                  <?php 
+			$bands = $this->bands->get_user_bands_for_qso_entry();	
+			foreach($bands as $key=>$bandgroup) {
+                          echo '<optgroup label="' . strtoupper($key) . '">';
+                          foreach($bandgroup as $band) {
+                            echo '<option value="' . $band . '"';
+                              if ($band == "20m") echo ' selected';
+                              echo '>' . $band . '</option>'."\n";
+                          }
+                          echo '</optgroup>';
+                        }
+                  ?>
+                  </select>
+                </div>
+	
 <figure class="highcharts-figure">
     <div id="bandmap"></div>
     <p class="highcharts-description">
