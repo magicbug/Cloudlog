c8ea174030895a148929468bcc183ab5565fbfe9
int2001 <joerg@dj7nt.de>
2023-08-02 06:34:12 +0000

    Implemented Userlanguages with a lot of help from @AndreasK79

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 132;
+$config['migration_version'] = 133;
 
 /*
 |--------------------------------------------------------------------------
