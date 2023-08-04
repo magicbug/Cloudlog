69ed49cbdf815d3f0281a898fcfaeed8efb70590
Ondrej Kolonicny <ondra@ok1cdj.com>
2023-08-04 14:15:59 +0200

    Create menu_lang.php in Czech

---
+++ application/language/czech/menu_lang.php
@@ -0,0 +1,84 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['menu_badge_developer_mode'] = 'Režim vývojáře';
+
+$lang['menu_logbook'] = 'Logbook';
+$lang['menu_overview'] = 'Přehled';
+$lang['menu_advanced'] = 'Pokročilé zobrazení';
+
+$lang['menu_qso'] = 'QSO';
+$lang['menu_live_qso'] = 'Živé QSO';
+$lang['menu_post_qso'] = 'Uložit QSO';
+$lang['menu_live_contest_logging'] = 'Živé závodní logování';
+$lang['menu_post_contest_logging'] = 'Uložit závodní log';
+$lang['menu_view_qsl'] = 'Zobrazit QSL';
+$lang['menu_view_eqsl'] = 'Zobrazit eQSL';
+
+$lang['menu_notes'] = 'Poznámky';
+
+$lang['menu_analytics'] = 'Analýza';
+$lang['menu_statistics'] = 'Statistiky';
+$lang['menu_gridsquares'] = 'Lokátory';
+$lang['menu_gridmap'] = 'Mapa lokátorů';
+$lang['menu_activated_gridsquares'] = 'Aktivované lokátory';
+$lang['menu_gridsquare_activators'] = 'Aktivátoři lokátorů';
+$lang['menu_distances_worked'] = 'Vzdálenosti';
+$lang['menu_days_with_qsos'] = 'Dny s QSO';
+$lang['menu_timeline'] = 'Časová osa';
+$lang['menu_accumulated_statistics'] = 'Kumulativní statistiky';
+$lang['menu_timeplotter'] = 'Graf času';
+$lang['menu_custom_maps'] = 'Vlastní mapy';
+$lang['menu_continents'] = 'Kontinenty';
+
+$lang['menu_awards'] = 'Diplomy';
+$lang['menu_cq'] = 'CQ';
+$lang['menu_dok'] = 'DOK';
+$lang['menu_dxcc'] = 'DXCC';
+$lang['menu_iota'] = 'IOTA';
+$lang['menu_pota'] = 'POTA';
+$lang['menu_sig'] = 'SIG';
+$lang['menu_sota'] = 'SOTA';
+$lang['menu_us_counties'] = 'US okresy';
+$lang['menu_vucc'] = 'VUCC';
+$lang['menu_was'] = 'WAS';
+$lang['menu_wwff'] = 'WWFF';
+
+$lang['menu_admin'] = 'Admin';
+$lang['menu_user_account'] = 'Uživatelské účty';
+$lang['menu_global_options'] = 'Globální nastavení';
+$lang['menu_modes'] = 'Režimy';
+$lang['menu_contests'] = 'Závody';
+$lang['menu_themes'] = 'Témata';
+$lang['menu_backup'] = 'Zálohování';
+$lang['menu_update_country_files'] = 'Aktualizace souborů';
+$lang['menu_debug_information'] = 'Informace pro ladění';
+
+$lang['menu_search_text'] = 'Hledat značku';
+
+$lang['menu_search_button'] = 'Hledat';
+$lang['menu_login_button'] = 'Přihlásit se';
+
+$lang['menu_account'] = 'Účet';
+$lang['menu_station_logbooks'] = 'Logy stanice';
+$lang['menu_station_locations'] = 'Umístění stanice';
+$lang['menu_bands'] = 'Pásmo';
+$lang['menu_adif_import_export'] = 'ADIF import / export';
+$lang['menu_kml_export'] = 'KML export';
+$lang['menu_dx_atlas_gridsquare_export'] = 'DX Atlas export lokátorů';
+$lang['menu_sota_csv_export'] = 'SOTA CSV export';
+$lang['menu_cabrillo_export'] = 'Cabrillo export';
+$lang['menu_oqrs_requests'] = 'OQRS požadavky';
+$lang['menu_print_requested_qsls'] = 'Tisk žádaných QSL';
+$lang['menu_labels'] = 'Štítky';
+$lang['menu_logbook_of_the_world'] = 'Logbook of the World';
+$lang['menu_eqsl_import_export'] = 'eQSL import / export';
+$lang['menu_qrz_logbook'] = 'QRZ Logbook';
+$lang['menu_hrd_logbook'] = 'HRDLog Logbook';
+$lang['menu_qo_100_dx_club_upload'] = 'QO-100 Dx Club Upload';
+$lang['menu_api_keys'] = 'API klíče';
+$lang['menu_hardware_interfaces'] = 'Hardwarové rozhraní';
+$lang['menu_help'] = 'Nápověda';
+$lang['menu_forum'] = 'Fórum';
+$lang['menu_logout'] = 'Odhlásit se';
