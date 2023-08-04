b2fbde9d6a1195aa4fd56fc187dff830f61884e7
Ondrej Kolonicny <ondra@ok1cdj.com>
2023-08-04 14:28:10 +0200

    Update general_words_lang.php in Czech

--- application/language/czech/general_words_lang.php
+++ application/language/czech/general_words_lang.php
@@ -2,9 +2,9 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['error_no_active_station_profile'] = 'Pozor: je třeba mít nastaven aktivní profil stanice.';
+$lang['error_no_active_station_profile'] = 'Pozor: musíte nastavit aktivní umístění stanice.';
 
-$lang['notice_turn_the_radio_on'] = 'Dnes žádné spojení, je čas zapnout rádio!';
+$lang['notice_turn_the_radio_on'] = 'Dnes jste neudělali žádná QSO spojení; je čas zapnout rádio!';
 
 $lang['general_word_important'] = 'Důležité';
 $lang['general_word_info'] = 'Info';
@@ -12,45 +12,59 @@ $lang['general_word_choose_file'] = 'Vybrat soubor';
 
 $lang['general_word_date'] = 'Datum';
 $lang['general_word_time'] = 'Čas';
-$lang['general_word_none'] = 'Nic';
+$lang['general_word_datetime'] = 'Datum/čas';
+$lang['general_word_none'] = 'Žádné';
 $lang['general_word_name'] = 'Jméno';
-$lang['general_word_location'] = 'Location';
+$lang['general_word_location'] = 'Místo';
 $lang['general_word_comment'] = 'Komentář';
-$lang['general_word_general'] = 'Všeobecné';
+$lang['general_word_general'] = 'Obecné';
 $lang['general_word_satellite'] = 'Satelit';
 $lang['general_word_satellite_short'] = 'Sat';
 $lang['general_word_notes'] = 'Poznámky';
-$lang['general_word_comment'] = 'Obsah';
 $lang['general_word_country'] = 'Země';
 
 $lang['general_word_total'] = 'Celkem';
 $lang['general_word_year'] = 'Rok';
 $lang['general_word_month'] = 'Měsíc';
 
-$lang['general_word_worked'] = 'Pracováno';
+$lang['general_word_worked'] = 'Spojeno';
 $lang['general_word_confirmed'] = 'Potvrzeno';
-$lang['general_word_needed'] = 'Potřeba';
+$lang['general_word_needed'] = 'Potřebuje se';
 
 $lang['general_word_no'] = 'Ne';
 $lang['general_word_yes'] = 'Ano';
-$lang['general_word_method'] = 'Method';
+$lang['general_word_method'] = 'Metoda';
 
 $lang['general_word_sent'] = 'Odesláno';
 $lang['general_word_received'] = 'Přijato';
-$lang['general_word_requested'] = 'Žádáno';
-$lang['general_word_qslcards'] = 'QSL lístků';
-$lang['general_word_qslcard_direct'] = 'Direct';
+$lang['general_word_requested'] = 'Vyžádáno';
+$lang['general_word_queued'] = 'Ve frontě';
+$lang['general_word_invalid_ignore'] = 'Neplatné (Ignorováno)';
+$lang['general_word_qslcard'] = 'QSL karta';
+$lang['general_word_qslcard_management'] = 'Správa QSL';
+$lang['general_word_qslcards'] = 'QSL karty';
+$lang['general_word_qslcard_direct'] = 'Přímá';
 $lang['general_word_qslcard_bureau'] = 'Bureau';
-$lang['general_word_qslcard_via'] = 'Via';
+$lang['general_word_qslcard_electronic'] = 'Elektronická';
+$lang['general_word_qslcard_manager'] = 'Správce';
+$lang['general_word_qslcard_via'] = 'Přes';
+$lang['general_word_eqslcard'] = 'eQSL karta';
+$lang['general_word_eqslcards'] = 'eQSL karty';
+$lang['general_word_lotw'] = 'Logbook of the World';
 
-$lang['general_edit_qso'] = 'Editovat QSO';
-$lang['general_mark_qsl_rx_bureau'] = 'Označit QSL jako přijaté (Bureau)';
-$lang['general_mark_qsl_rx_direct'] = 'Označit QSL jako přijaté (Direct)';
+$lang['general_edit_qso'] = 'Upravit QSO';
+$lang['general_mark_qsl_rx_bureau'] = 'Označit QSL přijato (Bureau)';
+$lang['general_mark_qsl_rx_direct'] = 'Označit QSL přijato (Přímá)';
+$lang['general_mark_qsl_rx_electronic'] = 'Označit QSL přijato (Elektronicky)';
+$lang['general_mark_qsl_tx_bureau'] = 'Označit QSL odesláno (Bureau)';
+$lang['general_mark_qsl_tx_direct'] = 'Označit QSL odesláno (Přímá)';
 
 $lang['general_delete_qso'] = 'Smazat QSO';
 
+$lang['general_total_distance'] = 'Celková vzdálenost';
+
 // Cloudlog Terms
-$lang['cloudlog_station_profile'] = 'Profil stanice';
+$lang['cloudlog_station_profile'] = 'Umístění stanice';
 
 // ham radio terms
 $lang['gen_hamradio_qso'] = 'QSO';
@@ -58,42 +72,71 @@ $lang['gen_hamradio_station'] = 'Stanice';
 
 $lang['gen_hamradio_call'] = 'Značka';
 $lang['gen_hamradio_callsign'] = 'Značka';
-$lang['gen_hamradio_mode'] = 'Mód';
+$lang['gen_hamradio_mode'] = 'Režim';
 $lang['gen_hamradio_rst_sent'] = 'Odesláno';
 $lang['gen_hamradio_rst_rcvd'] = 'Přijato';
 $lang['gen_hamradio_band'] = 'Pásmo';
 $lang['gen_hamradio_band_rx'] = 'Pásmo (RX)';
-$lang['gen_hamradio_frequency'] = 'Kmitočet';
-$lang['gen_hamradio_frequency_rx'] = 'Kmitočet (RX)';
+$lang['gen_hamradio_frequency'] = 'Frekvence';
+$lang['gen_hamradio_frequency_rx'] = 'Frekvence (RX)';
 $lang['gen_hamradio_radio'] = 'Rádio';
 $lang['gen_hamradio_rsts'] = 'RST (S)';
 $lang['gen_hamradio_rstr'] = 'RST (R)';
-$lang['gen_hamradio_exchange_sent_short'] = 'Exch (S)';
-$lang['gen_hamradio_exchange_rcvd_short'] = 'Exch (R)';
+$lang['gen_hamradio_exchange_sent_short'] = 'Výměna (S)';
+$lang['gen_hamradio_exchange_rcvd_short'] = 'Výměna (R)';
 $lang['gen_hamradio_qsl'] = 'QSL';
 $lang['gen_hamradio_locator'] = 'Lokátor';
-$lang['gen_hamradio_transmit_power'] = 'Výkon (W)';
-$lang['gen_hamradio_propagation_mode'] = 'Druh šíření';
+$lang['gen_hamradio_transmit_power'] = 'Vysílací výkon (W)';
+$lang['gen_hamradio_propagation_mode'] = 'Šíření signálu';
 
-$lang['gen_hamradio_satellite_name'] = 'Jméno satelitu';
-$lang['gen_hamradio_satellite_mode'] = 'Mód satelitu';
+$lang['gen_hamradio_satellite_name'] = 'Název satelitu';
+$lang['gen_hamradio_satellite_mode'] = 'Režim satelitu';
 
-$lang['gen_hamradio_logbook'] = 'Deník';
+$lang['gen_hamradio_logbook'] = 'Logbook';
 
 $lang['gen_hamradio_cq_zone'] = 'CQ zóna';
 $lang['gen_hamradio_dxcc'] = 'DXCC';
+$lang['gen_hamradio_deleted_dxcc'] = 'Smazané DXCC';
+$lang['gen_hamradio_continent'] = 'Kontinent';
 $lang['gen_hamradio_usa_state'] = 'Stát USA';
-$lang['gen_hamradio_iota_reference'] = 'IOTA Reference';
-$lang['gen_hamradio_sota_reference'] = 'SOTA Reference';
+$lang['gen_hamradio_county_reference'] = 'Označení okresu USA';
+$lang['gen_hamradio_iota_reference'] = 'IOTA označení';
+$lang['gen_hamradio_sota_reference'] = 'SOTA označení';
+$lang['gen_hamradio_wwff_reference'] = 'WWFF označení';
+$lang['gen_hamradio_pota_reference'] = 'POTA označení';
 $lang['gen_hamradio_dok'] = 'DOK';
+$lang['gen_hamradio_state'] = 'Stát';
+$lang['gen_hamradio_iota'] = 'IOTA';
+$lang['gen_hamradio_sota'] = 'SOTA';
+$lang['gen_hamradio_wwff'] = 'WWFF';
+$lang['gen_hamradio_pota'] = 'POTA';
+$lang['gen_hamradio_gridsquare'] = 'Lokátor';
+$lang['gen_hamradio_distance'] = 'Vzdálenost';
+$lang['gen_hamradio_operator'] = 'Operátor';
 
 $lang['gen_hamradio_sig'] = 'Sig';
 $lang['gen_hamradio_sig_info'] = 'Sig Info';
 
 // Dashboard Words
-$lang['dashboard_you_have_had'] = 'Máš dnes';
-$lang['dashboard_qsos_today'] = 'spojení!';
-$lang['dashboard_qso_breakdown'] = 'Statistika spojení';
-$lang['dashboard_countries_breakdown'] = 'Statistika zemí';
+$lang['dashboard_you_have_had'] = 'Máte';
+$lang['dashboard_qsos_today'] = 'QSO spojení dnes!';
+$lang['dashboard_qso_breakdown'] = 'Počet QSO spojení';
+$lang['dashboard_countries_breakdown'] = 'Počet zemí';
 
-$lang['gen_from_date'] = 'Datum od';
+$lang['gen_from_date'] = 'Od data';
+
+$lang['gen_this_qso_was_confirmed_on'] = 'Toto QSO bylo potvrzeno';
+
+$lang['error_no_logbook_found'] = 'Nebyly nalezeny žádné denníky. Musíte definovat denník! Udělejte to zde:';
+
+$lang['copy_to_clipboard'] = 'Kopírovat do schránky';
+
+$lang['africa'] = 'Afrika';
+$lang['antarctica'] = 'Antarktida';
+$lang['asia'] = 'Asie';
+$lang['europe'] = 'Evropa';
+$lang['northamerica'] = 'Severní Amerika';
+$lang['oceania'] = 'Oceánie';
+$lang['southamerica'] = 'Jižní Amerika';
+
+$lang['gen_band_selection'] = 'Výběr pásma';
