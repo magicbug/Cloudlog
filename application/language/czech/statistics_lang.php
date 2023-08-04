4cfa0ae2be8d79d09d0920614bf8602e88aaa12b
Ondrej Kolonicny <ondra@ok1cdj.com>
2023-08-04 14:19:55 +0200

    Create statistics_lang.php in Czech

---
+++ application/language/czech/statistics_lang.php
@@ -0,0 +1,13 @@
+$lang['statistics_statistics'] = 'Statistiky';
+
+$lang['statistics_explore_the_logbook'] = 'Prozkoumejte logbook.';
+
+$lang['statistics_years'] = 'Roky';
+$lang['statistics_mode'] = 'Módy';
+$lang['statistics_bands'] = 'Pásma';
+$lang['statistics_qsos'] = 'QSO';
+$lang['statistics_unique_callsigns'] = 'Jedinečné značky';
+
+$lang['statistics_total'] = 'Celkem';
+
+$lang['statistics_year'] = 'Rok';
