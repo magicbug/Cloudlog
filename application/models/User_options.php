5dc5adb4bbd0537e1c0d276f74b6708cda2849cd
int2001 <joerg@dj7nt.de>
2023-08-18 12:49:12 +0000

    Model for User-options

---
+++ application/models/User_options.php
@@ -0,0 +1,22 @@
+<?php
+
+class User_options extends CI_Model
+{
+	public function options($option_type) {
+		$this->db->where('user_id', $this->session->userdata('user_id'));
+		$this->db->where('option_type', $option_type);
+		return $this->db->get('user_options');
+	}
+
+	public function set_option($option_type, $option_array) {
+		$uid=$this->session->userdata('user_id');
+		$sql='insert into user_options (user_id,option_type,option_key,option_value) values (?,?,?,?) ON DUPLICATE KEY UPDATE option_value=?';
+		$query = $this->db->query($sql);
+          	foreach ($option_array() as $one_option) {
+	  		$query = $this->db->query($sql, array($uid, $option_type, $option_key, $option_value, $option_value));
+          	}
+	}
+
+}
+
+?>
