99a08d0d18ee36729ff0b01e88cd440fb3654cac
Andreas <6977712+AndreasK79@users.noreply.github.com>
2023-08-31 08:41:13 +0200

    Merged in dev and updated migration number

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 140;
+$config['migration_version'] = 141;
 
 /*
 |--------------------------------------------------------------------------
