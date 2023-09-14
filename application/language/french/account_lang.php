a4e8efc7add567765e89448c0a2d9752225b6016
abarrau <hesky378@gmail.com>
2023-09-14 18:03:54 +0200

    Translate to French account_lang.php

--- application/language/french/account_lang.php
+++ application/language/french/account_lang.php
@@ -8,3 +8,84 @@ $lang['account_colonne2_text'] = 'Choisir colonne 2';
 $lang['account_colonne3_text'] = 'Choisir colonne 3';
 $lang['account_colonne4_text'] = 'Choisir colonne 4';
 $lang['account_colonne5_text'] = 'Choisir colonne 5 (uniquement pour  logbook)';
+
+$lang['account_create_user_account'] = 'Creer un compte';
+$lang['account_edit_account'] = 'Editer un compte';
+
+$lang['account_account_information'] = 'Informations du compte';
+$lang['account_username'] = 'Utilisateur';
+$lang['account_email_address'] = 'Email';
+$lang['account_password'] = 'Mot de passe';
+
+$lang['account_roles'] = 'Rôles';
+$lang['account_user_role'] = 'Rôle de l\'utilisateur';
+
+$lang['account_theme'] = 'Thème';
+$lang['account_stylesheet'] = 'Nom du thème';
+
+$lang['account_personal_information'] = 'Informations personnelles';
+$lang['account_first_name'] = 'Nom';
+$lang['account_last_name'] = 'Prénom';
+$lang['account_callsign'] = 'Indicatif';
+$lang['account_gridsquare'] = 'Locator';
+
+$lang['account_cloudlog_preferences'] = 'Préférences Cloudlog';
+$lang['account_timezone'] = 'Timezone';
+$lang['account_date_format'] = 'Format de la date';
+$lang['account_measurement_preferences'] = 'Unité de mesure';
+$lang['account_select_how_you_would_like_dates_shown_when_logged_into_your_account'] = '(Sélectionnez le format de date à afficher)';
+$lang['account_choose_which_unit_distances_will_be_shown_in'] = '(Choisiez l\'unité de mesure à afficher)';
+
+$lang['account_main_menu'] = 'Menu principal';
+$lang['account_show_notes_in_the_main_menu'] = 'Voir les notes dans le main principal.';
+
+$lang['account_gridsquare_and_location_autocomplete'] = 'Renseignement automatique du lieu et du Locator';
+$lang['account_location_auto_lookup'] = 'Recherche automatique de lieu.';
+$lang['account_if_set_gridsquare_is_fetched_based_on_location_name'] = '(Si activé, le Locator est récupéré en fonction du nom du lieu.)';
+$lang['account_sota_auto_lookup_gridsquare_and_name_for_summit'] = 'SOTA : Renseigner automatiquement le Locator et le nom du sommet.';
+$lang['account_wwff_auto_lookup_gridsquare_and_name_for_reference'] = 'WWFF : Renseigner automatiquement le Locator et le nom de la référence.';
+$lang['account_pota_auto_lookup_gridsquare_and_name_for_park'] = 'POTA : Renseigner automatiquement le Locateur et le nom du parc.';
+$lang['account_if_set_name_and_gridsquare_is_fetched_from_the_api_and_filled_in_location_and_locator'] = '(Si activé, le nom et le Locator seront récupérés depuis l\'API et renseignés.)';
+
+$lang['account_previous_qsl_type'] = 'Type de QSL pour le statut';
+$lang['account_select_the_type_of_qsl_to_show_in_the_previous_qsos_section'] = 'Selectionner le type de QSL à afficher (statut) dans la session précédents QSOs.';
+
+$lang['account_qrzcom_hamqthcom_images'] = 'qrz.com/hamqth.com Images';
+$lang['account_show_profile_picture_of_qso_partner_from_qrzcom_hamqthcom_profile_in_the_log_qso_section'] = 'Affiche l\'image de profil du contact (depuis qrz.com/hamqth.com) dans la session QSO.';
+$lang['account_please_set_your_qrzcom_hamqthcom_credentials_in_the_general_config_file'] = 'Indiquer les éléments d\'authentification pour qrz.com/hamqth.com credentials dans le fichier de configuration (config.php).';
+
+$lang['account_amsat_status_upload'] = 'Envoi du statut AMSAT';
+$lang['account_upload_status_of_sat_qsos_to'] = 'Envoi des statuts des QSOs Satellites sur';
+
+$lang['account_logbook_of_the_world'] = 'Logbook of the World';
+$lang['account_logbook_of_the_world_lotw_username'] = 'Utilisateur';
+$lang['account_logbook_of_the_world_lotw_password'] = 'Mot de passe';
+$lang['account_leave_blank_to_keep_existing_password'] = '(Laisser vide pour conserver le mot de passe existant)';
+
+$lang['account_clublog'] = 'Club Log';
+$lang['account_clublog_email_callsign'] = 'Email/Callsign';
+$lang['account_clublog_password'] = 'Mot de passe';
+$lang['account_the_email_or_callsign_you_use_to_login_to_club_log'] = '(Renseigner l\'email ou l\'indicatif pour se connecter à Club Log)';
+
+$lang['account_eqsl'] = 'eQSL';
+$lang['account_eqslcc_username'] = 'Utilisateur';
+$lang['account_eqslcc_password'] = 'Mot de passe';
+
+$lang['account_save_account_changes'] = 'Sauvegarder les changements';
+$lang['account_create_account'] = 'Creation du compte';
+
+$lang['account_delete_user_account'] = 'Supprimer l\'utilisateur';
+$lang['account_are_you_sure_you_want_to_delete_the_user_account'] = 'Etes-vous certain de supprimer le compte de l\'utilisateur ?';
+$lang['account_yes_delete_this_user'] = 'Oui, supprimer l\'utilisateur';
+$lang['account_no_do_not_delete_this_user'] = 'Non, ne pas supprimer l\'utilisateur';
+
+$lang['account_forgot_password'] = 'Mot de passe oublié ?';
+$lang['account_you_can_reset_your_password_here'] = 'Vous pouvez réinitialiser votre mot de passe ici.';
+$lang['account_reset_password'] = 'Mot de passe réinitialisé';
+$lang['account_the_email_field_is_required'] = 'Le champs Email est obligatoire';
+$lang['account_confirm_password'] = 'Mot de passe confirmé';
+
+$lang['account_forgot_your_password'] = 'Mot de passe oublié ?';
+
+$lang['account_login_to_cloudlog'] = 'Connexion à Cloudlog';
+$lang['account_login'] = 'Login';
