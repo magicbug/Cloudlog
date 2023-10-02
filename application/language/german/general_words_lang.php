ab9b432ad33622a3990be1eb7a26d62227970c98
HB9HIL <80885850+HB9HIL@users.noreply.github.com>
2023-10-02 21:13:38 +0200

    correct syntax error

--- application/language/german/general_words_lang.php
+++ application/language/german/general_words_lang.php
@@ -54,7 +54,7 @@ $lang['general_word_eqslcards'] = 'eQSL Karten';
 $lang['general_word_lotw'] = 'Logbook of the World';
 $lang['general_word_lotw_short'] = 'LoTW';
 
-$lang['general_word_details'] = 'Details'
+$lang['general_word_details'] = 'Details';
 
 $lang['general_edit_qso'] = 'QSO bearbeiten';
 $lang['general_mark_qsl_rx_bureau'] = 'Markiere QSL erhalten (Büro)';
