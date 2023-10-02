3924eea5b670ed31b9788f1f8c9863a35152f310
HB9HIL <80885850+HB9HIL@users.noreply.github.com>
2023-10-03 01:01:16 +0200

    fixed syntax mistake

--- application/language/french/account_lang.php
+++ application/language/french/account_lang.php
@@ -35,7 +35,7 @@ $lang['account_date_format'] = 'Format de la date';
 $lang['account_measurement_preferences'] = 'Unité de mesure';
 $lang['account_select_how_you_would_like_dates_shown_when_logged_into_your_account'] = '(Sélectionnez le format de date à afficher)';
 $lang['account_choose_which_unit_distances_will_be_shown_in'] = '(Choisiez l\'unité de mesure à afficher)';
-$lang['account_choose_cloudlog_language'] = 'Choose Cloudlog language.'
+$lang['account_choose_cloudlog_language'] = 'Choose Cloudlog language.';
 
 $lang['account_main_menu'] = 'Menu principal';
 $lang['account_show_notes_in_the_main_menu'] = 'Voir les notes dans le main principal.';
