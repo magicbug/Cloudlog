ae949e3b2f4482c793c00e81804afa154e5c4634
HB9HIL <fabian.berg@hb9hil.org>
2023-10-06 13:14:54 +0200

    Contest Controller Adjustments

--- application/controllers/Contesting.php
+++ application/controllers/Contesting.php
@@ -80,7 +80,7 @@ class Contesting extends CI_Controller {
 		$this->load->library('form_validation');
 
 		$this->form_validation->set_rules('name', 'Contest Name', 'required');
-		$this->form_validation->set_rules('adifname', 'Contest Adif Name', 'required');
+		$this->form_validation->set_rules('adifname', 'Adif Contest Name', 'required');
 
 		if ($this->form_validation->run() == FALSE)
 		{
@@ -106,9 +106,8 @@ class Contesting extends CI_Controller {
 	}
 
 	public function edit($id) {
-		$this->load->library('form_validation');
-
 		$this->load->model('Contesting_model');
+		$this->load->library('form_validation');
 
 		$item_id_clean = $this->security->xss_clean($id);
 
