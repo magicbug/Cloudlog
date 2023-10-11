96eff2b80a3b883664c1540ff041c7957c161217
int2001 <joerg@dj7nt.de>
2023-10-11 09:40:17 +0000

    Pattern-matching on QRZ-APIKey

--- application/views/station_profile/edit.php
+++ application/views/station_profile/edit.php
@@ -355,7 +355,7 @@
 				<div class="card-body">
 					<div class="form-group">
 						<label for="qrzApiKey">QRZ.com Logbook API Key</label>
-						<input type="text" class="form-control" name="qrzapikey" id="qrzApiKey" aria-describedby="qrzApiKeyHelp" value="<?php if(set_value('qrzapikey') != "") { echo set_value('qrzapikey'); } else { echo $my_station_profile->qrzapikey; } ?>">
+						<input type="text" class="form-control" name="qrzapikey" pattern="^([A-F0-9]{4}-){3}[A-F0-9]{4}$" id="qrzApiKey" aria-describedby="qrzApiKeyHelp" value="<?php if(set_value('qrzapikey') != "") { echo set_value('qrzapikey'); } else { echo $my_station_profile->qrzapikey; } ?>">
 						<small id="qrzApiKeyHelp" class="form-text text-muted">Find your API key on <a href="https://logbook.qrz.com/logbook" target="_blank">QRZ.com's settings page</a></small>
 					</div>
 
