e3ba0d0f0249debb2899a423cc2b507cf8c1645c
HB9HIL <fabian.berg@hb9hil.org>
2023-10-16 11:05:47 +0200

    Translate

--- application/language/english/adif_lang.php
+++ application/language/english/adif_lang.php
@@ -9,12 +9,25 @@ Topbar
 ___________________________________________________________________________________________
 */
 
-$lang[adif_import] = "ADIF Import";
-$lang[adif_export] = "ADIF Export";
+$lang['adif_import'] = "ADIF Import";
+$lang['adif_export'] = "ADIF Export";
 // $lang['lotw_title']                      --> application/language/english/lotw_lang.php
 
 
+/*
+___________________________________________________________________________________________
+ADIF Import
+___________________________________________________________________________________________
+*/
 
+$lang['adif_alert_important'] = "Important";
+$lang['adif_alert_log_files_type'] = "Log Files must have the file type *.adi";
+// $lang['general_word_warning']            --> application/language/english/general_words_lang.php "PHP Upload Warning"
+// $lang['gen_max_file_upload_size']        --> application/language/english/general_words_lang.php "PHP Upload Warning"
 
-$lang[adif_alert_important] = "Important";
+$lang['adif_select_stationlocation'] = "Select Station Location";
+// $lang['gen_hamradio_callsign']           --> application/language/english/general_words_lang.php
 
+// The File Input is translated by the Browser
+
+$lang['adif_import_dup'] = "Import duplicate QSOs";
