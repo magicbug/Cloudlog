269811f5cc24687162b0f18c5f9d5867e2134a1c
HB9HIL <fabian.berg@hb9hil.org>
2023-10-16 10:37:02 +0200

    Created New Lang file adif_lang.php

---
+++ application/language/english/adif_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+/*
+___________________________________________________________________________________________
+Topbar
+___________________________________________________________________________________________
+*/
+
+$lang[adif_import] = "ADIF Import";
+$lang[adif_export] = "ADIF Export";
+// $lang['lotw_title']                      --> application/language/english/lotw_lang.php
+
+
+
+
+$lang[adif_alert_important] = "Important";
+
