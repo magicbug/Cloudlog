6a0290aead9516725de0535f4d79118d72e1481a
phl0 <github@florian-wolters.de>
2023-10-16 23:01:22 +0200

    Fix c+p mistake

--- application/migrations/145_rename_gridmap_variables.php
+++ application/migrations/145_rename_gridmap_variables.php
@@ -17,8 +17,8 @@ class Migration_rename_gridmap_variables extends CI_Migration {
                 if ($this->db->field_exists('user_gridmap_default_band', 'users')) {
                         $this->db->query("ALTER TABLE `users` CHANGE `user_gridmap_default_band` `user_default_band` VARCHAR(10) DEFAULT NULL;");
                 }
-                if ($this->db->field_exists('user_default_confirmation', 'users')) {
-                        $this->db->query("ALTER TABLE `users` CHANGE `user_default_confirmation` `user_default_confirmation` VARCHAR(3) DEFAULT NULL;");
+                if ($this->db->field_exists('user_gridmap_confirmation', 'users')) {
+                        $this->db->query("ALTER TABLE `users` CHANGE `user_gridmap_confirmation` `user_default_confirmation` VARCHAR(3) DEFAULT NULL;");
                 }
         }
 
