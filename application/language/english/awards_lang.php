b5ba513d247e5ac7fc21759f7bb69763c61c4b98
HB9HIL <fabian.berg@hb9hil.org>
2023-10-20 15:43:22 +0200

    added new language

--- application/language/english/awards_lang.php
+++ application/language/english/awards_lang.php
@@ -2,15 +2,27 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$lang['awards_info_button'] = "Award Info";
+$lang['awards_info_title'] = "Informationen about this award";
+
 /*
 ___________________________________________________________________________________________
-US Gridmaster
+US Gridmaster -- Use all 4 Lines of Text
 ___________________________________________________________________________________________
 */
 
-$lang['awards_gridmaster_what_is'] = "What is the US Gridmaster Award?";
-$lang['awards_gridmaster_description_ln1'] = "The 'GridMaster Award' is the most prestigious AMSAT award. It was first introduced in 2014 by the Star Comm Group and is available to all amateur radio operators worldwide who manage to work all 488 grid squares in the USA via satellite and can provide QSL confirmations for each.";
-$lang['awards_gridmaster_description_ln2'] = "Official Information from the <a href='https://www.amsat.org/gridmaster/' target='_blank'>Website</a>: Two-way communication must be established via amateur satellite with each grid. There is no minimum signal report required. Contacts must be made from the same location, or from locations no two of which are more than 200 kilometers apart. The applicant’s attestation in the award application serves as affirmation of abidance to the distance rule. Individuals may apply for and be granted multiple GridMaster awards, when achieved from another location, which is in a different 200-kilometer circle.";
-$lang['awards_gridmaster_warning_hint'] = "This map shows only QSO worked on SAT";
+$lang['awards_gridmaster_description_ln1'] = "US Gridmaster Award";
+$lang['awards_gridmaster_description_ln2'] = "The GridMaster Award is the most prestigious AMSAT award. It was first introduced in 2014 by the Star Comm Group and is available to all amateur radio operators worldwide who manage to work all 488 grid squares in the USA via satellite and can provide QSL confirmations for each.";
+$lang['awards_gridmaster_description_ln3'] = "Official Information from the <a href='https://www.amsat.org/gridmaster/' target='_blank'>Website</a>: Two-way communication must be established via amateur satellite with each grid. There is no minimum signal report required. Contacts must be made from the same location, or from locations no two of which are more than 200 kilometers apart. The applicant’s attestation in the award application serves as affirmation of abidance to the distance rule. Individuals may apply for and be granted multiple GridMaster awards, when achieved from another location, which is in a different 200-kilometer circle.";
+$lang['awards_gridmaster_description_ln4'] = "This map shows only QSO worked on SAT";
 
+/*
+___________________________________________________________________________________________
+VUCC -- Use all 4 Lines of Text
+___________________________________________________________________________________________
+*/
 
+$lang['awards_vucc_description_ln1'] = "VHF/UHF Century Club Award";
+$lang['awards_vucc_description_ln2'] = "The VHF/UHF Century Club Award will be given for a minimum number of worked and confirmed gridsquares on a desired band.";
+$lang['awards_vucc_description_ln3'] = "Offical Information and the Rules you can find in this document: <a href='https://www.amsat.org/gridmaster/' target='_blank'>Click me!</a>";
+$lang['awards_vucc_description_ln4'] = "Only VHF/UHF Bands are relevant.";
