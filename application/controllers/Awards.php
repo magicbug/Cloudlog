9522ed7ae58ee003c0d17a4450ae1b1bd36ef9d2
HB9HIL <fabian.berg@hb9hil.org>
2023-10-21 00:11:13 +0200

    adjusted cq award name

--- application/controllers/Awards.php
+++ application/controllers/Awards.php
@@ -361,7 +361,7 @@ class Awards extends CI_Controller {
         }
 
         // Render page
-        $data['page_title'] = "Awards - CQ Magazine WAZ";
+        $data['page_title'] = "CQ Magazine WAZ Award";
 		$this->load->view('interface_assets/header', $data);
 		$this->load->view('awards/cq/index');
 		$this->load->view('interface_assets/footer');
