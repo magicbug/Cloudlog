7a3002e08e9e8731e9c3f75fcb16925d8169c07f
HB9HIL <fabian.berg@hb9hil.org>
2023-10-20 15:44:11 +0200

    added award info box

--- application/views/awards/gridmaster/index.php
+++ application/views/awards/gridmaster/index.php
@@ -42,9 +42,22 @@
 </style>
 <div class="container">
 
+  <!-- Award Info Box -->
 	<br>
-
-	<h2><?php echo $page_title; ?></h2>
+  <div id="awardInfoButton">
+    <script>
+      var lang_awards_info_button = "<?php echo lang('awards_info_button'); ?>";
+      var lang_awards_info_title = "<?php echo lang('awards_info_title'); ?>";
+      var lang_awards_info_title = "<?php echo lang('awards_info_title'); ?>";
+      var lang_award_info_ln1 = "<?php echo lang('awards_gridmaster_description_ln1'); ?>";
+      var lang_award_info_ln2 = "<?php echo lang('awards_gridmaster_description_ln2'); ?>";
+      var lang_award_info_ln3 = "<?php echo lang('awards_gridmaster_description_ln3'); ?>";
+      var lang_award_info_ln4 = "<?php echo lang('awards_gridmaster_description_ln4'); ?>";
+    </script>
+    <h2><?php echo $page_title; ?></h2>
+    <button type="button" class="btn btn-sm btn-primary mr-1" id="displayAwardInfo"><?php echo lang('awards_info_button'); ?></button>
+  </div>
+  <!-- End of Award Info Box -->
 
 		<?php if($this->session->flashdata('message')) { ?>
 			<!-- Display Message -->
@@ -54,16 +67,6 @@
 		<?php } ?>
 </div>
 
-<div class="card">
-      <div class="card-header">
-        <p><?php echo lang('awards_gridmaster_what_is'); ?></p>
-      </div>
-      <div class="card-body">
-        <p><?php echo lang('awards_gridmaster_description_ln1'); ?></p>
-        <p><?php echo lang('awards_gridmaster_description_ln2'); ?></p>
-        <span class="badge badge-warning"> <?php echo lang('awards_gridmaster_warning_hint'); ?></span>
-      </div>
-</div>
 
 <div id="gridmapcontainer">
 	<div id="gridsquare_map" style="width: 100%; height: 800px"></div>
