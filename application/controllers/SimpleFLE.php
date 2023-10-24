bb9827d0cf5c5140cf4c2bd7f2021340eebf0d7f
HB9HIL <fabian.berg@hb9hil.org>
2023-10-24 23:45:51 +0200

    Simple FLE Controller

--- application/controllers/SimpleFLE.php
+++ application/controllers/SimpleFLE.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class SimpleFLE extends CI_Controller {
+class SimpleFastLogEntry extends CI_Controller {
 
     public function index() {
         $this->load->model('user_model');
