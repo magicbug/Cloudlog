a45f49f4e60979be52e8ceec6bb6f67f37c96d9c
abarrau <hesky378@gmail.com>
2023-11-04 14:56:36 +0100

    centralized folder : Update Qsl.php

--- application/controllers/Qsl.php
+++ application/controllers/Qsl.php
@@ -185,18 +185,20 @@ class Qsl extends CI_Controller {
 function folderSize($dir){
     $count_size = 0;
     $count = 0;
-    $dir_array = scandir($dir);
-      foreach($dir_array as $key=>$filename){
-        if($filename!=".." && $filename!="."){
-           if(is_dir($dir."/".$filename)){
-              $new_foldersize = foldersize($dir."/".$filename);
-              $count_size = $count_size+ $new_foldersize;
-            }else if(is_file($dir."/".$filename)){
-              $count_size = $count_size + filesize($dir."/".$filename);
-              $count++;
+    if (is_dir($dir)) { // aba 
+        $dir_array = scandir($dir);
+        foreach($dir_array as $key=>$filename){
+            if($filename!=".." && $filename!="."){
+               if(is_dir($dir."/".$filename)){
+                  $new_foldersize = foldersize($dir."/".$filename);
+                  $count_size = $count_size+ $new_foldersize;
+                }else if(is_file($dir."/".$filename)){
+                  $count_size = $count_size + filesize($dir."/".$filename);
+                  $count++;
+                }
             }
-       }
-     }
+        }
+    } else log_message('error','QSL folder not exist ('.$dir.')');
     return $count_size;
 }
 
