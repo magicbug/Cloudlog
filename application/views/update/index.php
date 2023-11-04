77ea7f553bd7d90b74127ae142a5611c03a6d90c
abarrau <hesky378@gmail.com>
2023-11-04 14:34:53 +0100

    centralized folder : Update index.php

--- application/views/update/index.php
+++ application/views/update/index.php
@@ -7,9 +7,11 @@
 				<li class="nav-item">
 					<a class="nav-link active" id="dxcc-tab" data-toggle="tab" href="#dxcc" role="tab" aria-controls="update" aria-selected="true">DXCC Lookup Data</a>
 				</li>
-
 				<li class="nav-item">
 					<a class="nav-link" id="distance-tab" data-toggle="tab" href="#distance" role="tab" aria-controls="update" aria-selected="false">Distance Data</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" id="mig2datafolder-tab" data-toggle="tab" href="#mig2datafolder" role="tab" aria-controls="update" aria-selected="false">Data folder migration</a>
 				</li>
 			</ul>
 		</div>
@@ -52,10 +54,52 @@
 						</style>
 					<?php } ?>
 				</div>
+				
 				<div class="tab-pane fade" id="distance" role="tabpanel" aria-labelledby="distance-tab">
 					<p class="card-text">Here you can update QSOs with missing distance information.</p>
 					<p><a class="btn btn-primary" hx-get="<?php echo site_url('update/update_distances');?>"  hx-target="#distance_results" href="<?php echo site_url('update/update_distances');?>">Update distance data</a></p>
 					<div id="distance_results"></div>
+				</div>
+				
+				<div class="tab-pane fade" id="mig2datafolder" role="tabpanel" aria-labelledby="mig2datafolder-tab">
+					<?php if ($update_mig2datafolder_centralized_data_folder_not_exist) {
+						echo "<div class=\"alert alert-danger\">WARNING : You config value 'centralized_data_folder' was not create on config.php file !</div>";
+					} ?>
+					<p class="card-text">Here you can migrate (move) oldest folder to a centralized data folder.</p>
+					<div class="form-row">
+                    	<div class="col-sm-1"><b>EQSL :</b>
+                    	</div>
+                    	<div class="col-sm-5">
+                    		<label class="form-label">Original folder : </label><input name="update_mg2datafolder_eqsl_oldfolder" type="text" class="form-control" value="<?php echo $update_mig2datafolder_eqsl_oldfolder;?>" disabled />
+                    	</div>
+                    	<div class="col-sm-5">
+                    		<label class="form-label">New folder : </label>
+                    		<input name="update_mg2datafolder_eqsl_newfolder" type="text" class="form-control" value="<?php echo $update_mig2datafolder_eqsl_newfolder;?>" disabled />
+                    	</div>
+						<div class="col-sm-1">
+							<?php if (!$update_mig2datafolder_centralized_data_folder_not_exist) { ?>
+							<a class="btn btn-primary" hx-get="<?php echo site_url('update/update_datafolder_eqsl');?>" hx-target="#data_move2_results" style="padding:8px;margin-top:25px;" href="<?php echo site_url('update/update_datafolder_eqsl');?>">Move To</a>
+							<?php } ?>
+						</div>
+                    </div>
+                    <br/>
+					<div class="form-row">
+                    	<div class="col-sm-1"><b>QSL Card:</b>
+                    	</div>
+                    	<div class="col-sm-5">
+                    		<label class="form-label">Original folder : </label><input name="update_mg2datafolder_eqsl_oldfolder" type="text" class="form-control" value="<?php echo $update_mig2datafolder_qsl_oldfolder;?>" disabled />
+                    	</div>
+                    	<div class="col-sm-5">
+                    		<label class="form-label">New folder : </label>
+                    		<input name="update_mg2datafolder_eqsl_newfolder" type="text" class="form-control" value="<?php echo $update_mig2datafolder_qsl_newfolder;?>" disabled />
+                    	</div>
+						<div class="col-sm-1">
+							<?php if (!$update_mig2datafolder_centralized_data_folder_not_exist) { ?>
+							<a class="btn btn-primary" hx-get="<?php echo site_url('update/update_datafolder_qsl');?>" hx-target="#data_move2_results" style="padding:8px;margin-top:25px;" href="<?php echo site_url('update/update_datafolder_qsl');?>">Move To</a>
+							<?php } ?>
+						</div>
+                    </div>
+					<div id="data_move2_results" style="margin:10px 20px 10px 10px;"></div>
 				</div>
 			</div>
 		</div>
