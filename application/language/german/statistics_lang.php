61e331335529cceefdcb12d06ba46f98b9cb6e8d
HB9HIL <fabian.berg@hb9hil.org>
2023-11-05 19:30:35 +0100

    added various translations

--- application/language/german/statistics_lang.php
+++ application/language/german/statistics_lang.php
@@ -19,4 +19,12 @@ $lang['statistics_year'] = 'Jahr';
 $lang['statistics_number_of_qso_worked_each_year'] = "Anzahl der QSOs gearbeitet pro Jahr";
 $lang['statistics_number_of_qso_worked'] = "# gearbeitete QSOs";
 
+$lang['statistics_distances_worked'] = "Gearbeitete Entfernungen";
+$lang['statistics_distances_part1_contacts_were_plotted_furthest'] = "Kontakte wurden dargestellt.<br /> Der weiteste Kontakt war"; // make sure'<br />' stays there
+$lang['statistics_distances_part2_contacts_were_plotted_furthest'] = "im Planquadrat";
+$lang['statistics_distances_part3_contacts_were_plotted_furthest'] = "Die Distanz betrug";
+$lang['statistics_distances_number_of_qsos'] = "Anzahl der QSOs";
+$lang['statistics_distances_callsigns_worked'] = "Gearbeitete(s) Rufzeichen (max 5 werden gezeigt)";
+$lang['statistics_distances_qsos_with'] = "QSOs mit";
+$lang['statistics_distances_and_band'] = "und Band";
 
