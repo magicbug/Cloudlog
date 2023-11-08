4d997c1e70a43d7e3c0c9be619e8db6b7f7d81e9
HB9HIL <fabian.berg@hb9hil.org>
2023-11-09 00:54:26 +0100

    removed console.log message

--- assets/js/sections/simplefle.js
+++ assets/js/sections/simplefle.js
@@ -129,8 +129,7 @@ function handleInput() {
 			}
 			if (item.trim().match(/^day (\+)+$/)) {
 				var plusCount = item.match(/\+/g).length;
-				var originalDate = new Date(extraQsoDate);;
-				console.log(plusCount)
+				var originalDate = new Date(extraQsoDate);
 				originalDate.setDate(originalDate.getDate() + plusCount);
 				extraQsoDate = originalDate.toISOString().split("T")[0];
 			} else if (item.match(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/)) {
