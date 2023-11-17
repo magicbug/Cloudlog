fd745d883e2128e70b02d6a99e598b90f1c994d7
HB9HIL <fabian.berg@hb9hil.org>
2023-11-17 05:37:00 +0100

    updated

--- assets/js/datetime-moment.js
+++ assets/js/datetime-moment.js
@@ -12,6 +12,7 @@
  * @summary Sort date and time in any format using Moment.js
  * @author [Allan Jardine](//datatables.net)
  * @depends DataTables 1.10+, Moment.js 1.7+
+ * @deprecated
  *
  * @example
  *    $.fn.dataTable.moment( 'HH:mm MMM D, YY' );
@@ -26,45 +27,45 @@
 	} else {
 		factory(jQuery, moment);
 	}
-}(function ($, moment) {
+})(function ($, moment) {
+	function strip(d) {
+		if (typeof d === "string") {
+			// Strip HTML tags and newline characters if possible
+			d = d.replace(/(<.*?>)|(\r?\n|\r)/g, "");
 
-function strip (d) {
-	if ( typeof d === 'string' ) {
-		// Strip HTML tags and newline characters if possible
-		d = d.replace(/(<.*?>)|(\r?\n|\r)/g, '');
-
-		// Strip out surrounding white space
-		d = d.trim();
-	}
-
-	return d;
-}
-
-$.fn.dataTable.moment = function ( format, locale, reverseEmpties ) {
-	var types = $.fn.dataTable.ext.type;
-
-	// Add type detection
-	types.detect.unshift( function ( d ) {
-		d = strip(d);
-
-		// Null and empty values are acceptable
-		if ( d === '' || d === null ) {
-			return 'moment-'+format;
+			// Strip out surrounding white space
+			d = d.trim();
 		}
 
-		return moment( d, format, locale, true ).isValid() ?
-			'moment-'+format :
-			null;
-	} );
+		return d;
+	}
 
-	// Add sorting method - use an integer for the sorting
-	types.order[ 'moment-'+format+'-pre' ] = function ( d ) {
-		d = strip(d);
-		
-		return !moment(d, format, locale, true).isValid() ?
-			(reverseEmpties ? -Infinity : Infinity) :
-			parseInt( moment( d, format, locale, true ).format( 'x' ), 10 );
+	$.fn.dataTable.moment = function (format, locale, reverseEmpties) {
+		var types = $.fn.dataTable.ext.type;
+
+		// Add type detection
+		types.detect.unshift(function (d) {
+			d = strip(d);
+
+			// Null and empty values are acceptable
+			if (d === "" || d === null) {
+				return "moment-" + format;
+			}
+
+			return moment(d, format, locale, true).isValid()
+				? "moment-" + format
+				: null;
+		});
+
+		// Add sorting method - use an integer for the sorting
+		types.order["moment-" + format + "-pre"] = function (d) {
+			d = strip(d);
+
+			return !moment(d, format, locale, true).isValid()
+				? reverseEmpties
+					? -Infinity
+					: Infinity
+				: parseInt(moment(d, format, locale, true).format("x"), 10);
+		};
 	};
-};
-
-}));
+});
