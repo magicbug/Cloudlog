d6edea7809bb4a891a0dc902f79147d1168a673d
HB9HIL <fabian.berg@hb9hil.org>
2023-11-17 22:25:30 +0100

    responsive dropdown

--- application/views/view_log/partial/log_ajax.php
+++ application/views/view_log/partial/log_ajax.php
@@ -239,7 +239,7 @@ function echoQrbCalcLink($mygrid, $grid, $vucc) {
                             <i class="fas fa-cog"></i>
                         </a>
 
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink" data-bs-toggle="popover" data-bs-placement="auto">
                             <a class="dropdown-item" id="edit_qso" href="javascript:qso_edit(<?php echo $row->COL_PRIMARY_KEY; ?>)"><i class="fas fa-edit"></i> <?php echo lang('general_edit_qso'); ?></a>
 
                             <?php if($row->COL_QSL_SENT !='Y') { ?>
