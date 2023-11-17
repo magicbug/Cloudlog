a7f2b17365871c6765a5cad6f85244bfd8501ac3
HB9HIL <fabian.berg@hb9hil.org>
2023-11-17 09:38:13 +0100

    html5 datepicker

--- application/views/kml/index.php
+++ application/views/kml/index.php
@@ -101,22 +101,12 @@
                 <div class="row">
                     <div class="mb-3 col-md-3">
                         <label for="datetimepicker1"><?php echo lang('gen_from_date') . ':'; ?></label>
-                        <div class="exportdatepicker input-group date col-md-12" id="datetimepicker1" data-target-input="nearest">
-                            <input name="fromdate" type="text" placeholder="DD/MM/YYYY" class="form-control datetimepicker-input" data-bs-target="#datetimepicker1"/>
-                            <div class="input-group-append"  data-bs-target="#datetimepicker1" data-bs-toggle="datetimepicker">
-                                <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                            </div>
-                        </div>
+                        <input name="fromdate" type="date" id="datetimepicker1" class="form-control"/>
                     </div>
 
                     <div class="mb-3 col-md-3">
                         <label for="datetimepicker2"><?php echo lang('gen_to_date') . ':'; ?></label>
-                        <div class="exportdatepicker input-group date col-md-12" id="datetimepicker2" data-target-input="nearest">
-                            <input name="todate" totype="text" placeholder="DD/MM/YYYY" class="form-control datetimepicker-input" data-bs-target="#datetimepicker2"/>
-                            <div class="input-group-append" data-bs-target="#datetimepicker2" data-bs-toggle="datetimepicker">
-                                <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                            </div>
-                        </div>
+                        <input name="todate" type="date" id="datetimepicker2" class="form-control"/>
                     </div>
                 </div>    
                 <br>
