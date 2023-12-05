60e667da5fbc00476a8e6ff726dcfd8add392df8
HB9HIL <fabian.berg@hb9hil.org>
2023-12-05 23:53:23 +0100

    removed not needed class

--- application/views/awards/sig/index.php
+++ application/views/awards/sig/index.php
@@ -15,7 +15,7 @@
         <!-- End of Award Info Box -->
 
 <?php if ($sig_types) { ?>
-    <table style="width:100%" class="table-sm table tabledxcc table-bordered table-hover table-striped table-condensed text-center">
+    <table style="width:100%" class="table-sm table table-bordered table-hover table-striped table-condensed text-center">
 
 	<tr>
 		<td>Award Type</td>
