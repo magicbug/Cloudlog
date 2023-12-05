d52155ef1e2c8396623d449fbbe5f47063acd4f2
HB9HIL <fabian.berg@hb9hil.org>
2023-12-06 00:20:44 +0100

    popper is included in bootstrap 5

--- application/views/qso/edit_ajax.php
+++ application/views/qso/edit_ajax.php
@@ -19,7 +19,6 @@
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/jquery.fancybox.min.css" />
 
     <script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/jquery.fancybox.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 </head>
