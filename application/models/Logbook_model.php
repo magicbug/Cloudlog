96d63f49aa959136cfef686ea2493cdc08b7f35b
Peter Goodhall <peter@magicbug.co.uk>
2024-02-03 14:12:40 +0000

    Cleaned up the code better for #2947

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -294,11 +294,8 @@ class Logbook_model extends CI_Model
 
       $data['COL_STATION_CALLSIGN'] = strtoupper(trim($station['station_callsign']));
       $data['COL_MY_DXCC'] = strtoupper(trim($station['station_dxcc']));
-      if (isset($station['station_country'])) {
-        $data['COL_MY_COUNTRY'] = strtoupper(trim($station['station_country']));
-      } else {
-        $data['COL_MY_COUNTRY'] = null;
-      }
+      $data['COL_MY_COUNTRY'] = $station['station_country'] == null ? '' : strtoupper(trim($station['station_country']));
+
       $data['COL_MY_CNTY'] = strtoupper(trim($station['station_cnty']));
       $data['COL_MY_CQ_ZONE'] = strtoupper(trim($station['station_cq']));
       $data['COL_MY_ITU_ZONE'] = strtoupper(trim($station['station_itu']));
