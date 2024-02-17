4eb412c27be850e839a2941b264c7317c7bc9aae
Peter Goodhall <peter@magicbug.co.uk>
2024-02-17 16:57:48 +0000

    Cleaned up the code got rid of two database calls

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -112,7 +112,7 @@ function echoQrbCalcLink($mygrid, $grid, $vucc) {
 <?php } ?>
 </div>
 
-<?php if($this->optionslib->get_option('dashboard_map') != "false" && $this->optionslib->get_option('dashboard_map') != "map_at_right") { ?>
+<?php if($this->config->item('option_dashboard_map ') != "false" && $this->config->item('option_dashboard_map ') != "map_at_right") { ?>
 <!-- Map -->
 <div id="map" class="map-leaflet" style="width: 100%; height: 350px"></div>
 <?php } ?>
