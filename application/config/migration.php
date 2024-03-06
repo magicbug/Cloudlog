bac23f02cc65d32403100f6da8e49d7197aad571
Peter Goodhall <peter@magicbug.co.uk>
2024-03-06 15:25:43 +0000

    tag 2.6.6

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 172;
+$config['migration_version'] = 173;
 
 /*
 |--------------------------------------------------------------------------
