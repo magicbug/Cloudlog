51eb502144913afb01f14a1eaf229fcd16351cb1
Peter Goodhall <peter@magicbug.co.uk>
2024-03-21 13:30:21 +0000

    Get ready for version 2.6.7 stable

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 173;
+$config['migration_version'] = 174;
 
 /*
 |--------------------------------------------------------------------------
