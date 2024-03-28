41bc9b3e1beac571788dd7c01d2a1d3e8550c049
Peter Goodhall <peter@magicbug.co.uk>
2024-03-28 15:22:01 +0000

    Make sure that sstv images table doesnt already exist

--- application/migrations/175_add_sstv_images_table.php
+++ application/migrations/175_add_sstv_images_table.php
@@ -1,33 +1,37 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Migration_add_sstv_images_table extends CI_Migration {
+class Migration_add_sstv_images_table extends CI_Migration
+{
 
         public function up()
         {
-                $this->dbforge->add_field(array(
-                        'id' => array(
-                                'type' => 'INT',
-                                'constraint' => 5,
-                                'unsigned' => TRUE,
-                                'auto_increment' => TRUE
-                        ),
-                        'qsoid' => array(
-                                'type' => 'VARCHAR',
-                                'constraint' => '250',
-                        ),
-                        'filename' => array(
-                                'type' => 'VARCHAR',
-                                'constraint' => '250',
-                        ),
-                        'modified' => array(
-                                'type' => 'timestamp',
-                                'null' => TRUE,
-                        ),
-                ));
-                $this->dbforge->add_key('id', TRUE);
-                $this->dbforge->create_table('sstv_images');
+                // if sstv_images table doesn't exist
+                if (!$this->db->table_exists('sstv_images')) {
+                        $this->dbforge->add_field(array(
+                                'id' => array(
+                                        'type' => 'INT',
+                                        'constraint' => 5,
+                                        'unsigned' => TRUE,
+                                        'auto_increment' => TRUE
+                                ),
+                                'qsoid' => array(
+                                        'type' => 'VARCHAR',
+                                        'constraint' => '250',
+                                ),
+                                'filename' => array(
+                                        'type' => 'VARCHAR',
+                                        'constraint' => '250',
+                                ),
+                                'modified' => array(
+                                        'type' => 'timestamp',
+                                        'null' => TRUE,
+                                ),
+                        ));
+                        $this->dbforge->add_key('id', TRUE);
+                        $this->dbforge->create_table('sstv_images');
+                }
         }
 
         public function down()
