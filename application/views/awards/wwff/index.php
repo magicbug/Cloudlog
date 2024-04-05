aff522334a16a779f11505d238bb47e194e08fcc
phl0 <github@florian-wolters.de>
2024-04-05 10:56:03 +0200

    Fix WWFF award info

--- application/views/awards/wwff/index.php
+++ application/views/awards/wwff/index.php
@@ -4,10 +4,10 @@
         <div id="awardInfoButton">
             <script>
             var lang_awards_info_button = "<?php echo lang('awards_info_button'); ?>";
-            var lang_award_info_ln1 = "<?php echo lang('awards_dok_description_ln1'); ?>";
-            var lang_award_info_ln2 = "<?php echo lang('awards_dok_description_ln2'); ?>";
-            var lang_award_info_ln3 = "<?php echo lang('awards_dok_description_ln3'); ?>";
-            var lang_award_info_ln4 = "<?php echo lang('awards_dok_description_ln4'); ?>";
+            var lang_award_info_ln1 = "<?php echo lang('awards_wwff_description_ln1'); ?>";
+            var lang_award_info_ln2 = "<?php echo lang('awards_wwff_description_ln2'); ?>";
+            var lang_award_info_ln3 = "<?php echo lang('awards_wwff_description_ln3'); ?>";
+            var lang_award_info_ln4 = "<?php echo lang('awards_wwff_description_ln4'); ?>";
             </script>
             <h2><?php echo $page_title; ?></h2>
             <button type="button" class="btn btn-sm btn-primary me-1" id="displayAwardInfo"><?php echo lang('awards_info_button'); ?></button>
