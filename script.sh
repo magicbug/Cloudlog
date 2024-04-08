ede0b201a5b4c75120a2498466222281fb449d03
Patrick Burns <burnsoft@gmail.com>
2024-04-07 20:04:08 -0500

    attempt to add the permissions stuff to the script.sh before adding it to github actions

--- script.sh
+++ script.sh
@@ -79,5 +79,24 @@ rm -rf /install
 
 echo "Replacement complete."
 
+# Set Permissions
+chown -R root:www-data /var/www/html/application/config/
+chown -R root:www-data /var/www/html/application/logs/
+chown -R root:www-data /var/www/html/assets/qslcard/
+chown -R root:www-data /var/www/html/backup/
+chown -R root:www-data /var/www/html/updates/
+chown -R root:www-data /var/www/html/uploads/
+chown -R root:www-data /var/www/html/images/eqsl_card_images/
+chown -R root:www-data /var/www/html/assets/json
+
+chmod -R g+rw /var/www/html/application/config/
+chmod -R g+rw /var/www/html/application/logs/
+chmod -R g+rw /var/www/html/assets/qslcard/
+chmod -R g+rw /var/www/html/backup/
+chmod -R g+rw /var/www/html/updates/
+chmod -R g+rw /var/www/html/uploads/
+chmod -R g+rw /var/www/html/images/eqsl_card_images/
+chmod -R g+rw /var/www/html/assets/json
+
 # Start Apache in the foreground
 exec apache2-foreground
