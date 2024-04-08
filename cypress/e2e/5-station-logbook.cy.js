f619a6b7026191958e52156e214aa460838f22f1
Patrick Burns <burnsoft@gmail.com>
2024-04-07 20:53:47 -0500

    add a step to check that the link station button works

--- cypress/e2e/5-station-logbook.cy.js
+++ cypress/e2e/5-station-logbook.cy.js
@@ -4,6 +4,7 @@ describe("Create station logbook", () => {
 	});
 
 	it("should load an empty list of station locations", () => {
+		// Navigate to the logbooks page
 		cy.visit("/index.php/logbooks");
 
 		// Check that the table is not present
@@ -11,6 +12,7 @@ describe("Create station logbook", () => {
 	});
 
 	it("should have a button to create a new station location", () => {
+		// Navigate to the logbooks page
 		cy.visit("/index.php/logbooks");
 
 		// Check that the button is present
@@ -20,6 +22,7 @@ describe("Create station logbook", () => {
 	});
 
 	it("should create a new station location", () => {
+		// Navigate to the create logbook page
 		cy.visit("/index.php/logbooks/create");
 
 		// Define the station location name
@@ -39,6 +42,42 @@ describe("Create station logbook", () => {
 		// // Check if the station location is present in the table
 		cy.get("#station_logbooks_table")
 			.contains(stationLogbookName)
+			.should("exist");
+	});
+
+	it("should set as active station logbook when button clicked", () => {
+		// Navigate to the logbooks page
+		cy.visit("/index.php/logbooks");
+
+		// Check that the button is present
+		cy.get("a").contains("Set as Active Logbook").should("exist").click();
+
+		// Check if the station was set to active
+		cy.get("body")
+			.contains("Active Logbook")
+			.should("be.visible")
+			.and("have.class", "badge text-bg-success");
+	});
+
+	it("should link to a station location from the edit logbook page", () => {
+		// Navigate to the logbooks page
+		cy.visit("/index.php/logbooks");
+
+		// Click the edit button
+		cy.get("i.fas.fa-edit").should("exist").click();
+
+		// Ensure that the edit link navigates to the correct page
+		cy.url().should("include", "/logbooks/edit");
+
+		// Scroll to the bottom of the page
+		cy.scrollTo("bottom");
+
+		// Click the link location button
+		cy.get("button").contains("Link Location").should("exist").click();
+
+		// Make sure that our table now shows the linked station location
+		cy.get("#station_logbooks_linked_table")
+			.contains("Test Station Location")
 			.should("exist");
 	});
 });
