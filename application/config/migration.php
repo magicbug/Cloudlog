0c55dd6bf498100aa01ae0415f9ddfbbd6320d64
Peter Goodhall <peter@magicbug.co.uk>
2024-10-03 15:21:47 +0100

    tag 2.6.16

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 187;
+$config['migration_version'] = 188;
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/migrations/188_tag_2_6_16.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+*   Tag Cloudlog as 2.6.16
+*/
+
+class Migration_tag_2_6_16 extends CI_Migration {
+
+    public function up()
+    {
+    
+        // Tag Cloudlog 2.6.15
+        $this->db->where('option_name', 'version');
+        $this->db->update('options', array('option_value' => '2.6.16'));
+
+        // Trigger Version Info Dialog
+        $this->db->where('option_type', 'version_dialog');
+        $this->db->where('option_name', 'confirmed');
+        $this->db->update('user_options', array('option_value' => 'false'));
+
+    }
+
+    public function down()
+    {
+        $this->db->where('option_name', 'version');
+        $this->db->update('options', array('option_value' => '2.6.15'));
+    }
+}
