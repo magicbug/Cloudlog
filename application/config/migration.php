3052986a90aa1b1ae1dba96dc69e139e1da1fe05
Peter Goodhall <peter@magicbug.co.uk>
2025-03-05 21:46:32 +0000

    Set SO-124 to upload to Lotw

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 193;
+$config['migration_version'] = 194;
 
 /*
 |--------------------------------------------------------------------------
