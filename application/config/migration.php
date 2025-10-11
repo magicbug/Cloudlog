9c3a8d3c51b113e8261a4b6ec5622e5398675336
Peter Goodhall <peter@magicbug.co.uk>
2025-10-11 15:42:11 +0100

    Add composite indexes for LoTW performance
    Introduces migration 218 to add composite indexes to optimize LoTW upload and certificate lookup queries. Updates migration version to 218 in the config. These indexes improve query performance for LoTW-related operations.

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 217;
+$config['migration_version'] = 218;
 
 /*
 |--------------------------------------------------------------------------
