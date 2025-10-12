ca0f344d9de55e9bad2dcb4d773fc140588e4c68
Peter Goodhall <peter@magicbug.co.uk>
2025-10-12 17:38:40 +0100

    Add IOTA table performance indexes and migration
    Introduces migration 221 to add indexes on the IOTA table for Tag, Prefix, and Status fields to improve query performance. Updates migration version in config to 221.

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 220;
+$config['migration_version'] = 221;
 
 /*
 |--------------------------------------------------------------------------
