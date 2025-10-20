5fce83b2015622f40064f7163cc31eb41d888b82
Peter Goodhall <peter@magicbug.co.uk>
2025-10-20 17:36:03 +0100

    Add migration for Cloudlog version 2.7.6
    Introduces migration 231 to update the application version to 2.7.6 and trigger the version info dialog for users. Updates the migration version in the configuration to 231.

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 230;
+$config['migration_version'] = 231;
 
 /*
 |--------------------------------------------------------------------------
