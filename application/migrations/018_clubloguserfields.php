04401423bc0a0e818b0f7877a7d6601f36e67283
Peter Goodhall <peter@magicbug.co.uk>
2025-10-29 15:14:31 +0000

    Add clublog user fields with existence check
    Migration now checks if 'user_clublog_name', 'user_clublog_password', and 'user_clublog_callsign' fields exist before adding them to the 'users' table, preventing errors if fields are already present.

--- application/migrations/018_clubloguserfields.php
+++ application/migrations/018_clubloguserfields.php
@@ -6,12 +6,21 @@ class Migration_clubloguserfields extends CI_Migration {
 
         public function up()
         {
-                $user_fields = array(
-                        'user_clublog_name VARCHAR(255) DEFAULT NULL',
-                        'user_clublog_password VARCHAR(255) DEFAULT NULL',
-                        'user_clublog_callsign VARCHAR(255) DEFAULT NULL'
+                // Check and add each field only if it does not exist
+                $fields_to_add = array(
+                        'user_clublog_name' => 'VARCHAR(255) DEFAULT NULL',
+                        'user_clublog_password' => 'VARCHAR(255) DEFAULT NULL',
+                        'user_clublog_callsign' => 'VARCHAR(255) DEFAULT NULL'
                 );
-                $this->dbforge->add_column('users', $user_fields);
+
+                // Get current columns in 'users' table
+                $fields = $this->db->list_fields('users');
+
+                foreach ($fields_to_add as $field => $definition) {
+                        if (!in_array($field, $fields)) {
+                                $this->dbforge->add_column('users', array($field . ' ' . $definition));
+                        }
+                }
         }
 
         public function down()
