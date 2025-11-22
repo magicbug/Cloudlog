fed2acd44f6a60b3f02ee405f6f8066f3a0e502d
Peter Goodhall <peter@magicbug.co.uk>
2025-11-22 17:46:03 +0000

    Fix font file path usage in AddFont method
    Ensures that the current font file path is always used in the AddFont function, rather than relying on a cached path from .mtx.php. This change improves reliability when loading font files.

--- src/Label/tfpdf.php
+++ src/Label/tfpdf.php
@@ -535,6 +535,8 @@ function AddFont($family, $style='', $file='', $uni=false)
 			unset($ttf);
 		}
 		else {
+			// Always use the current path, not the cached one from .mtx.php
+			$ttffile = $ttffilename;
 			$cw = @file_get_contents($unifilename.'.cw.dat'); 
 		}
 		$i = count($this->fonts)+1;
