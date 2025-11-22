ac27e6a65350b92ce13652f68f9f590106d95717
Peter Goodhall <peter@magicbug.co.uk>
2025-11-22 15:23:47 +0000

    Add welcome email feature for new and existing users
    Introduces functionality to send a welcome email with credentials to new users upon creation and allows admins to resend a reminder email (without password) to existing users. Adds email templates, UI options for sending emails, and improves user management interface with new actions and enhanced layout.

---
+++ application/views/email/welcome.php
@@ -0,0 +1,35 @@
+Hi <?php echo $user_firstname; ?> <?php echo $user_lastname; ?>,
+
+Welcome to Cloudlog!
+
+Your account has been created with the following details:
+
+Username: <?php echo $username; ?>
+
+Callsign: <?php echo $callsign; ?>
+
+Email: <?php echo $email; ?>
+
+Temporary Password: <?php echo $password; ?>
+
+
+IMPORTANT - Security Notice:
+For your security, please log in and change your password immediately.
+
+To change your password:
+1. Log in at: <?php echo $base_url; ?>
+
+2. Go to Account Settings
+3. Enter your current password and choose a new secure password
+4. Click "Save Account Changes"
+
+Getting Started with Cloudlog:
+- Create Station Locations for your operating locations
+- Set up your logbooks to organize your QSOs
+- Start logging your contacts!
+
+If you have any questions, please contact the administrator.
+
+Regards,
+
+Cloudlog.
