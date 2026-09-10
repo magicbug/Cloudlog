2c0e0e6cf6c35686d906d55d7098f1f02093c552
Peter Goodhall <84308+magicbug@users.noreply.github.com>
2026-09-10 14:37:14 +0100

    Add 2.8.19 version migration
    Bump the migration version to 281 and add a new migration that tags Cloudlog as 2.8.19. The migration also resets the version dialog confirmation so users see the updated version info, and the down method restores the previous 2.8.18 version tag.

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 280;
+$config['migration_version'] = 281;
 
 /*
 |--------------------------------------------------------------------------
