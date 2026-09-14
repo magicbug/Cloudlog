d0f93df48f4f20567ca955b71049454306bebda2
Peter Goodhall <84308+magicbug@users.noreply.github.com>
2026-09-14 10:45:02 +0100

    Fix KML export so Google Earth can parse the downloaded qsos.kml file.

--- application/controllers/Kmlexport.php
+++ application/controllers/Kmlexport.php
@@ -33,7 +33,7 @@ class Kmlexport extends CI_Controller {
         if(!$this->user_model->authorize(2)) { $this->session->set_flashdata('notice', 'You\'re not allowed to do that!'); redirect('dashboard'); }
 		// Load Libraries
 		$this->load->library('qra');
-		$this->load->helper('file');
+		$this->load->helper('download');
 
 		// Load Database connections
 		$this->load->model('logbook_model');
@@ -50,50 +50,41 @@ class Kmlexport extends CI_Controller {
 		// Get QSOs with Valid QRAs
 		$qsos = $this->logbook_model->kml_get_all_qsos($band, $mode, $dxcc, $cqz, $propagation, $fromdate, $todate);
 
-		$output = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
-		$output .= "<kml xmlns=\"http://www.opengis.net/kml/2.2\">";
-
-		$output .= "<Document>";
-
-		foreach ($qsos->result() as $row) {
-			if($row->COL_GRIDSQUARE != null) {
+		$placemarks = '';
+		if ($qsos) {
+			foreach ($qsos->result() as $row) {
 				$stn_loc = $this->qra->qra2latlong($row->COL_GRIDSQUARE);
-				
+				if (!is_array($stn_loc) || !isset($stn_loc[0], $stn_loc[1])) {
+					continue;
+				}
+
 				$lat = $stn_loc[0];
 				$lng = $stn_loc[1];
-			} else {
-				$query = $this->db->query('
-					SELECT *
-					FROM dxcc_entities
-					WHERE prefix = SUBSTRING( \''.$row->COL_CALL.'\', 1, LENGTH( prefix ) )
-					ORDER BY LENGTH( prefix ) DESC
-					LIMIT 1 
-				');
-					foreach ($query->result() as $dxcc) {
-						$lat = $dxcc->lat;
-						$lng = $dxcc->long;
-					}
-			}
+				$timestamp = strtotime($row->COL_TIME_ON);
+				$call = htmlspecialchars((string) $row->COL_CALL, ENT_XML1 | ENT_QUOTES, 'UTF-8');
+				$qso_band = htmlspecialchars((string) $row->COL_BAND, ENT_XML1 | ENT_QUOTES, 'UTF-8');
+				$datetime = htmlspecialchars(date('Y-m-d H:i:s', $timestamp), ENT_XML1, 'UTF-8');
 
-			if (isset($lat) && isset($lng)) {
-				$output .= "<Placemark>";
-
-				$timestamp = strtotime($row->COL_TIME_ON); 
-
-				$output .= "<name>".$row->COL_CALL."</name>";
-				$output .= "<description><![CDATA[<p>Date/Time: ".date('Y-m-d H:i:s', ($timestamp))."<br/>Band: ".$row->COL_BAND."<br /></p>]]></description>";		
-				$output .= "<Point>";
-				$output .= "<coordinates>".$lng.",".$lat.",0</coordinates>";
-				$output .= "</Point>";
-				$output .= "</Placemark>";
+				$placemarks .= "    <Placemark>\n";
+				$placemarks .= "      <name>".$call."</name>\n";
+				$placemarks .= "      <description><![CDATA[<p>Date/Time: ".$datetime."<br/>Band: ".$qso_band."<br /></p>]]></description>\n";
+				$placemarks .= "      <Point>\n";
+				$placemarks .= "        <coordinates>".$lng.",".$lat.",0</coordinates>\n";
+				$placemarks .= "      </Point>\n";
+				$placemarks .= "    </Placemark>\n";
 			}
 		}
 
-		$output .= "</Document>";
-		$output .= "</kml>";
+		// XML declaration and <kml> must be on separate lines. Google Earth rejects
+		// a single-line file as an invalid root element (line 1, column 7).
+		$output  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+		$output .= "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n";
+		$output .= "  <Document>\n";
+		$output .= "    <name>Cloudlog QSOs</name>\n";
+		$output .= $placemarks;
+		$output .= "  </Document>\n";
+		$output .= "</kml>\n";
 
-		header("Content-Disposition: attachment; filename=\"qsos.kml\"");
-		echo $output;
-
+		force_download('qsos.kml', $output, TRUE);
 	}
 }
--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -2744,6 +2744,10 @@ class Logbook_model extends CI_Model
     $CI->load->model('logbooks_model');
     $logbooks_locations_array = $CI->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
 
+    if (!$logbooks_locations_array) {
+      return null;
+    }
+
     $this->db->select('COL_CALL, COL_BAND, COL_TIME_ON, COL_RST_RCVD, COL_RST_SENT, COL_MODE, COL_SUBMODE, COL_NAME, COL_COUNTRY, COL_PRIMARY_KEY, COL_SAT_NAME, COL_GRIDSQUARE');
     $this->db->where_in('station_id', $logbooks_locations_array);
     $this->db->where("coalesce(COL_GRIDSQUARE, '') <> ''");
